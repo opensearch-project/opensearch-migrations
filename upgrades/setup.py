6bb99e066c3886e6792a63ca6f861f05d2d544dd
Chris Helma <chelma+github@amazon.com>
2022-11-28 10:22:03 -0600

    Added core library code for shell interactions
    * Added code for invoking shell commands via pexpect
    * Added unit tests for the new code

---
+++ upgrades/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="upgrade_testing_framework",
+    version="0.1",
+    description="Tools and scripts to help test upgrades between versions of ElasticSearch and OpenSearch",
+    author="Chris Helma",
+    package_dir={"": "upgrade_testing_framework"},
+    packages=setuptools.find_packages(where="upgrade_testing_framework"),
+    install_requires=[
+        "pexpect",
+        "pytest"
+    ],
+    python_requires=">=3.6",
+)
