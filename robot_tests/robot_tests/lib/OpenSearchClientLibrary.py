24cc1dddf34154065c9b316bd2f2232ce795f69f
Mikayla Thompson <mrt@mikayla.codes>
2023-01-03 13:09:03 -0700

    UTF Reporting Prototype (#63)
    * Add note in UTF readme on how to run it
    
    Signed-off-by: Mikayla Thompson <thomika@amazon.com>
    
    * Add comparisons to VersionEngine
    
    Signed-off-by: Mikayla Thompson <thomika@amazon.com>
    
    * Add Expectations and update SelectExpectations step
    
    Signed-off-by: Mikayla Thompson <thomika@amazon.com>
    
    * Add example expectations
    
    Signed-off-by: Mikayla Thompson <thomika@amazon.com>
    
    * Linter fixes
    
    Signed-off-by: Mikayla Thompson <thomika@amazon.com>
    
    * Address review comments & add unit tests
    
    Signed-off-by: Mikayla Thompson <thomika@amazon.com>
    
    * Add Report Results step
    
    Signed-off-by: Mikayla Thompson <thomika@amazon.com>
    
    * Adding documentation & linter fixes
    
    Signed-off-by: Mikayla Thompson <thomika@amazon.com>
    
    * Update data storage for robot tests
    
    Signed-off-by: Mikayla Thompson <thomika@amazon.com>

--- robot_tests/robot_tests/lib/OpenSearchClientLibrary.py
+++ robot_tests/robot_tests/lib/OpenSearchClientLibrary.py
@@ -1,11 +1,18 @@
+from pathlib import Path
+import json
+from typing import Any
+
 from opensearchpy import OpenSearch
+
+DEFAULT_TEMP_STORAGE="/tmp/utf/robot_data.json"
 
 # Change this library to use REST requests instead of Python client
 class OpenSearchClientLibrary(object):
 
-    def __init__(self, host="localhost", port=9200):
+    def __init__(self, host="localhost", port=9200, temp_storage_location=DEFAULT_TEMP_STORAGE):
         os_client = OpenSearch(hosts=[{'host': host, 'port': port}])
         self._os_client = os_client
+        self._temp_storage_location = Path(temp_storage_location)
 
     def create_index(self, index_name: str):
         self._os_client.indices.create(index_name)
@@ -23,15 +30,24 @@ class OpenSearchClientLibrary(object):
         self._os_client.indices.refresh(index=index_name)
 
     # Storing intermediate state needs to be thought through, this uses a
-    # a file to store state for time being
-    def store_data_with_label(self, data: str, label: str):
-        file = open('temp-storage.txt', 'a')
-        file.write(label + "=" + str(data) + "\n")
-        file.close()
+    # a file to store state for time being. "Any" is a bit generous of a type
+    # for data -- what we really mean is any json-serializable type.
+    def store_data_with_label(self, data: Any, label: str):
+        if self._temp_storage_location.exists():
+            with self._temp_storage_location.open('r') as file_pointer:
+                try:
+                    stored_data = json.load(file_pointer)
+                except json.JSONDecodeError:
+                    # This is most likely an empty file.
+                    stored_data = {}
+        else:
+            stored_data = {}
+        stored_data[label] = data
+        with self._temp_storage_location.open('w') as file_pointer:
+            json.dump(stored_data, file_pointer)
 
-    def retrieve_stored_number_by_label(self, label: str) -> int:
-        file = open('temp-storage.txt', 'r')
-        for line in file:
-            if line.startswith(label + "="):
-                return int(line.split("=", 1)[1])
-        return -1
+    # Retrieve the stored data. If it doesn't exist, return None.
+    def retrieve_stored_data_by_label(self, label: str) -> Any:
+        with self._temp_storage_location.open('r') as file_pointer:
+            stored_data = json.load(file_pointer)
+        return stored_data.get(label)
--- upgrades/upgrade_testing_framework/steps/step_select_expectations.py
+++ upgrades/upgrade_testing_framework/steps/step_select_expectations.py
@@ -1,4 +1,8 @@
+from pathlib import Path
+
 from upgrade_testing_framework.core.framework_step import FrameworkStep
+from upgrade_testing_framework.core.expectation import load_knowledge_base
+
 
 class SelectExpectations(FrameworkStep):
     """
@@ -7,11 +11,22 @@ class SelectExpectations(FrameworkStep):
 
     def _run(self):
         # Get the state we need
+        source_version = self.state.test_config.clusters_def.source.engine_version
+        target_version = self.state.test_config.clusters_def.target.engine_version
+
+        path_to_knowledge_base = Path("../knowledge_base")
 
         # Begin the step body
-        # Query the expectation knowledge base
-        self.logger.info("Querying applicable expectations...")
+        # Load all expectations
+        self.logger.info("Loading Knowledge Base and filtering for relevant expectations")
+        expectations = load_knowledge_base(path_to_knowledge_base)
+        # Filter for relevant ones
+        relevant_expectation_ids = [e.id for e in expectations
+                                    if e.applies_to_version(source_version) or
+                                    e.applies_to_version(target_version)]
+        self.logger.info(f"Found {len(relevant_expectation_ids)} relevant expectations.")
+        self.logger.debug(f"Relevant expectations: {relevant_expectation_ids}")
 
         # Update our state
         # TODO Temporary measure until this step queries for expectations
-        self.state.eligible_expectations = ['consistent-document-count']
+        self.state.eligible_expectations = relevant_expectation_ids
