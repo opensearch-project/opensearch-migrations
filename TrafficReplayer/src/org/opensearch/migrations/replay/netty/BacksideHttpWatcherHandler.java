27141a6bbd104020ac7850c64c174fbd0bb408bd
Greg Schohn <schohn@amazon.com>
2023-02-20 09:40:56 -0500

    Still a major WIP, but a lot of bugfixes to get flow working better. In a small test, I'm able to see response summaries for 76 completed HTTP responses.  There's too much logging and not enough effort to surface information to understand where there are errors.

--- TrafficReplayer/src/org/opensearch/migrations/replay/netty/BacksideHttpWatcherHandler.java
+++ TrafficReplayer/src/org/opensearch/migrations/replay/netty/BacksideHttpWatcherHandler.java
@@ -4,11 +4,12 @@ import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.FullHttpResponse;
 import org.opensearch.migrations.replay.AggregatedRawResponse;
 
 import java.util.function.Consumer;
 
-public class BacksideHttpWatcherHandler extends SimpleChannelInboundHandler<FullHttpRequest> {
+public class BacksideHttpWatcherHandler extends SimpleChannelInboundHandler<FullHttpResponse> {
 
     private final AggregatedRawResponse.Builder aggregatedRawResponseBuilder;
     private boolean doneReadingRequest; // later, when connections are reused, switch this to a counter?
@@ -21,7 +22,7 @@ public class BacksideHttpWatcherHandler extends SimpleChannelInboundHandler<Full
     }
 
     @Override
-    protected void channelRead0(ChannelHandlerContext ctx, FullHttpRequest msg) throws Exception {
+    protected void channelRead0(ChannelHandlerContext ctx, FullHttpResponse msg) throws Exception {
         doneReadingRequest = true;
         if (this.responseCallback != null) {
             this.responseCallback.accept(aggregatedRawResponseBuilder.build());
