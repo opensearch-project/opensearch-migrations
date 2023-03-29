18f6b01d3036cfc27b37a42dc1fb140e2a70ae61
Omar Khasawneh <okhasawn@amazon.com>
2023-03-29 16:34:44 -0500

    Updates - still not ready for review
    Signed-off-by: Omar Khasawneh <okhasawn@amazon.com>

--- TrafficReplayer/pull_then_poll_cw_logs.py
+++ TrafficReplayer/pull_then_poll_cw_logs.py
@@ -5,52 +5,47 @@ import sys
 
 import boto3
 
-DEFAULT_REGION = "us-east-1"
-DEFAULT_LOG_GROUP = sys.argv[1]
-DEFAULT_LOG_STREAM = sys.argv[2]
+TRAFFIC_LOG_GROUP = os.environ["CW_LOG_GROUP_NAME"]
+TRAFFIC_LOG_STREAM = os.environ["CW_LOG_STREAM_NAME"]
 
 
 
 def main():
-    logs_client = boto3.client(
-        'logs',
-        region_name=DEFAULT_REGION
-    )
+    #This is intended to assume the IAM role of the fargate container.
+    logs_client = boto3.client('logs')
 
     messages = []
 
     # Continuously get events and add them to our final list until we see a repeat of the "nextForwardToken"
     # Then script will start checking periodically
-    #print(f"Pulling CW events from {DEFAULT_LOG_GROUP}:{DEFAULT_LOG_STREAM}...")
-    current_response = logs_client.get_log_events(logGroupName=DEFAULT_LOG_GROUP,
-                                                  logStreamName=DEFAULT_LOG_STREAM, startFromHead=True)
+    print(f"Pulling CW events from {TRAFFIC_LOG_GROUP}:{TRAFFIC_LOG_STREAM}...", file=sys.stderr)
+    current_response = logs_client.get_log_events(logGroupName=TRAFFIC_LOG_GROUP,
+                                                  logStreamName=TRAFFIC_LOG_STREAM, startFromHead=True)
     current_events_raw = current_response["events"]
     messages.extend([event["message"] for event in current_events_raw])
     current_token = current_response["nextForwardToken"]
 
-    next_response = logs_client.get_log_events(logGroupName=DEFAULT_LOG_GROUP,
-                                               logStreamName=DEFAULT_LOG_STREAM,
+    next_response = logs_client.get_log_events(logGroupName=TRAFFIC_LOG_GROUP,
+                                               logStreamName=TRAFFIC_LOG_STREAM,
                                                startFromHead=True, nextToken=current_token)
     next_token = next_response["nextForwardToken"]
 
     while current_token != next_token:
-        #print("More events to pull...")
+        print("More events to pull...", file=sys.stderr)
         current_response = next_response
         current_events_raw = current_response["events"]
         messages.extend([event["message"] for event in current_events_raw])
         current_token = current_response["nextForwardToken"]
 
-        next_response = logs_client.get_log_events(logGroupName=DEFAULT_LOG_GROUP,
-                                                   logStreamName=DEFAULT_LOG_STREAM,
+        next_response = logs_client.get_log_events(logGroupName=TRAFFIC_LOG_GROUP,
+                                                   logStreamName=TRAFFIC_LOG_STREAM,
                                                    startFromHead=True, nextToken=current_token)
         next_token = next_response["nextForwardToken"]
 
-    #print(f"Pulled {len(messages)} events")
+    print(f"Pulled {len(messages)} events", file=sys.stderr)
 
-    #print(f"Writing events to file: {DEFAULT_FILE}")
-    #with open(DEFAULT_FILE, 'w') as output_file:
-        print("\n".join(messages))
-        print("\n")
+    print("\n".join(messages))
+    print("\n")
 
     # Now that all currently available CloudWatch events were logged.
     # We start checking for new events every now and then, and append new ones to already existing file, if available
@@ -58,8 +53,8 @@ def main():
     while True:
         messages.clear()
 
-        next_response = logs_client.get_log_events(logGroupName=DEFAULT_LOG_GROUP,
-                                                   logStreamName=DEFAULT_LOG_STREAM,
+        next_response = logs_client.get_log_events(logGroupName=TRAFFIC_LOG_GROUP,
+                                                   logStreamName=TRAFFIC_LOG_STREAM,
                                                    startFromHead=False, nextToken=current_token)
         next_token = next_response["nextForwardToken"]
 
@@ -69,18 +64,17 @@ def main():
             messages.extend([event["message"] for event in current_events_raw])
             current_token = current_response["nextForwardToken"]
 
-            next_response = logs_client.get_log_events(logGroupName=DEFAULT_LOG_GROUP,
-                                                       logStreamName=DEFAULT_LOG_STREAM,
+            next_response = logs_client.get_log_events(logGroupName=TRAFFIC_LOG_GROUP,
+                                                       logStreamName=TRAFFIC_LOG_STREAM,
                                                        startFromHead=False, nextToken=current_token)
             next_token = next_response["nextForwardToken"]
 
             if len(messages):
-                #print(f"Found new log events. Writing events to file: {DEFAULT_FILE} - Checking again in 10 seconds")
-                #with open(DEFAULT_FILE, 'a') as output_file:
-                    print("\n".join(messages))
+                print(f"Found new log events. Writing events - Checking again in 10 seconds", file=sys.stderr)
+                print("\n".join(messages))
 
-        #if not len(messages):
-            #print("No new log events found, sleeping for 10 seconds then checking again")
+        if not len(messages):
+            print("No new log events found, sleeping for 10 seconds then checking again", file=sys.stderr)
 
         time.sleep(10)
 
