1e155c3b004def201db0434f8fd3db10bc7937d6
Tanner Lewis <lewijacn@amazon.com>
2023-04-04 20:09:38 -0400

    MIGRATIONS-1026: Minor updates and OINO box
    Signed-off-by: Tanner Lewis <lewijacn@amazon.com>

--- deployment/cdk/opensearch-service/docker-cw-puller/run_app.sh
+++ deployment/cdk/opensearch-service/docker-cw-puller/run_app.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# Pipe output to STDOUT as well as to port with netcap
+# Pipe output to STDOUT as well as to port with netcat
 python3 -u pull_then_poll_cw_logs.py | tee >(nc -v -l -p 9210)
