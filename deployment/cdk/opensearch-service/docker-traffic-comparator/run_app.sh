1f8564e62b6c0c0124c79b25f4ce62c7ee81508e
Omar Khasawneh <okhasawn@amazon.com>
2023-04-06 10:46:03 -0500

    Updated command and comments to reflect that
    Signed-off-by: Omar Khasawneh <okhasawn@amazon.com>

--- deployment/cdk/opensearch-service/docker-traffic-comparator/run_app.sh
+++ deployment/cdk/opensearch-service/docker-traffic-comparator/run_app.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-nc -l -p 9220 | trafficcomparator -v stream | trafficcomparator stream-report --export-reports DiffReport diffs.log
+nc -l -p 9220 | trafficcomparator -v stream | trafficcomparator stream-report
 
 # IF the user cares about the diffs.log file, then another command to save it somewhere can be inserted here.
-# Otherwise, this part of the above command "--export-reports DiffReport diffs.log" isn't needed, neither is the
-# additional command to save it (see below)
+# AND, the user can use the "--export-reports DiffReport" option of the trafficcomparator
 
 # Next steps: Save the diffs.log somewhere. (Again, if the user needs them)
-# In this case, we still need this part of the command that runs the comparator "--export-reports DiffReport diffs.log".
+# In this case, we will need to add the part of the command mentioned above to save the diff logs e.g
+# "--export-reports DiffReport diffs.log".
 # So it's either you include the full command AND insert another command here to save the file, or neither of them.
 # The command to save the diffs.log can to save it to either a shared volume (which is docker and fargate compatible),
 # S3 bucket, or whichever works and is a more convenient solution for the user.
+# A task was created to add this command (https://opensearch.atlassian.net/browse/MIGRATIONS-1043)
