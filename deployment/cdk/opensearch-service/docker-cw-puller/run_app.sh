30b63a828edc9009efa543b9fca39b4a0dd9746a
Tanner Lewis <lewijacn@amazon.com>
2023-04-07 18:52:31 -0400

    MIGRATIONS-1026: Modify dockerfiles to deploy CW-Puller,TR,TC in stable state
    Signed-off-by: Tanner Lewis <lewijacn@amazon.com>

--- deployment/cdk/opensearch-service/docker-cw-puller/run_app.sh
+++ deployment/cdk/opensearch-service/docker-cw-puller/run_app.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 # Pipe output to STDOUT as well as to port with netcat
-python3 -u pull_then_poll_cw_logs.py | tee >(nc -v -l -p 9210)
+#python3 -u pull_then_poll_cw_logs.py | tee >(nc localhost 9210)
+
+while true
+do
+  python3 -u pull_then_poll_cw_logs.py | tee /dev/stderr | nc localhost 9210
+  >&2 echo "Command has encountered error. Restarting now ..."
+  sleep 5
+done
