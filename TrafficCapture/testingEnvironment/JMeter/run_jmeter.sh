0e35502778ae238d186aff43b460bdc449007570
Omar Khasawneh <okhasawn@amazon.com>
2023-05-18 13:13:47 -0500

    removing a few things I had while testing
    Signed-off-by: Omar Khasawneh <okhasawn@amazon.com>

--- TrafficCapture/testingEnvironment/JMeter/run_jmeter.sh
+++ TrafficCapture/testingEnvironment/JMeter/run_jmeter.sh
@@ -6,8 +6,8 @@ bind_port="$2"
 jars=$(ls extracted/*/*/*.jar | tr \\n :)
 
 cd trafficCaptureProxyServerTest
-#while true
-#do
-java -cp build/libs/trafficCaptureProxyServerTest.jar:"$jars" org.opensearch.migrations.trafficcapture.JMeterLoadTest -p $bind_port -d $domain_name
-#done
+while true
+do
+  java -cp build/libs/trafficCaptureProxyServerTest.jar:"$jars" org.opensearch.migrations.trafficcapture.JMeterLoadTest -p $bind_port -d $domain_name
+done
 
--- TrafficCapture/testingEnvironment/trafficCaptureProxy/run_proxy.sh
+++ TrafficCapture/testingEnvironment/trafficCaptureProxy/run_proxy.sh
@@ -6,4 +6,4 @@ bind_port="$3"
 jars=$(ls extracted/*/*/*.jar | tr \\n :)
 
 cd trafficCaptureProxyServer
-java -Xmx2g -cp build/libs/trafficCaptureProxyServer.jar:"$jars" org.opensearch.migrations.trafficcapture.proxyserver.Main -h $backside_host -p $backside_port -b $bind_port #backside-port $(BACKSIDE_PORT) same for backsidehost and port to bind
+java -cp build/libs/trafficCaptureProxyServer.jar:"$jars" org.opensearch.migrations.trafficcapture.proxyserver.Main -h $backside_host -p $backside_port -b $bind_port #backside-port $(BACKSIDE_PORT) same for backsidehost and port to bind
