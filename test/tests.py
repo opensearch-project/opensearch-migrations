36e558ae9fb6a88b57ca151244628037de209e50
Omar Khasawneh <okhasawn@amazon.com>
2023-06-02 14:12:12 -0500

    Add default value for some environment variables + fixing some linting errors
    Signed-off-by: Omar Khasawneh <okhasawn@amazon.com>

--- test/tests.py
+++ test/tests.py
@@ -8,11 +8,13 @@ import os
 # and will be using the replayer instead, so we're able to check comparator's results once the replayer outputs
 # the triples log file.
 # TODO: Add endpoints to be read from environment variable.
+
+
 class MyTestCase(unittest.TestCase):
     def test_index(self):
-        endpoint1=os.getenv('ENDPOINT_1')
-        endpoint2=os.getenv('ENDPOINT_2')
-        index="my_index"
+        endpoint1 = os.getenv('ENDPOINT_1', 'http://localhost:9200')
+        endpoint2 = os.getenv('ENDPOINT_2')
+        index = "my_index"
         response1 = Operations.create_index(endpoint1, index)
         response2 = Operations.create_index(endpoint2, index)
         self.assert_(response1.status_code == HTTPStatus.OK and response2.status_code == HTTPStatus.OK)
@@ -20,18 +22,16 @@ class MyTestCase(unittest.TestCase):
 
         response1 = Operations.delete_index(endpoint1, index)
         response2 = Operations.delete_index(endpoint2, index)
-        self.assert_(response1.status_code==HTTPStatus.OK and response2.status_code==HTTPStatus.OK)
+        self.assert_(response1.status_code == HTTPStatus.OK and response2.status_code == HTTPStatus.OK)
 
     def test_document(self):
-        endpoint1=os.getenv('ENDPOINT_1')
-        endpoint2=os.getenv('ENDPOINT_2')
-        index="my_index2"
-        doc_id='7'
+        endpoint1 = os.getenv('ENDPOINT_1', 'http://localhost:9200')
+        endpoint2 = os.getenv('ENDPOINT_2')
+        index = "my_index2"
+        doc_id = '7'
         response1 = Operations.create_index(endpoint1, index)
         response2 = Operations.create_index(endpoint2, index)
         self.assert_(response1.status_code == HTTPStatus.OK and response2.status_code == HTTPStatus.OK)
-
-
 
         response1 = Operations.create_document(endpoint1, index, doc_id)
         response2 = Operations.create_document(endpoint2, index, doc_id)
@@ -45,14 +45,16 @@ class MyTestCase(unittest.TestCase):
         response1 = Operations.delete_index(endpoint1, index)
         response2 = Operations.delete_index(endpoint2, index)
         # TODO: check comparator's results here, and add more logging to know where exactly the test fails, if it does.
-        self.assert_(response1.status_code==HTTPStatus.OK and response2.status_code==HTTPStatus.OK)
+        self.assert_(response1.status_code == HTTPStatus.OK and response2.status_code == HTTPStatus.OK)
 
     def test_unsupported_transformation(self):
 
         pass
 
     def test_supported_transformation(self):
+
         pass
+
 
 if __name__ == '__main__':
     unittest.main()
