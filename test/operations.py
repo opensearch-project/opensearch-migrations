2e69a28f08acf60965ad891f51e334695cae8e61
Omar Khasawneh <okhasawn@amazon.com>
2023-06-02 01:55:23 -0500

    Adds some tests
    Signed-off-by: Omar Khasawneh <okhasawn@amazon.com>

--- test/operations.py
+++ test/operations.py
@@ -1,41 +1,57 @@
-import requests, json
+from http import HTTPStatus
+import requests
+import json
+import os
+
 
 class Operations:
     @staticmethod
     def create_index(endpoint, index_name, auth=None, data=None):
         response = requests.put(f'{endpoint}/{index_name}', auth=auth)
-        if response.status_code != 200:
+        if response.status_code != HTTPStatus.OK:
             print('Failed to create index')
             print(response.text)
         else:
             print('Created index successfully')
             print(response.text)
 
-        pass
+        return response
+
+    @staticmethod
+    def check_index(endpoint, index_name, auth=None, data=None):
+        response = requests.head(f'{endpoint}/{index_name}', auth=auth)
+        if response.status_code != HTTPStatus.OK:
+            print('Failed to create index')
+            print(response.text)
+        else:
+            print('Created index successfully')
+            print(response.text)
+
+        return response
 
     @staticmethod
     def delete_index(endpoint, index_name, auth=None, data=None):
         response = requests.delete(f'{endpoint}/{index_name}', auth=auth)
-        if response.status_code != 200:
+        if response.status_code != HTTPStatus.OK:
             print('Failed to delete index')
             print(response.text)
         else:
             print('Deleted index successfully')
             print(response.text)
 
-    pass
+        return response
 
     @staticmethod
     def delete_document(endpoint, index_name, doc_id, auth=None, data=None):
         response = requests.delete(f'{endpoint}/{index_name}/_doc/{doc_id}', auth=auth)
-        if response.status_code != 200:
+        if response.status_code != HTTPStatus.OK:
             print('Failed to delete document')
             print(response.text)
         else:
             print('Deleted document successfully')
             print(response.text)
 
-    pass
+        return response
 
     @staticmethod
     def create_document(endpoint, index_name, doc_id, auth=None):
@@ -48,30 +64,30 @@ class Operations:
 
         response = requests.put(url, headers=headers, data=json.dumps(document), auth=auth)
 
-        if response.status_code != 201:
+        if response.status_code != HTTPStatus.CREATED:
             print('Failed to create document')
             print(response.text)
         else:
             print('Created document successfully')
             print(response.text)
-        pass
 
+        return response
 
 
 def main():
 
-    username = '*'  # Enter master username and password
-    password = '*'
+    username = os.getenv('USERNAME')
+    password = os.getenv('PASSWORD')
+    endpoint = os.getenv('ENDPOINT') # Dont forget port number.
+
     auth = (username, password)
-    endpoint = '*:443'  # Replace * with domain endpoint
     index = 'my_index'
     doc_id = '7'
 
-    Operations.create_index(endpoint, index, auth)
-    Operations.create_document(endpoint, index, doc_id, auth)
-    Operations.delete_document(endpoint, index, doc_id, auth)
-    Operations.delete_index(endpoint, index, auth)
-
+    response1 = Operations.create_index(endpoint, index)
+    response2 = Operations.create_document(endpoint, index, doc_id, auth)
+    response3 = Operations.delete_document(endpoint, index, doc_id, auth)
+    response4 = Operations.delete_index(endpoint, index)
 
 
 if __name__ == "__main__":
