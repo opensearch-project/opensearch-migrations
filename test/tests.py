e59dedfdeefb06a28a68830fc3fcc491827a3e1e
Omar Khasawneh <okhasawn@amazon.com>
2023-06-05 13:40:11 -0500

    Addressing feedback + Actually using the proxy and replayer
    Signed-off-by: Omar Khasawneh <okhasawn@amazon.com>

--- test/tests.py
+++ test/tests.py
@@ -1,59 +1,103 @@
-from operations import Operations
+from operations import create_index, check_index, create_document, delete_document, get_document, delete_index
 from http import HTTPStatus
 import unittest
 import os
 
-# This code is probably existing as a placeholder only for testing purposes,
-# final code here will be using comparator's results. Also, won't be sending request directly to target cluster,
-# and will be using the replayer instead, so we're able to check comparator's results once the replayer outputs
-# the triples log file.
-# TODO: Add endpoints to be read from environment variable.
+from time import sleep
 
 
-class MyTestCase(unittest.TestCase):
-    def test_index(self):
-        endpoint1 = os.getenv('ENDPOINT_1', 'http://localhost:9200')
-        endpoint2 = os.getenv('ENDPOINT_2')
+# Tests will say which line the test failed at and what was the result of the execution, but better logging can be done.
+
+
+class E2ETests(unittest.TestCase):
+    def test_cleanup(self):
+        # This is not necessarily a test, but if a test fails for whatever reason, it can cause next tests to fail even
+        # though they could've passed, due to a previous test e.g failing at a point where it creates the index, but it
+        # never gets to the point where it gets deleted.
+        # The final state of the script should be cleaning up after each test run.
+        source_endpoint = os.getenv('SOURCE_ENDPOINT', 'https://localhost:9200')
+        username = os.getenv('username', 'admin')
+        password = os.getenv('password', 'admin')
+        auth = (username, password)
         index = "my_index"
-        response1 = Operations.create_index(endpoint1, index)
-        response2 = Operations.create_index(endpoint2, index)
-        self.assert_(response1.status_code == HTTPStatus.OK and response2.status_code == HTTPStatus.OK)
-        # TODO: check comparator's results here, and add more logging to know where exactly the test fails, if it does.
+        doc_id = '7'
+        delete_index(source_endpoint, index, auth)
+        delete_document(source_endpoint, index, doc_id, auth)
 
-        response1 = Operations.delete_index(endpoint1, index)
-        response2 = Operations.delete_index(endpoint2, index)
-        self.assert_(response1.status_code == HTTPStatus.OK and response2.status_code == HTTPStatus.OK)
+    def test_index(self):
+        source_endpoint = os.getenv('SOURCE_ENDPOINT', 'https://localhost:9200')
+        target_endpoint = os.getenv('TARGET_ENDPOINT', 'https://localhost:29200')
+        username = os.getenv('username', 'admin')
+        password = os.getenv('password', 'admin')
+        auth = (username, password)
+        index = "my_index"
+
+        source_response = create_index(source_endpoint, index, auth)
+        self.assertEqual(source_response.status_code, HTTPStatus.OK)
+
+        target_response = check_index(target_endpoint, index, auth)
+        self.assertEqual(target_response.status_code, HTTPStatus.OK)
+
+        # TODO: check comparator's results here.
+
+        source_response = delete_index(source_endpoint, index, auth)
+        self.assertEqual(source_response.status_code, HTTPStatus.OK)
+        # Add a stall here maybe? Sometimes the check index function is performed before the delete request is replayed
+        # on the target cluster, so the check will find the index and return a code 200 instead of 404.
+
+        # TODO: check comparator's results here.
+
+        sleep(3)
+        target_response = check_index(target_endpoint, index, auth)
+        self.assertEqual(target_response.status_code, HTTPStatus.NOT_FOUND)
 
     def test_document(self):
-        endpoint1 = os.getenv('ENDPOINT_1', 'http://localhost:9200')
-        endpoint2 = os.getenv('ENDPOINT_2')
-        index = "my_index2"
+        source_endpoint = os.getenv('SOURCE_ENDPOINT', 'https://localhost:9200')
+        target_endpoint = os.getenv('TARGET_ENDPOINT', 'https://localhost:29200')
+        username = os.getenv('username', 'admin')
+        password = os.getenv('password', 'admin')
+        auth = (username, password)
+        index = "my_index"
         doc_id = '7'
-        response1 = Operations.create_index(endpoint1, index)
-        response2 = Operations.create_index(endpoint2, index)
-        self.assert_(response1.status_code == HTTPStatus.OK and response2.status_code == HTTPStatus.OK)
 
-        response1 = Operations.create_document(endpoint1, index, doc_id)
-        response2 = Operations.create_document(endpoint2, index, doc_id)
-        # TODO: check comparator's results here, and add more logging to know where exactly the test fails, if it does.
-        self.assert_(response1.status_code == HTTPStatus.CREATED and response2.status_code == HTTPStatus.CREATED)
+        source_response = create_index(source_endpoint, index, auth)
+        self.assertEqual(source_response.status_code, HTTPStatus.OK)
 
-        response1 = Operations.delete_document(endpoint1, index, doc_id)
-        response2 = Operations.delete_document(endpoint2, index, doc_id)
-        self.assert_(response1.status_code == HTTPStatus.OK and response2.status_code == HTTPStatus.OK)
+        target_response = check_index(target_endpoint, index, auth)
+        self.assertEqual(target_response.status_code, HTTPStatus.OK)
 
-        response1 = Operations.delete_index(endpoint1, index)
-        response2 = Operations.delete_index(endpoint2, index)
-        # TODO: check comparator's results here, and add more logging to know where exactly the test fails, if it does.
-        self.assert_(response1.status_code == HTTPStatus.OK and response2.status_code == HTTPStatus.OK)
+        # TODO: check comparator's results here.
+
+        source_response = create_document(source_endpoint, index, doc_id, auth)
+        self.assertEqual(source_response.status_code, HTTPStatus.CREATED)
+        # TODO: check comparator's results here.
+        # TODO: compare two documents below instead of just confirming they exist
+
+        source_response = get_document(source_endpoint, index, doc_id, auth)
+        self.assertEqual(source_response.status_code, HTTPStatus.OK)
+
+        target_response = get_document(target_endpoint, index, doc_id, auth)
+        self.assertEqual(target_response.status_code, HTTPStatus.OK)
+
+        source_response = delete_document(source_endpoint, index, doc_id, auth)
+        self.assertEqual(source_response.status_code, HTTPStatus.OK)
+
+        target_response = get_document(target_endpoint, index, doc_id, auth)
+        self.assertEqual(target_response.status_code, HTTPStatus.NOT_FOUND)
+
+        source_response = delete_index(source_endpoint, index, auth)
+        self.assertEqual(source_response.status_code, HTTPStatus.OK)
+
+        # TODO: check comparator's results here.
+
+        target_response = check_index(target_endpoint, index, auth)
+        self.assertEqual(target_response.status_code, HTTPStatus.NOT_FOUND)
 
     def test_unsupported_transformation(self):
-
-        pass
+        self.assertTrue(True)
 
     def test_supported_transformation(self):
-
-        pass
+        self.assertTrue(True)
 
 
 if __name__ == '__main__':
