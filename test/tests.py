01e606b4402beb3c77d47120f53a017f7d5c389d
Omar Khasawneh <okhasawn@amazon.com>
2023-06-07 10:21:41 -0500

    More thorough testing + Adding setup and teardown for tests
    Signed-off-by: Omar Khasawneh <okhasawn@amazon.com>

--- test/tests.py
+++ test/tests.py
@@ -11,42 +11,59 @@ from time import sleep
 
 
 class E2ETests(unittest.TestCase):
-    def test_cleanup(self):
+    def setUp(self):
         # This is not necessarily a test, but if a test fails for whatever reason, it can cause next tests to fail even
         # though they could've passed, due to a previous test e.g failing at a point where it creates the index, but it
         # never gets to the point where it gets deleted.
         # The final state of the script should be cleaning up after each test run.
-        source_endpoint = os.getenv('SOURCE_ENDPOINT', 'https://localhost:9200')
+        proxy_endpoint = os.getenv('SOURCE_ENDPOINT', 'https://localhost:9200')
         username = os.getenv('username', 'admin')
         password = os.getenv('password', 'admin')
         auth = (username, password)
         index = "my_index"
         doc_id = '7'
-        delete_index(source_endpoint, index, auth)
-        delete_document(source_endpoint, index, doc_id, auth)
+        delete_index(proxy_endpoint, index, auth)
+        delete_document(proxy_endpoint, index, doc_id, auth)
+
+    def tearDown(self):
+        # This is not necessarily a test, but if a test fails for whatever reason, it can cause next tests to fail even
+        # though they could've passed, due to a previous test e.g failing at a point where it creates the index, but it
+        # never gets to the point where it gets deleted.
+        # The final state of the script should be cleaning up after each test run.
+        proxy_endpoint = os.getenv('SOURCE_ENDPOINT', 'https://localhost:9200')
+        username = os.getenv('username', 'admin')
+        password = os.getenv('password', 'admin')
+        auth = (username, password)
+        index = "my_index"
+        doc_id = '7'
+        delete_index(proxy_endpoint, index, auth)
+        delete_document(proxy_endpoint, index, doc_id, auth)
 
     def test_index(self):
-        source_endpoint = os.getenv('SOURCE_ENDPOINT', 'https://localhost:9200')
+        proxy_endpoint = os.getenv('PROXY_ENDPOINT', 'https://localhost:9200')
+        source_endpoint = os.getenv('SOURCE_ENDPOINT', 'http://localhost:19200')
         target_endpoint = os.getenv('TARGET_ENDPOINT', 'https://localhost:29200')
         username = os.getenv('username', 'admin')
         password = os.getenv('password', 'admin')
         auth = (username, password)
         index = "my_index"
 
-        source_response = create_index(source_endpoint, index, auth)
-        self.assertEqual(source_response.status_code, HTTPStatus.OK)
+        proxy_response = create_index(proxy_endpoint, index, auth)
+        self.assertEqual(proxy_response.status_code, HTTPStatus.OK)
 
         sleep(3)
 
         target_response = check_index(target_endpoint, index, auth)
         self.assertEqual(target_response.status_code, HTTPStatus.OK)
+        source_response = check_index(source_endpoint, index, auth)
+        self.assertEqual(source_response.status_code, HTTPStatus.OK)
 
         # TODO: check comparator's results here.
 
         sleep(3)
 
-        source_response = delete_index(source_endpoint, index, auth)
-        self.assertEqual(source_response.status_code, HTTPStatus.OK)
+        proxy_response = delete_index(proxy_endpoint, index, auth)
+        self.assertEqual(proxy_response.status_code, HTTPStatus.OK)
         # Add a stall here maybe? Sometimes the check index function is performed before the delete request is replayed
         # on the target cluster, so the check will find the index and return a code 200 instead of 404.
 
@@ -55,9 +72,12 @@ class E2ETests(unittest.TestCase):
         sleep(3)
         target_response = check_index(target_endpoint, index, auth)
         self.assertEqual(target_response.status_code, HTTPStatus.NOT_FOUND)
+        source_response = check_index(source_endpoint, index, auth)
+        self.assertEqual(source_response.status_code, HTTPStatus.NOT_FOUND)
 
     def test_document(self):
-        source_endpoint = os.getenv('SOURCE_ENDPOINT', 'https://localhost:9200')
+        proxy_endpoint = os.getenv('PROXY_ENDPOINT', 'https://localhost:9200')
+        source_endpoint = os.getenv('SOURCE_ENDPOINT', 'http://localhost:19200')
         target_endpoint = os.getenv('TARGET_ENDPOINT', 'https://localhost:29200')
         username = os.getenv('username', 'admin')
         password = os.getenv('password', 'admin')
@@ -65,16 +85,18 @@ class E2ETests(unittest.TestCase):
         index = "my_index"
         doc_id = '7'
 
-        source_response = create_index(source_endpoint, index, auth)
-        self.assertEqual(source_response.status_code, HTTPStatus.OK)
+        proxy_response = create_index(proxy_endpoint, index, auth)
+        self.assertEqual(proxy_response.status_code, HTTPStatus.OK)
 
         target_response = check_index(target_endpoint, index, auth)
         self.assertEqual(target_response.status_code, HTTPStatus.OK)
+        source_response = check_index(source_endpoint, index, auth)
+        self.assertEqual(source_response.status_code, HTTPStatus.OK)
 
         # TODO: check comparator's results here.
 
-        source_response = create_document(source_endpoint, index, doc_id, auth)
-        self.assertEqual(source_response.status_code, HTTPStatus.CREATED)
+        proxy_response = create_document(proxy_endpoint, index, doc_id, auth)
+        self.assertEqual(proxy_response.status_code, HTTPStatus.CREATED)
         # TODO: check comparator's results here.
         # TODO: compare two documents below instead of just confirming they exist
 
@@ -88,20 +110,23 @@ class E2ETests(unittest.TestCase):
         target_content = get_document(target_endpoint, index, doc_id, auth)
         self.assertEqual(source_content, target_content)
 
-        source_response = delete_document(source_endpoint, index, doc_id, auth)
+        proxy_response = delete_document(proxy_endpoint, index, doc_id, auth)
         self.assertEqual(source_response.status_code, HTTPStatus.OK)
 
         target_response = check_document(target_endpoint, index, doc_id, auth)
-
         self.assertEqual(target_response.status_code, HTTPStatus.NOT_FOUND)
+        source_response = check_document(source_endpoint, index, doc_id, auth)
+        self.assertEqual(source_response.status_code, HTTPStatus.NOT_FOUND)
 
-        source_response = delete_index(source_endpoint, index, auth)
-        self.assertEqual(source_response.status_code, HTTPStatus.OK)
+        proxy_response = delete_index(proxy_endpoint, index, auth)
+        self.assertEqual(proxy_response.status_code, HTTPStatus.OK)
 
         # TODO: check comparator's results here.
 
         target_response = check_index(target_endpoint, index, auth)
         self.assertEqual(target_response.status_code, HTTPStatus.NOT_FOUND)
+        source_response = check_index(source_endpoint, index, auth)
+        self.assertEqual(source_response.status_code, HTTPStatus.NOT_FOUND)
 
     def test_unsupported_transformation(self):
         self.assertTrue(True)
