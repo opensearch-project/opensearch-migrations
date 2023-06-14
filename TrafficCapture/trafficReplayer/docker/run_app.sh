23119ac53d6bffc4b1989fd957334a838ef3e4f6
Tanner Lewis <lewijacn@amazon.com>
2023-06-14 16:45:43 -0400

    MIGRATIONS-1134: Remove unnecessary changes
    Signed-off-by: Tanner Lewis <lewijacn@amazon.com>

--- TrafficCapture/trafficReplayer/docker/run_app.sh
+++ TrafficCapture/trafficReplayer/docker/run_app.sh
@@ -8,7 +8,7 @@ target_endpoint="$1"
 while true
 do
   # Verify connection with TC can be made before disconnecting via 1 second timeout and moving forward
-  while ! nc -v -w 1 traffic-comparator 9220
+  while ! nc -v -w 1 localhost 9220
   do
     >&2 echo 'Attempting to connect to Traffic Comparator...'
     sleep 2
@@ -22,7 +22,7 @@ do
   # These commands look a bit backwards but ultimately will receive input from CW-Puller via netcat and then
   # process with the Replayer jar before ultimately printing the produced triples to stdout via tee and sending to TC via netcat
   # Note: Unbuffer has not been verified to actually be effective here. stdbuf was ruled out for its caveat with tee (https://linux.die.net/man/1/stdbuf)
-  unbuffer tail -f triples.log | tee /dev/stderr | nc -v traffic-comparator 9220 &
+  unbuffer tail -f triples.log | tee /dev/stderr | nc -v localhost 9220 &
   nc -v -l -p 9210 | java -jar trafficReplayer-uber.jar "$target_endpoint" -o triples.log --insecure
 
   rm triple.log
