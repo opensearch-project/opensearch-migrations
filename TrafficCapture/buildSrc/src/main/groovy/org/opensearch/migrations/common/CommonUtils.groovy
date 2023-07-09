e2e7ef27d281736941d2a1bbaf55924fb936f2ab
Omar Khasawneh <okhasawn@amazon.com>
2023-07-09 18:22:59 -0500

    Moving more common functions to commonUtils
    Signed-off-by: Omar Khasawneh <okhasawn@amazon.com>

--- TrafficCapture/buildSrc/src/main/groovy/org/opensearch/migrations/common/CommonUtils.groovy
+++ TrafficCapture/buildSrc/src/main/groovy/org/opensearch/migrations/common/CommonUtils.groovy
@@ -3,6 +3,7 @@ package org.opensearch.migrations.common
 import org.gradle.api.tasks.Copy
 import org.gradle.api.Project
 import com.bmuschko.gradle.docker.tasks.image.Dockerfile
+import com.bmuschko.gradle.docker.tasks.image.DockerBuildImage
 
 class CommonUtils {
     static def calculateDockerHash(def projectName, def project) {
@@ -21,22 +22,59 @@ class CommonUtils {
         return digest.digest().encodeHex().toString()
     }
 
-    static def copyArtifact(String projectName, Project project) {
+    static def copyArtifact(Project project, String projectName) {
         def dockerBuildDir = "build/docker/${projectName}"
         def artifactsDir = "${dockerBuildDir}/jars"
         project.task("copyArtifact_${projectName}", type: Copy) {
             dependsOn ":${projectName}:build"
             dependsOn ":${projectName}:jar"
+            if (projectName == "trafficCaptureProxyServerTest") {
+                include "*.properties"
+                def jmeterPropertiesFile = project.file("src/main/docker/${projectName}/jmeter.properties")
+                if (jmeterPropertiesFile.exists()) {
+                    from jmeterPropertiesFile.absolutePath
+                    into "${dockerBuildDir}"
+                }
+            }
             from { project.project(":${projectName}").configurations.findByName("runtimeClasspath").files }
             from { project.project(":${projectName}").tasks.getByName('jar') }
             into artifactsDir
             include "*.jar"
             duplicatesStrategy = 'WARN'
-            if (projectName == "trafficCaptureProxyServerTest") {
-                include "*.properties"
-                from project.file("src/main/docker/${projectName}/jmeter.properties").absolutePath
-                into "${dockerBuildDir}"
+        }
+    }
+
+    static def createDockerfile(Project project, String projectName, Map<String, String> baseImageProjectOverrides, Map<String, String> dockerFilesForExternalServices) {
+        def dockerBuildDir = "build/docker/${projectName}"
+        project.task("createDockerfile_${projectName}", type: Dockerfile) {
+            destFile = project.file("${dockerBuildDir}/Dockerfile")
+            dependsOn "copyArtifact_${projectName}"
+            def baseImageOverrideProjectName = baseImageProjectOverrides.get(projectName)
+            if (baseImageOverrideProjectName) {
+                def dependentDockerImageName = dockerFilesForExternalServices.get(baseImageOverrideProjectName)
+                def hashNonce = CommonUtils.calculateDockerHash(baseImageOverrideProjectName, project)
+                from "migrations/${dependentDockerImageName}:${hashNonce}"
+                dependsOn "buildDockerImage_${baseImageOverrideProjectName}"
+                runCommand("sed -i -e \"s|mirrorlist=|#mirrorlist=|g\" /etc/yum.repos.d/CentOS-* ;  sed -i -e \"s|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g\" /etc/yum.repos.d/CentOS-*")
+                runCommand("yum -y install nmap-ncat")
+            } else {
+                from 'openjdk:11-jre'
+                if (projectName == "trafficCaptureProxyServerTest") {
+                    instruction 'COPY jmeter.properties /jmeter.properties'
+                }
+                runCommand("apt-get update && apt-get install -y netcat")
             }
+
+            copyFile("jars", "/jars")
+            // can't set the environment variable from the runtimeClasspath because the Dockerfile is
+            // constructed in the configuration phase and the classpath won't be realized until the
+            // execution phase.  Therefore, we need to have docker run the command to resolve the classpath
+            // and it's simplest to pack that up into a helper script.
+            runCommand("printf \"#!/bin/sh\\njava -cp `echo /jars/*.jar | tr \\   :` \\\"\\\$@\\\" \" > /runJavaWithClasspath.sh");
+            runCommand("chmod +x /runJavaWithClasspath.sh")
+            // container stay-alive
+            defaultCommand('tail', '-f', '/dev/null')
+            //defaultCommand('/runJavaWithClasspath.sh', '...')
         }
     }
 }
