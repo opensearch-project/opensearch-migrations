21ae9114c77350b04772decbe3f2aad5df97f216
Omar Khasawneh <okhasawn@amazon.com>
2023-07-25 02:05:33 -0500

    One very simple test with no payload
    Signed-off-by: Omar Khasawneh <okhasawn@amazon.com>

--- TrafficCapture/trafficReplayer/src/main/java/org/opensearch/migrations/replay/SigV4Signer.java
+++ TrafficCapture/trafficReplayer/src/main/java/org/opensearch/migrations/replay/SigV4Signer.java
@@ -1,34 +1,90 @@
 package org.opensearch.migrations.replay;
 
+import org.opensearch.migrations.replay.ReplayUtils;
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.net.URI;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
+import org.opensearch.migrations.replay.datahandlers.http.HttpJsonMessageWithFaultingPayload;
 import org.opensearch.migrations.replay.datahandlers.http.IHttpMessageMetadata;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.auth.signer.Aws4Signer;
+import software.amazon.awssdk.auth.signer.params.Aws4SignerParams;
+import software.amazon.awssdk.http.SdkHttpFullRequest;
+import software.amazon.awssdk.http.SdkHttpMethod;
+import software.amazon.awssdk.regions.Region;
+
+
 
 public class SigV4Signer {
     public IHttpMessageMetadata messageMetadata;
-    public String processedPayload;
-    public Map<String, String> signedHeaders;
-    public SigV4Signer(IHttpMessageMetadata messageMetadata){ // add defaultcredentials.
-        this.messageMetadata = messageMetadata;
-        this.signedHeaders = new HashMap<>();
+
+    public DefaultCredentialsProvider credentialsProvider;
+    public Aws4Signer signer;
+    public SdkHttpFullRequest.Builder httpRequestBuilder;
+    public StringBuilder processedPayload;
+    public HttpJsonMessageWithFaultingPayload message;
+
+
+    public SigV4Signer(HttpJsonMessageWithFaultingPayload message, DefaultCredentialsProvider credentialsProvider) {
+        this.message = message;
+        this.credentialsProvider = credentialsProvider;
+        this.credentialsProvider = DefaultCredentialsProvider.create();
+        this.signer = Aws4Signer.create();
+        var host = ((List<String>) (message.headers().get("HOST"))).get(0);
+        this.httpRequestBuilder = SdkHttpFullRequest.builder()
+                .method(SdkHttpMethod.fromValue(this.message.method()))
+                .uri(URI.create(this.message.uri()))
+                .protocol(this.message.protocol())
+                .host(host)
+                .appendHeader("host", host);
+        this.processedPayload = new StringBuilder();
+
     }
 
     public void processNextPayload(ByteBuf payloadChunk) {
 
-        this.processedPayload = payloadChunk.toString(StandardCharsets.UTF_8).toUpperCase();
+        // append new chunks to the existing payload
+        this.processedPayload.append(payloadChunk.toString(StandardCharsets.UTF_8));
+
+        // Request builder action here?
     }
 
-    public Map<String, String> getSignatureheaders() {
-        // Simple placeholder logic.
-        // This should return the signed header.
-        if (this.processedPayload != null) {
-            this.signedHeaders.put("Signature", this.processedPayload);
+    public Map<String, List<String>> getSignatureheaders(String service, String region) {
+
+        // #TODO: NIT: Aws4Signer signer = Aws4Signer.create(); Should Aws4Sginer variable exist here or in the class itself and call it as this.signer?
+
+        InputStream payloadStream = ReplayUtils.byteArraysToInputStream(
+                Collections.singletonList(this.processedPayload.toString().getBytes(StandardCharsets.UTF_8))
+        );
+        this.httpRequestBuilder.contentStreamProvider(() -> payloadStream);
+
+        SdkHttpFullRequest request = this.httpRequestBuilder.build(); // Is this the right time?
+
+        SdkHttpFullRequest signedRequest = this.signer.sign(request, Aws4SignerParams.builder()
+                .signingName(service)
+                .signingRegion(Region.of(region))
+                .awsCredentials(credentialsProvider.resolveCredentials())
+                .build());
+
+
+        System.out.println("Tried to sign: " + request.method().name() + ", " + request.getUri() +
+                "region: " + region + " serviceName:" + service +
+                " headers: ");
+        for (var header : request.headers().entrySet()) {
+            System.out.println(header.getKey() + ":" + header.getValue());
         }
 
-        return this.signedHeaders;
+        return signedRequest.headers();
+
     }
 }
+
+
--- TrafficCapture/trafficReplayer/src/test/java/org/opensearch/migrations/replay/SigV4SignerTest.java
+++ TrafficCapture/trafficReplayer/src/test/java/org/opensearch/migrations/replay/SigV4SignerTest.java
@@ -1,47 +1,97 @@
 package org.opensearch.migrations.replay;
 
-import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.handler.codec.http.DefaultFullHttpRequest;
+import io.netty.handler.codec.http.FullHttpRequest;
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.opensearch.migrations.replay.datahandlers.http.HttpJsonMessageWithFaultingPayload;
-
 import java.nio.charset.Charset;
+import io.netty.buffer.ByteBuf;
+import org.opensearch.migrations.replay.datahandlers.NettyPacketToHttpConsumer;
+import org.opensearch.migrations.replay.datahandlers.http.HttpJsonMessageWithFaultingPayload;
+import org.opensearch.migrations.replay.datahandlers.http.ListKeyAdaptingCaseInsensitiveHeadersMap;
+import org.opensearch.migrations.replay.datahandlers.http.StrictCaseInsensitiveHttpHeadersMap;
+import software.amazon.awssdk.auth.credentials.AwsCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.http.SdkHttpFullRequest;
+import software.amazon.awssdk.http.SdkHttpMethod;
 import java.util.Map;
+import io.netty.handler.codec.http.HttpMethod;
+import io.netty.handler.codec.http.HttpVersion;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.DefaultFullHttpRequest;
+import io.netty.handler.codec.http.HttpHeaderNames;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SigV4SignerTest {
 
-    private SigV4Signer sigV4Signer;
-    private HttpJsonMessageWithFaultingPayload testMetadata;
+    public static final String HOSTNAME_STRING = "YOUR_AOSS_ENDPOINT_WITHOUT_THE_PROTOCOL";
+    private SigV4Signer signer;
+    private static final String service = "aoss";
+    private static final String region = "us-east-1";
+
 
     @BeforeEach
-    void setUp() {
-        testMetadata = new HttpJsonMessageWithFaultingPayload();
-        sigV4Signer = new SigV4Signer(testMetadata);
+    void setup() {
+
     }
 
+    @SneakyThrows
     @Test
-    void processNextPayload() {
-        ByteBuf payload = Unpooled.copiedBuffer("Original payload", Charset.defaultCharset());
-        sigV4Signer.processNextPayload(payload);
+    void testEmptyBodySignedRequestTest() {
+        var nettyHandler = new NettyPacketToHttpConsumer(new NioEventLoopGroup(), new URI("YOUR_AOSS_ENDPOINT:443"),
+                null, "");
 
-        // Testing if expected is uppercase only because that's what the current state of signing looks like.
-        String expectedPayload = "ORIGINAL PAYLOAD";
+        HttpJsonMessageWithFaultingPayload msg = new HttpJsonMessageWithFaultingPayload();
+        msg.setMethod("PUT");
+        msg.setUri("/ok92ok92");
+        msg.setProtocol("https");
+        var headers = new ListKeyAdaptingCaseInsensitiveHeadersMap(new StrictCaseInsensitiveHttpHeadersMap());
+        headers.put("Host", HOSTNAME_STRING);
+        msg.setHeaders(headers);
 
-        Map<String, String> signatureHeaders = sigV4Signer.getSignatureheaders();
-        assertEquals(expectedPayload, signatureHeaders.get("Signature"));
-    }
+        var key_id = System.getenv("AWS_ACCESS_KEY_ID");
+        var secret_key = System.getenv("AWS_SECRET_ACCESS_KEY");
+        var token = System.getenv("AWS_SESSION_TOKEN");
+        System.setProperty("aws.accessKeyId", key_id);
+        System.setProperty("aws.secretAccessKey", secret_key);
+        System.setProperty("aws.sessionToken", token);
+        DefaultCredentialsProvider credentialsProvider = DefaultCredentialsProvider.create();
+        var resolvedValue = credentialsProvider.resolveCredentials();
 
-    @Test
-    void getSignatureHeaders() {
-        String processedPayload = "PROCESSED PAYLOAD";
-        // Setting payload in headers
-        sigV4Signer.getSignatureheaders().put("Signature", processedPayload);
+        signer = new SigV4Signer(msg, credentialsProvider);
+        Map<String, List<String>> signedHeaders = signer.getSignatureheaders(service, region);
 
-        Map<String, String> result = sigV4Signer.getSignatureheaders();
-        // Were headers succesfully retrieved?
-        assertEquals(Map.of("Signature", processedPayload), result);
+        var headersAsAString =  msg.headers().entrySet().stream()
+                .map(kvp->String.format("%1$s: %2$s", kvp.getKey(), ((List<String>)kvp.getValue()).get(0)))
+                .collect(Collectors.joining("\n"));
+
+        var signedHeadersString = signedHeaders.entrySet().stream()
+                .map(kvp -> String.format("%s: %s", kvp.getKey(), kvp.getValue().get(0)))
+                .collect(Collectors.joining("\n"));
+
+        String mergedHeadersString = headersAsAString + "\n" + signedHeadersString;
+
+        byte[] arr = new byte[9999]; // YOUR REQUEST AS A BYTE ARRAY
+        var requestString = String.format("%1$s %2$s %3$s\n" +
+                        "%4$s\n\n",
+                msg.method(), msg.uri(), HttpVersion.HTTP_1_1,
+                mergedHeadersString);
+        arr = requestString.getBytes();
+
+        nettyHandler.consumeBytes(arr);
+        AggregatedRawResponse response = nettyHandler.finalizeRequest().get();
+        var uglyAnswer = response.responsePackets;
+        System.out.println(uglyAnswer);
     }
 }
