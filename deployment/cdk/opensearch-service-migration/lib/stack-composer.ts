c43de9f1003b8e0cfb6deff2cb2ddadca71b56a8
Brian Presley <bjpres@amazon.com>
2023-08-17 15:15:10 -0500

    Revert "MIGRATIONS-1200 - Remove historical capture CDK stack"
    This reverts commit 4c9309261291acee697b83633a3cd8930d05a937.

--- deployment/cdk/opensearch-service-migration/lib/stack-composer.ts
+++ deployment/cdk/opensearch-service-migration/lib/stack-composer.ts
@@ -7,6 +7,7 @@ import {AnyPrincipal, Effect, PolicyStatement} from "aws-cdk-lib/aws-iam";
 import * as defaultValuesJson from "../default-values.json"
 import {NetworkStack} from "./network-stack";
 import {MigrationAssistanceStack} from "./migration-assistance-stack";
+import {HistoricalCaptureStack} from "./historical-capture-stack";
 import {MSKUtilityStack} from "./msk-utility-stack";
 
 export interface StackPropsExt extends StackProps {
@@ -58,6 +59,9 @@ export class StackComposer {
         const migrationAssistanceEnabled = getContextForType('migrationAssistanceEnabled', 'boolean')
         const mskARN = getContextForType('mskARN', 'string')
         const mskEnablePublicEndpoints = getContextForType('mskEnablePublicEndpoints', 'boolean')
+        const sourceClusterEndpoint = getContextForType('sourceClusterEndpoint', 'string')
+        const historicalCaptureEnabled = getContextForType('historicalCaptureEnabled', 'boolean')
+        const logstashConfigFilePath = getContextForType('logstashConfigFilePath', 'string')
 
         if (!domainName) {
             throw new Error("Domain name is not present and is a required field")
@@ -182,6 +186,25 @@ export class StackComposer {
             mskUtilityStack.addDependency(migrationStack)
             this.stacks.push(mskUtilityStack)
         }
+
+        // Currently, placing a requirement on a VPC for a historical capture stack but this can be revisited
+        // Note: Future work to provide orchestration between historical capture and migration assistance as the current
+        // state will potentially have both stacks trying to add the same data
+        if (historicalCaptureEnabled && networkStack) {
+            const historicalCaptureStack = new HistoricalCaptureStack(scope, "historicalCaptureStack", {
+                vpc: networkStack.vpc,
+                logstashConfigFilePath: logstashConfigFilePath,
+                sourceEndpoint: sourceClusterEndpoint,
+                targetEndpoint: opensearchStack.domainEndpoint,
+                stackName: `OSServiceHistoricalCDKStack-${stage}-${region}`,
+                description: "This stack contains resources to assist migrating historical data to an OpenSearch Service domain",
+                ...props,
+            })
+
+            historicalCaptureStack.addDependency(opensearchStack)
+            this.stacks.push(historicalCaptureStack)
+        }
+
 
         function getContextForType(optionName: string, expectedType: string): any {
             const option = scope.node.tryGetContext(optionName)
