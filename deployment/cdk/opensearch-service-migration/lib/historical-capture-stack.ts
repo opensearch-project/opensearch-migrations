c7f35453f741d11ccf0b9371971f86845b71f1cf
Kartik Ganesh <gkart@amazon.com>
2023-08-24 11:25:39 -0700

    [Fetch Migration] Remove Step Function from CDK
    Signed-off-by: Kartik Ganesh <gkart@amazon.com>

--- deployment/cdk/opensearch-service-migration/lib/historical-capture-stack.ts
+++ deployment/cdk/opensearch-service-migration/lib/historical-capture-stack.ts
@@ -4,8 +4,6 @@ import {Construct} from "constructs";
 import {Effect, ManagedPolicy, PolicyDocument, PolicyStatement, Role, ServicePrincipal} from "aws-cdk-lib/aws-iam";
 import {Cluster, ContainerImage, FargateService, FargateTaskDefinition, LogDrivers} from "aws-cdk-lib/aws-ecs";
 import {DockerImageAsset} from "aws-cdk-lib/aws-ecr-assets";
-import {Pass, StateMachine, IntegrationPattern} from "aws-cdk-lib/aws-stepfunctions"
-import {EcsRunTask, EcsFargateLaunchTarget} from "aws-cdk-lib/aws-stepfunctions-tasks";
 import {join} from "path";
 import { readFileSync } from "fs"
 
@@ -16,11 +14,6 @@ export interface historicalCaptureStackProps extends StackProps {
     readonly targetEndpoint: string
 }
 
-/**
- * This stack is a short exploratory task into having a deployable Fetch-Migration / Data Prepper
- * Step Function workflow that executes the historical data migration task via ECS.
- * NOTE: It should only be used for development purposes in its current state
- */
 export class HistoricalCaptureStack extends Stack {
 
     constructor(scope: Construct, id: string, props: historicalCaptureStackProps) {
@@ -37,17 +30,18 @@ export class HistoricalCaptureStack extends Stack {
             cpu: 512
         });
 
-        // Docker image asset
         let dpPipelineData: string = readFileSync(props.dpPipelineTemplatePath, 'utf8');
         dpPipelineData = dpPipelineData.replace("<SOURCE_CLUSTER_HOST>", props.sourceEndpoint);
         dpPipelineData = dpPipelineData.replace("<TARGET_CLUSTER_HOST>", props.targetEndpoint);
         // Base64 encode
         let encodedPipeline = Buffer.from(dpPipelineData).toString("base64");
-        // Create Historical Capture Container
+        
+        // Docker image asset
         const historicalCaptureImage = new DockerImageAsset(this, "fetchMigrationImage", {
             directory: join(__dirname, "../../..", "docker/fetch-migration")
         });
 
+        // Create Historical Capture Container
         const historicalCaptureContainer = historicalCaptureFargateTask.addContainer("historicalCaptureContainer", {
             image: ContainerImage.fromDockerImageAsset(historicalCaptureImage),
             // Add in region and stage
@@ -61,28 +55,6 @@ export class HistoricalCaptureStack extends Stack {
         // TODO - Limit outbound to only ECR endpoints somehow
         const fetchMigrationSecurityGroup = new SecurityGroup(this, 'fetchMigrationECRAccessSG', {
             vpc: ecsCluster.vpc
-        });
-
-        // Create Step Function workflow with a RunTask step
-        const runTask = new EcsRunTask(this, "historicalCaptureRunTask", {
-            cluster: ecsCluster,
-            taskDefinition: historicalCaptureFargateTask,
-            launchTarget: new EcsFargateLaunchTarget(),
-            integrationPattern: IntegrationPattern.RUN_JOB
-        });
-
-        const startState = new Pass(this, "historicalCaptureStartState");
-        const historicalCaptureWorkflowDef = startState.next(runTask);
-
-        const historicalCaptureStateMachine = new StateMachine(this, "historicalCaptureStateMachine", {
-            definition: historicalCaptureWorkflowDef
-        });
-
-        // Console output
-        let executionCommand: string = "aws stepfunctions start-execution --state-machine-arn "
-        new CfnOutput(this, 'HistoricalCaptureExports', {
-            value: executionCommand + historicalCaptureStateMachine.stateMachineArn,
-            description: 'Exported CLI command to kick off the historical data migration Step Function workflow',
         });
 
         // CFN outputs that will be consumed by Copilot
