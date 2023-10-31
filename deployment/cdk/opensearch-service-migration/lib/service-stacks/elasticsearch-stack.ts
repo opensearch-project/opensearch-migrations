7b71472dd57b9e379661c1549bc211506728456b
Tanner Lewis <lewijacn@amazon.com>
2023-10-31 15:27:24 -0400

    Add the rest of PR comment changes and minor fixes noticed
    Signed-off-by: Tanner Lewis <lewijacn@amazon.com>

--- deployment/cdk/opensearch-service-migration/lib/service-stacks/elasticsearch-stack.ts
+++ deployment/cdk/opensearch-service-migration/lib/service-stacks/elasticsearch-stack.ts
@@ -12,6 +12,11 @@ export interface ElasticsearchProps extends StackPropsExt {
     readonly vpc: IVpc,
 }
 
+/**
+ * The stack for the "elasticsearch" service. This service will spin up a simple Elasticsearch with
+ * Search Guard instance for simulating an Elasticsearch cluster. It will be partially duplicated by the
+ * "capture-proxy-es" service which contains a Capture Proxy instance and an Elasticsearch with Search Guard instance.
+ */
 export class ElasticsearchStack extends MigrationServiceCore {
 
     constructor(scope: Construct, id: string, props: ElasticsearchProps) {
