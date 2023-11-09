117d62178ee9175f2d1cd77e79c58a7aac86747b
Kartik Ganesh <gkart@amazon.com>
2023-11-08 16:48:56 -0800

    Support for dynamic target host substitution via INLINE_TARGET_HOST environment variable
    Signed-off-by: Kartik Ganesh <gkart@amazon.com>

--- FetchMigration/python/fetch_orchestrator.py
+++ FetchMigration/python/fetch_orchestrator.py
@@ -2,20 +2,79 @@ import argparse
 import base64
 import logging
 import os
+import re
 import subprocess
 import sys
 from typing import Optional
+
+import yaml
 
 import metadata_migration
 import migration_monitor
 from metadata_migration_params import MetadataMigrationParams
 from migration_monitor_params import MigrationMonitorParams
 
+__PROTOCOL_PREFIX_PATTERN = re.compile(r"^https?://")
+__HTTPS_PREFIX = "https://"
 __DP_EXECUTABLE_SUFFIX = "/bin/data-prepper"
 __PIPELINE_OUTPUT_FILE_SUFFIX = "/pipelines/pipeline.yaml"
 
 
+def __get_env_string(name: str) -> Optional[str]:
+    val: str = os.environ.get(name, "")
+    if len(val) > 0:
+        return val
+    else:
+        return None
+
+
+def update_target_host(dp_config: dict, target_host: str):
+    # Inline target host only supports HTTPS, so force it
+    target_with_protocol = target_host
+    match = __PROTOCOL_PREFIX_PATTERN.match(target_with_protocol)
+    if match:
+        target_with_protocol = target_host[match.end():]
+    target_with_protocol = __HTTPS_PREFIX + target_with_protocol
+    if len(dp_config) > 0:
+        # We expect the Data Prepper pipeline to only have a single top-level value
+        pipeline_config = next(iter(dp_config.values()))
+        # The entire pipeline will be validated later
+        if metadata_migration.SINK_KEY in pipeline_config:
+            # throws ValueError if no supported endpoints are found
+            plugin_name, plugin_config = metadata_migration.get_supported_endpoint(pipeline_config,
+                                                                                   metadata_migration.SINK_KEY)
+            plugin_config[metadata_migration.HOSTS_KEY] = [target_with_protocol]
+            pipeline_config[metadata_migration.SINK_KEY] = {plugin_name: plugin_config}
+
+
+def write_inline_pipeline(pipeline_file_path: str):
+    # This is expected to be a base64 encoded string
+    inline_pipeline = __get_env_string("INLINE_PIPELINE")
+    pipeline_yaml = yaml.safe_load(base64.b64decode(inline_pipeline))
+    inline_target_host = __get_env_string("INLINE_TARGET_HOST")
+    if inline_target_host is not None:
+        update_target_host(pipeline_yaml, inline_target_host)
+    with open(pipeline_file_path, 'w') as out_file:
+        # Note - this does not preserve comments
+        yaml.safe_dump(pipeline_yaml, out_file)
+
+
+def write_inline_target_host(pipeline_file_path: str, inline_target_host: str):
+    with open(pipeline_file_path, 'rw') as pipeline_file:
+        pipeline_yaml = yaml.safe_load(pipeline_file)
+        update_target_host(pipeline_yaml, inline_target_host)
+        # Note - this does not preserve comments
+        yaml.safe_dump(pipeline_yaml, pipeline_file)
+
+
 def run(dp_base_path: str, dp_config_file: str, dp_endpoint: str) -> Optional[int]:
+    # This is expected to be a base64 encoded string
+    inline_pipeline = __get_env_string("INLINE_PIPELINE")
+    inline_target_host = __get_env_string("INLINE_TARGET_HOST")
+    if inline_pipeline is not None:
+        write_inline_pipeline(dp_config_file)
+    elif inline_target_host is not None:
+        write_inline_target_host(dp_config_file, inline_target_host)
     dp_exec_path = dp_base_path + __DP_EXECUTABLE_SUFFIX
     output_file = dp_base_path + __PIPELINE_OUTPUT_FILE_SUFFIX
     metadata_migration_params = MetadataMigrationParams(dp_config_file, output_file, report=True)
@@ -46,7 +105,7 @@ if __name__ == '__main__':  # pragma no cover
         help="Path to the base directory where Data Prepper is installed "
     )
     arg_parser.add_argument(
-        "config_file_path",
+        "pipeline_file_path",
         help="Path to the Data Prepper pipeline YAML file to parse for source and target endpoint information"
     )
     arg_parser.add_argument(
@@ -55,13 +114,7 @@ if __name__ == '__main__':  # pragma no cover
     )
     cli_args = arg_parser.parse_args()
     base_path = os.path.expandvars(cli_args.data_prepper_path)
-
-    inline_pipeline = os.environ.get("INLINE_PIPELINE", None)
-    if inline_pipeline is not None:
-        decoded_bytes = base64.b64decode(inline_pipeline)
-        with open(cli_args.config_file_path, 'wb') as config_file:
-            config_file.write(decoded_bytes)
-    return_code = run(base_path, cli_args.config_file_path, cli_args.data_prepper_endpoint)
+    return_code = run(base_path, os.path.expandvars(cli_args.pipeline_file_path), cli_args.data_prepper_endpoint)
     if return_code == 0:
         sys.exit(0)
     else:
--- FetchMigration/python/tests/test_fetch_orchestrator.py
+++ FetchMigration/python/tests/test_fetch_orchestrator.py
@@ -1,10 +1,13 @@
+import copy
+import os
 import unittest
-from unittest.mock import patch, MagicMock, ANY
+from unittest import mock
+from unittest.mock import patch, MagicMock, ANY, mock_open
 
 import fetch_orchestrator as orchestrator
-from migration_monitor_params import MigrationMonitorParams
 from metadata_migration_params import MetadataMigrationParams
 from metadata_migration_result import MetadataMigrationResult
+from migration_monitor_params import MigrationMonitorParams
 
 
 class TestFetchOrchestrator(unittest.TestCase):
@@ -13,6 +16,7 @@ class TestFetchOrchestrator(unittest.TestCase):
     @patch('subprocess.Popen')
     @patch('metadata_migration.run')
     # Note that mock objects are passed bottom-up from the patch order above
+    @mock.patch.dict(os.environ, {}, clear=True)
     def test_orchestrator_run(self, mock_metadata_migration: MagicMock, mock_subprocess: MagicMock,
                               mock_monitor: MagicMock):
         test_path = "test_path"
@@ -37,6 +41,7 @@ class TestFetchOrchestrator(unittest.TestCase):
     @patch('subprocess.Popen')
     @patch('metadata_migration.run')
     # Note that mock objects are passed bottom-up from the patch order above
+    @mock.patch.dict(os.environ, {}, clear=True)
     def test_orchestrator_no_migration(self, mock_metadata_migration: MagicMock, mock_subprocess: MagicMock,
                                        mock_monitor: MagicMock):
         # Setup empty result from pre-migration
@@ -46,6 +51,81 @@ class TestFetchOrchestrator(unittest.TestCase):
         # Subsequent steps should not be called
         mock_subprocess.assert_not_called()
         mock_monitor.assert_not_called()
+
+    @patch('fetch_orchestrator.write_inline_target_host')
+    @patch('metadata_migration.run')
+    # Note that mock objects are passed bottom-up from the patch order above
+    @mock.patch.dict(os.environ, {"INLINE_TARGET_HOST": "host"}, clear=True)
+    def test_orchestrator_inline_target_host(self, mock_metadata_migration: MagicMock, mock_write_host: MagicMock):
+        # For testing, return no indices to migrate
+        mock_metadata_migration.return_value = MetadataMigrationResult()
+        orchestrator.run("test", "test", "test")
+        mock_metadata_migration.assert_called_once_with(ANY)
+        mock_write_host.assert_called_once_with(ANY, "host")
+
+    @patch('fetch_orchestrator.update_target_host')
+    @patch('fetch_orchestrator.write_inline_pipeline')
+    @patch('metadata_migration.run')
+    # Note that mock objects are passed bottom-up from the patch order above
+    @mock.patch.dict(os.environ, {"INLINE_PIPELINE": "test"}, clear=True)
+    def test_orchestrator_inline_pipeline(self, mock_metadata_migration: MagicMock, mock_write_pipeline: MagicMock,
+                                          mock_update_host: MagicMock):
+        # For testing, return no indices to migrate
+        mock_metadata_migration.return_value = MetadataMigrationResult()
+        orchestrator.run("test", "test", "test")
+        mock_metadata_migration.assert_called_once_with(ANY)
+        mock_write_pipeline.assert_called_once_with("test")
+        mock_update_host.assert_not_called()
+
+    @patch('builtins.open', new_callable=mock_open())
+    @patch('fetch_orchestrator.update_target_host')
+    @patch('metadata_migration.run')
+    # INLINE_PIPELINE value is base64 encoded version of {}
+    @mock.patch.dict(os.environ, {"INLINE_PIPELINE": "e30K", "INLINE_TARGET_HOST": "host"}, clear=True)
+    def test_orchestrator_inline_pipeline_and_host(self, mock_metadata_migration: MagicMock,
+                                                   mock_update_host: MagicMock, mock_file_open: MagicMock):
+        # For testing, return no indices to migrate
+        mock_metadata_migration.return_value = MetadataMigrationResult()
+        test_config_file = "config_file"
+        orchestrator.run("test", test_config_file, "test")
+        mock_metadata_migration.assert_called_once_with(ANY)
+        mock_update_host.assert_called_once_with(ANY, "host")
+        mock_file_open.assert_called_once_with(test_config_file, 'w')
+
+    @patch('yaml.safe_dump')
+    @patch('yaml.safe_load')
+    @patch('builtins.open', new_callable=mock_open())
+    # Note that mock objects are passed bottom-up from the patch order above
+    def test_write_inline_target_host(self, mock_file_open: MagicMock, mock_yaml_load: MagicMock,
+                                      mock_yaml_dump: MagicMock):
+        mock_yaml_load.return_value = {"root": {"sink": {"opensearch": {"hosts": ["val"]}}}}
+        inline_target = "host"
+        expected_target_url = "https://" + inline_target
+        test_values = [
+            inline_target,  # base case test
+            "http://" + inline_target  # tests forcing of HTTPS
+        ]
+        expected_pipeline = copy.deepcopy(mock_yaml_load.return_value)
+        # TODO - replace with jsonpath
+        expected_pipeline["root"]["sink"]["opensearch"]["hosts"] = [expected_target_url]
+        for val in test_values:
+            mock_file_open.reset_mock()
+            mock_yaml_dump.reset_mock()
+            orchestrator.write_inline_target_host("test", val)
+            mock_file_open.assert_called_once_with("test", "rw")
+            mock_yaml_dump.assert_called_once_with(expected_pipeline, ANY)
+
+    def test_update_target_host_bad_config(self):
+        bad_configs = [
+            {},  # empty config
+            {"root": {}},  # only root element
+            {"root": {"source": True}},  # no sink
+        ]
+        for config in bad_configs:
+            original = copy.deepcopy(config)
+            orchestrator.update_target_host(config, "host")
+            # Bad configs shoudl result in no change
+            self.assertEqual(config, original)
 
 
 if __name__ == '__main__':
