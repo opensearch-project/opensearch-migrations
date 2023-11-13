8f268f26bc17cfdcc8bcd0ceaa42d08371ed8e72
Greg Schohn <greg.schohn@gmail.com>
2023-11-12 21:45:08 -0500

    Fix a bad merge from earlier that broke the build. The fix was more involved because the way that the test was verifying output changed in the collateral damage.  The new test uses Log4J classes directly (just like the SourceTargetCaptureTuple), but is able to confirm that log4j will get the right messages and not throw an error, etc.
    Signed-off-by: Greg Schohn <greg.schohn@gmail.com>

--- TrafficCapture/trafficReplayer/src/test/java/org/opensearch/migrations/replay/SourceTargetCaptureTupleTest.java
+++ TrafficCapture/trafficReplayer/src/test/java/org/opensearch/migrations/replay/SourceTargetCaptureTupleTest.java
@@ -1,6 +1,12 @@
 package org.opensearch.migrations.replay;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.LogEvent;
+import org.apache.logging.log4j.core.Logger;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.appender.AbstractAppender;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.opensearch.migrations.replay.datatypes.PojoTrafficStreamKey;
@@ -10,6 +16,8 @@ import org.opensearch.migrations.replay.datatypes.UniqueSourceRequestKey;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,11 +26,41 @@ class SourceTargetCaptureTupleTest {
     private static final String NODE_ID = "n";
     public static final String TEST_EXCEPTION_MESSAGE = "TEST_EXCEPTION";
 
+    private static class CloseableLogSetup implements AutoCloseable {
+        List<LogEvent> logEvents = new ArrayList<>();
+        AbstractAppender testAppender;
+        public CloseableLogSetup() {
+            testAppender = new AbstractAppender(SourceTargetCaptureTuple.OUTPUT_TUPLE_JSON_LOGGER,
+                    null, null, false, null) {
+                @Override
+                public void append(LogEvent event) {
+                    logEvents.add(event);
+                }
+            };
+            var tupleLogger = (Logger) LogManager.getLogger(SourceTargetCaptureTuple.OUTPUT_TUPLE_JSON_LOGGER);
+            tupleLogger.setLevel(Level.ALL);
+            testAppender.start();
+            tupleLogger.setAdditive(false);
+            tupleLogger.addAppender(testAppender);
+            var loggerCtx = ((LoggerContext) LogManager.getContext(false));
+        }
+
+        @Override
+        public void close() {
+            var tupleLogger = (Logger) LogManager.getLogger(SourceTargetCaptureTuple.OUTPUT_TUPLE_JSON_LOGGER);
+            tupleLogger.removeAppender(testAppender);
+            testAppender.stop();
+        }
+    }
+
     @Test
     public void testTupleNewWithNullKeyThrows() {
-        Assertions.assertThrows(Exception.class,
-                ()->new SourceTargetCaptureTuple(null, null, null,
-                        null, null, null, null));
+        try (var closeableLogSetup = new CloseableLogSetup()) {
+            Assertions.assertThrows(Exception.class,
+                    () -> new SourceTargetCaptureTuple(null, null, null,
+                            null, null, null, null));
+            Assertions.assertEquals(0, closeableLogSetup.logEvents.size());
+        }
     }
 
     @Test
@@ -30,14 +68,14 @@ class SourceTargetCaptureTupleTest {
         var emptyTuple = new SourceTargetCaptureTuple(
                 new UniqueReplayerRequestKey(new PojoTrafficStreamKey(NODE_ID,"c",0), 0, 0),
                 null, null, null, null, null, null);
-        String contents;
-        try (var byteArrayOutputStream = new ByteArrayOutputStream()) {
-            var consumer = new SourceTargetCaptureTuple.TupleToStreamConsumer(byteArrayOutputStream);
+        try (var closeableLogSetup = new CloseableLogSetup()) {
+            var consumer = new SourceTargetCaptureTuple.TupleToStreamConsumer();
             consumer.accept(emptyTuple);
-            contents = new String(byteArrayOutputStream.toByteArray(), StandardCharsets.UTF_8);
+            Assertions.assertEquals(1, closeableLogSetup.logEvents.size());
+            var contents = closeableLogSetup.logEvents.get(0).getMessage().getFormattedMessage();
+            log.info("Output="+contents);
+            Assertions.assertTrue(contents.contains(NODE_ID));
         }
-        log.info("Output="+contents);
-        Assertions.assertTrue(contents.contains(NODE_ID));
     }
 
     @Test
@@ -46,14 +84,14 @@ class SourceTargetCaptureTupleTest {
         var emptyTuple = new SourceTargetCaptureTuple(
                 new UniqueReplayerRequestKey(new PojoTrafficStreamKey(NODE_ID,"c",0), 0, 0),
                 null, null, null, null, exception, null);
-        String contents;
-        try (var byteArrayOutputStream = new ByteArrayOutputStream()) {
-            var consumer = new SourceTargetCaptureTuple.TupleToStreamConsumer(byteArrayOutputStream);
+        try (var closeableLogSetup = new CloseableLogSetup()) {
+            var consumer = new SourceTargetCaptureTuple.TupleToStreamConsumer();
             consumer.accept(emptyTuple);
-            contents = new String(byteArrayOutputStream.toByteArray(), StandardCharsets.UTF_8);
+            Assertions.assertEquals(1, closeableLogSetup.logEvents.size());
+            var contents = closeableLogSetup.logEvents.get(0).getMessage().getFormattedMessage();
+            log.info("Output="+contents);
+            Assertions.assertTrue(contents.contains(NODE_ID));
+            Assertions.assertTrue(contents.contains(TEST_EXCEPTION_MESSAGE));
         }
-        log.info("Output="+contents);
-        Assertions.assertTrue(contents.contains(NODE_ID));
-        Assertions.assertTrue(contents.contains(TEST_EXCEPTION_MESSAGE));
     }
 }
