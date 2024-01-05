1172912eb101399cba335ee0831d8720bffd60d9
Greg Schohn <greg.schohn@gmail.com>
2024-01-05 15:40:35 -0500

    Make attribute name filtering more generic and fix a bug in negation so that the connectionId is now emitted from the activeConnections metric.
    Signed-off-by: Greg Schohn <greg.schohn@gmail.com>

--- TrafficCapture/captureOffloader/src/main/java/org/opensearch/migrations/trafficcapture/tracing/ConnectionContext.java
+++ TrafficCapture/captureOffloader/src/main/java/org/opensearch/migrations/trafficcapture/tracing/ConnectionContext.java
@@ -3,14 +3,14 @@ package org.opensearch.migrations.trafficcapture.tracing;
 import io.opentelemetry.api.common.Attributes;
 import lombok.Getter;
 import org.opensearch.migrations.tracing.AbstractNestedSpanContext;
+import org.opensearch.migrations.tracing.AttributeNameMatchingPredicate;
 import org.opensearch.migrations.tracing.FilteringAttributeBuilder;
 import org.opensearch.migrations.tracing.RootOtelContext;
 import org.opensearch.migrations.tracing.commoncontexts.IConnectionContext;
 
-import java.util.Set;
-
 public class ConnectionContext extends AbstractNestedSpanContext<RootOtelContext> implements IConnectionContext {
-    private static final Set KEYS_TO_ALLOW_FOR_ACTIVE_CONNECTION_COUNT = Set.of(CONNECTION_ID_ATTR.getKey());
+    private static final AttributeNameMatchingPredicate KEYS_TO_EXCLUDE_FOR_ACTIVE_CONNECTION_COUNT =
+            AttributeNameMatchingPredicate.builder(true).add(CONNECTION_ID_ATTR.getKey()).build();
     public static final String ACTIVE_CONNECTION = "activeConnection";
 
     @Getter
@@ -28,13 +28,13 @@ public class ConnectionContext extends AbstractNestedSpanContext<RootOtelContext
         this.nodeId = nodeId;
         initializeSpan();
         meterDeltaEvent(ACTIVE_CONNECTION, 1,
-                new FilteringAttributeBuilder(Attributes.builder(), false, KEYS_TO_ALLOW_FOR_ACTIVE_CONNECTION_COUNT));
+                new FilteringAttributeBuilder(Attributes.builder(), KEYS_TO_EXCLUDE_FOR_ACTIVE_CONNECTION_COUNT));
     }
 
     @Override
     public void sendMeterEventsForEnd() {
         super.sendMeterEventsForEnd();
         meterDeltaEvent(ACTIVE_CONNECTION, -1,
-                new FilteringAttributeBuilder(Attributes.builder(), false, KEYS_TO_ALLOW_FOR_ACTIVE_CONNECTION_COUNT));
+                new FilteringAttributeBuilder(Attributes.builder(), KEYS_TO_EXCLUDE_FOR_ACTIVE_CONNECTION_COUNT));
     }
 }
--- TrafficCapture/coreUtilities/src/main/java/org/opensearch/migrations/tracing/FilteringAttributeBuilder.java
+++ TrafficCapture/coreUtilities/src/main/java/org/opensearch/migrations/tracing/FilteringAttributeBuilder.java
@@ -3,25 +3,20 @@ package org.opensearch.migrations.tracing;
 import io.opentelemetry.api.common.AttributeKey;
 import io.opentelemetry.api.common.Attributes;
 import io.opentelemetry.api.common.AttributesBuilder;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.Set;
-import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Predicate;
 
 @Getter
 public class FilteringAttributeBuilder implements AttributesBuilder {
     private AttributesBuilder underlyingBuilder;
-    public final boolean matchExcludes;
-    public final Set<String> keysToMatch;
+    private final Predicate<AttributeKey> excludePredicate;
 
-    public FilteringAttributeBuilder(AttributesBuilder underlyingBuilder, boolean matchesExclude,
-                                     Set<String> keysToMatch) {
+    public FilteringAttributeBuilder(AttributesBuilder underlyingBuilder, Predicate<AttributeKey> excludePredicate) {
         this.underlyingBuilder = underlyingBuilder;
-        this.matchExcludes = matchesExclude;
-        this.keysToMatch = Collections.unmodifiableSet(keysToMatch);
+        this.excludePredicate = excludePredicate;
     }
 
     @Override
@@ -31,7 +26,7 @@ public class FilteringAttributeBuilder implements AttributesBuilder {
 
     @Override
     public <T> AttributesBuilder put(AttributeKey<Long> key, int value) {
-        if (keysToMatch.contains(key.getKey()) == matchExcludes) {
+        if (excludePredicate.test(key)) {
             return this;
         }
         underlyingBuilder = underlyingBuilder.put(key, value);
@@ -40,7 +35,7 @@ public class FilteringAttributeBuilder implements AttributesBuilder {
 
     @Override
     public <T> AttributesBuilder put(AttributeKey<T> key, T value) {
-        if (keysToMatch.contains(key.getKey()) == matchExcludes) {
+        if (excludePredicate.test(key)) {
             return this;
         }
         underlyingBuilder = underlyingBuilder.put(key, value);
