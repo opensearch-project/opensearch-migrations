8e51d31a61328768b97cb2882b4a6c20750c75a5
Omar Khasawneh <okhasawn@amazon.com>
2024-01-17 02:15:27 -0600

    Add Fetch in Docker
    Signed-off-by: Omar Khasawneh <okhasawn@amazon.com>

--- deployment/cdk/opensearch-service-migration/buildDockerImages.sh
+++ deployment/cdk/opensearch-service-migration/buildDockerImages.sh
@@ -5,5 +5,8 @@ script_abs_path=$(readlink -f "$0")
 script_dir_abs_path=$(dirname "$script_abs_path")
 cd $script_dir_abs_path || exit
 
-cd ../../../TrafficCapture || exit
+cd ../../../FetchMigration || exit
+docker build -t migrations/fetch-migration .
+
+cd ../TrafficCapture || exit
 ./gradlew :dockerSolution:buildDockerImages -x test
