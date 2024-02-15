6ce6166f98c606322733575ba57dddd63daeeb99
Andre Kurait <akurait@amazon.com>
2024-02-15 16:11:58 -0600

    Fixing annotation chaining
    Signed-off-by: Andre Kurait <akurait@amazon.com>

--- TrafficCapture/trafficCaptureProxyServer/src/test/java/org/opensearch/migrations/trafficcapture/proxyserver/testcontainers/annotations/HttpdContainerTest.java
+++ TrafficCapture/trafficCaptureProxyServer/src/test/java/org/opensearch/migrations/trafficcapture/proxyserver/testcontainers/annotations/HttpdContainerTest.java
@@ -1,10 +1,16 @@
 package org.opensearch.migrations.trafficcapture.proxyserver.testcontainers.annotations;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import org.junit.jupiter.api.parallel.ResourceLock;
 
 @Inherited
 @ResourceLock("HttpdContainer")
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE})
 @TestContainerTest
 public @interface HttpdContainerTest {
 
