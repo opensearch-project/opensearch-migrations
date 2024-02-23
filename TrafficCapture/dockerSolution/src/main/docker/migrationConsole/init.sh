77245dc5fca4b0dd290e97adc38baa47efcab0ef
Tanner Lewis <lewijacn@amazon.com>
2024-02-23 09:54:17 -0500

    Update dockerfile
    Signed-off-by: Tanner Lewis <lewijacn@amazon.com>

--- TrafficCapture/dockerSolution/src/main/docker/migrationConsole/init.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ "$EXPERIMENTAL" = "true" ]; then
-    python3 /console/console_api/manage.py runserver_plus 0.0.0.0:8000
-else
-    # TODO: Replace command below with "tail -f /dev/null" once env variable used.
-    python3 /console/console_api/manage.py runserver_plus 0.0.0.0:8000
-fi
-
