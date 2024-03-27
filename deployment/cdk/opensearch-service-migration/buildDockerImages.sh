3ab9fbd1888547bfc45b2dd89303eb66009a104a
Tanner Lewis <lewijacn@amazon.com>
2024-03-27 11:13:35 -0500

    Wrap path for buildDockerImages
    Signed-off-by: Tanner Lewis <lewijacn@amazon.com>

--- deployment/cdk/opensearch-service-migration/buildDockerImages.sh
+++ deployment/cdk/opensearch-service-migration/buildDockerImages.sh
@@ -3,8 +3,8 @@
 # Allow executing this script from any dir
 script_abs_path=$(readlink -f "$0")
 script_dir_abs_path=$(dirname "$script_abs_path")
-echo $script_dir_abs_path
-cd $script_dir_abs_path || exit
+echo "$script_dir_abs_path"
+cd "$script_dir_abs_path" || exit
 
 cd ../../../TrafficCapture || exit
 ./gradlew :dockerSolution:buildDockerImages -x test
