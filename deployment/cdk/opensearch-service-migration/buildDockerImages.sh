2f23c185e49e18928254032543ac16cb9e6e40af
Tanner Lewis <lewijacn@amazon.com>
2024-03-28 18:05:55 -0400

    Remove echo line
    Signed-off-by: Tanner Lewis <lewijacn@amazon.com>

--- deployment/cdk/opensearch-service-migration/buildDockerImages.sh
+++ deployment/cdk/opensearch-service-migration/buildDockerImages.sh
@@ -3,7 +3,6 @@
 # Allow executing this script from any dir
 script_abs_path=$(readlink -f "$0")
 script_dir_abs_path=$(dirname "$script_abs_path")
-echo "$script_dir_abs_path"
 cd "$script_dir_abs_path" || exit
 
 cd ../../../TrafficCapture || exit
