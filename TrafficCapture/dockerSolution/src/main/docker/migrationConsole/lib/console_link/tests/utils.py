3e83480fdbe29917b781294a1e05642d48371d5e
Tanner Lewis <lewijacn@amazon.com>
2024-05-29 11:49:33 -0400

    Add basic sanity tests for osi_utils create functions
    Signed-off-by: Tanner Lewis <lewijacn@amazon.com>

--- TrafficCapture/dockerSolution/src/main/docker/migrationConsole/lib/console_link/tests/utils.py
+++ TrafficCapture/dockerSolution/src/main/docker/migrationConsole/lib/console_link/tests/utils.py
@@ -1,15 +1,19 @@
-from console_link.models.cluster import Cluster
-
-# Define a valid cluster configuration
-valid_cluster_config = {
-    "endpoint": "https://opensearchtarget:9200",
-    "allow_insecure": True,
-    "authorization": {
-        "type": "basic_auth",
-        "details": {"username": "admin", "password": "myStrongPassword123!"},
-    },
-}
+from console_link.models.cluster import Cluster, AuthMethod
 
 
-def create_valid_cluster():
-    return Cluster(valid_cluster_config)
+def create_valid_cluster(endpoint="https://opensearchtarget:9200",
+                         allow_insecure=True,
+                         auth_type=AuthMethod.BASIC_AUTH.name.lower(),
+                         details=None):
+    if details is None:
+        details = {"username": "admin", "password": "myStrongPassword123!"}
+
+    custom_cluster_config = {
+        "endpoint": endpoint,
+        "allow_insecure": allow_insecure,
+        "authorization": {
+            "type": auth_type,
+            "details": details,
+        },
+    }
+    return Cluster(custom_cluster_config)
