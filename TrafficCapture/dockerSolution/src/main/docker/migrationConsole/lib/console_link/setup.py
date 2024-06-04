bfaf97cab85587250789e0ccb33b4e890d4c6b7d
Tanner Lewis <lewijacn@amazon.com>
2024-06-04 11:01:44 -0400

    Update license on console library
    Signed-off-by: Tanner Lewis <lewijacn@amazon.com>

--- TrafficCapture/dockerSolution/src/main/docker/migrationConsole/lib/console_link/setup.py
+++ TrafficCapture/dockerSolution/src/main/docker/migrationConsole/lib/console_link/setup.py
@@ -21,6 +21,6 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: Apache Software License",
     ],
 )
