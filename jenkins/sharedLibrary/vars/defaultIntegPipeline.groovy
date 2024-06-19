6c5c2c495ea4c6b22544031a88f8dbd8934513e4
Tanner Lewis <lewijacn@amazon.com>
2024-06-19 12:03:57 -0400

    Update vars
    Signed-off-by: Tanner Lewis <lewijacn@amazon.com>

--- jenkins/sharedLibrary/vars/defaultIntegPipeline.groovy
+++ jenkins/sharedLibrary/vars/defaultIntegPipeline.groovy
@@ -3,54 +3,54 @@
 //    public subnets as well as request an extension to allow more than 5 VPCs per region
 // 2. There is a still a manual step needed on the EC2 source load balancer to replace its security group rule which allows all traffic (0.0.0.0/0) to
 //    allow traffic for the relevant service security group. This needs a better story around accepting user security groups in our Migration CDK.
-def source_cdk_context = """
-{
-  "source-single-node-ec2": {
-    "suffix": "ec2-source-<STAGE>",
-    "networkStackSuffix": "ec2-source-<STAGE>",
-    "vpcId": "vpc-07d3e089b8e9139e4",
-    "distVersion": "7.10.2",
-    "cidr": "12.0.0.0/16",
-    "distributionUrl": "https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-oss-7.10.2-linux-x86_64.tar.gz",
-    "captureProxyEnabled": true,
-    "securityDisabled": true,
-    "minDistribution": false,
-    "cpuArch": "x64",
-    "isInternal": true,
-    "singleNodeCluster": true,
-    "networkAvailabilityZones": 2,
-    "dataNodeCount": 1,
-    "managerNodeCount": 0,
-    "serverAccessType": "ipv4",
-    "restrictServerAccessTo": "0.0.0.0/0"
-  }
-}
-"""
-def migration_cdk_context = """
-{
-  "migration-default": {
-    "stage": "<STAGE>",
-    "vpcId": "vpc-07d3e089b8e9139e4",
-    "engineVersion": "OS_2.11",
-    "domainName": "os-cluster-<STAGE>",
-    "dataNodeCount": 2,
-    "openAccessPolicyEnabled": true,
-    "domainRemovalPolicy": "DESTROY",
-    "artifactBucketRemovalPolicy": "DESTROY",
-    "trafficReplayerExtraArgs": "--speedup-factor 10.0",
-    "fetchMigrationEnabled": true,
-    "reindexFromSnapshotServiceEnabled": true,
-    "sourceClusterEndpoint": "<SOURCE_CLUSTER_ENDPOINT>",
-    "dpPipelineTemplatePath": "../../../test/dp_pipeline_aws_integ.yaml",
-    "migrationConsoleEnableOSI": true,
-    "migrationAPIEnabled": true
-  }
-}
-"""
-def source_context_file_name = 'sourceJenkinsContext.json'
-def migration_context_file_name = 'migrationJenkinsContext.json'
 
 def call(Map config = [:]) {
+    def source_cdk_context = """
+    {
+      "source-single-node-ec2": {
+        "suffix": "ec2-source-<STAGE>",
+        "networkStackSuffix": "ec2-source-<STAGE>",
+        "vpcId": "vpc-07d3e089b8e9139e4",
+        "distVersion": "7.10.2",
+        "cidr": "12.0.0.0/16",
+        "distributionUrl": "https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-oss-7.10.2-linux-x86_64.tar.gz",
+        "captureProxyEnabled": true,
+        "securityDisabled": true,
+        "minDistribution": false,
+        "cpuArch": "x64",
+        "isInternal": true,
+        "singleNodeCluster": true,
+        "networkAvailabilityZones": 2,
+        "dataNodeCount": 1,
+        "managerNodeCount": 0,
+        "serverAccessType": "ipv4",
+        "restrictServerAccessTo": "0.0.0.0/0"
+      }
+    }
+    """
+    def migration_cdk_context = """
+    {
+      "migration-default": {
+        "stage": "<STAGE>",
+        "vpcId": "vpc-07d3e089b8e9139e4",
+        "engineVersion": "OS_2.11",
+        "domainName": "os-cluster-<STAGE>",
+        "dataNodeCount": 2,
+        "openAccessPolicyEnabled": true,
+        "domainRemovalPolicy": "DESTROY",
+        "artifactBucketRemovalPolicy": "DESTROY",
+        "trafficReplayerExtraArgs": "--speedup-factor 10.0",
+        "fetchMigrationEnabled": true,
+        "reindexFromSnapshotServiceEnabled": true,
+        "sourceClusterEndpoint": "<SOURCE_CLUSTER_ENDPOINT>",
+        "dpPipelineTemplatePath": "../../../test/dp_pipeline_aws_integ.yaml",
+        "migrationConsoleEnableOSI": true,
+        "migrationAPIEnabled": true
+      }
+    }
+    """
+    def source_context_file_name = 'sourceJenkinsContext.json'
+    def migration_context_file_name = 'migrationJenkinsContext.json'
     pipeline {
         agent { label config.overrideAgent ?: 'Jenkins-Default-Agent-X64-C5xlarge-Single-Host' }
 
