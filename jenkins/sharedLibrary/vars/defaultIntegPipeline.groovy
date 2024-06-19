97dd3607d35e9fa3cea88b8b2b8932979fba3014
Tanner Lewis <lewijacn@amazon.com>
2024-06-19 10:28:45 -0400

    Add shared library
    Signed-off-by: Tanner Lewis <lewijacn@amazon.com>

---
+++ jenkins/sharedLibrary/vars/defaultIntegPipeline.groovy
@@ -0,0 +1,44 @@
+def call(Map config = [:]) {
+    pipeline {
+        agent any
+
+        stages {
+            stage('Build') {
+                steps {
+                    script {
+                        if (config.buildStep) {
+                            config.buildStep()
+                        } else {
+                            echo 'Default Build Step'
+                            // Add default build steps here
+                        }
+                    }
+                }
+            }
+            stage('Test') {
+                steps {
+                    script {
+                        if (config.testStep) {
+                            config.testStep()
+                        } else {
+                            echo 'Default Test Step'
+                            // Add default test steps here
+                        }
+                    }
+                }
+            }
+            stage('Deploy') {
+                steps {
+                    script {
+                        if (config.deployStep) {
+                            config.deployStep()
+                        } else {
+                            echo 'Default Deploy Step'
+                            // Add default deploy steps here
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
