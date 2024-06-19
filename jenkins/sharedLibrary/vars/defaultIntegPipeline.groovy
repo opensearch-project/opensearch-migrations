3f6b900d2cb1bef7639f801faedf05cfcbcd85f3
Tanner Lewis <lewijacn@amazon.com>
2024-06-19 12:38:03 -0400

    Test shared lib
    Signed-off-by: Tanner Lewis <lewijacn@amazon.com>

--- jenkins/sharedLibrary/vars/defaultIntegPipeline.groovy
+++ jenkins/sharedLibrary/vars/defaultIntegPipeline.groovy
@@ -51,9 +51,9 @@ def call(Map config = [:]) {
     """
     def source_context_file_name = 'sourceJenkinsContext.json'
     def migration_context_file_name = 'migrationJenkinsContext.json'
-    def gitUrl = config.gitUrl ?: 'https://github.com/opensearch-project/opensearch-migrations.git'
-    def gitBranch = config.gitBranch ?: 'main'
-    def stage = config.stage ?: 'aws-integ'
+    def gitUrl = 'https://github.com/opensearch-project/opensearch-migrations.git'
+    def gitBranch = 'main'
+    def stage = 'aws-integ'
     pipeline {
         agent { label config.overrideAgent ?: 'Jenkins-Default-Agent-X64-C5xlarge-Single-Host' }
 
