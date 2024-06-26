d73c2572f45e5c8ce1c1dede3a3bbfd1f4292fa8
Peter Nied <peternied@hotmail.com>
2024-06-26 14:43:23 +0000

    Test stubs
    Signed-off-by: Peter Nied <peternied@hotmail.com>

---
+++ MetadataMigration/src/test/java/org/opensearch/migrations/transformation/VersionRangeTest.java
@@ -0,0 +1,10 @@
+package org.opensearch.migrations.transformation;
+
+import org.junit.jupiter.api.Test;
+
+public class VersionRangeTest {
+    @Test
+    void testInRange() {
+
+    }
+}
---
+++ MetadataMigration/src/test/java/org/opensearch/migrations/transformation/VersionTest.java
@@ -0,0 +1,40 @@
+package org.opensearch.migrations.transformation;
+
+import org.junit.jupiter.api.Test;
+
+public class VersionTest {
+    @Test
+    void testCanEqual() {
+
+    }
+
+    @Test
+    void testCompareTo() {
+
+    }
+
+    @Test
+    void testEquals() {
+
+    }
+
+    @Test
+    void testGreaterThan() {
+
+    }
+
+    @Test
+    void testHashCode() {
+
+    }
+
+    @Test
+    void testLessThan() {
+
+    }
+
+    @Test
+    void testToString() {
+
+    }
+}
