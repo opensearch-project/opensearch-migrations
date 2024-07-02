88fc0a35f194ac7d84d01e76a30a8b31114dc687
Tanner Lewis <lewijacn@amazon.com>
2024-07-01 20:44:24 -0400

    Minor updates to jenkins integ tests workflow
    Signed-off-by: Tanner Lewis <lewijacn@amazon.com>

--- test/awsRunIntegTests.sh
+++ test/awsRunIntegTests.sh
@@ -5,23 +5,32 @@ usage() {
   echo "Script to run integrations tests on AWS Migration Console"
   echo ""
   echo "Usage: "
-  echo "  ./awsRunIntegTests.sh [--unique-id] [--stage]"
+  echo "  ./awsRunIntegTests.sh [--command] [--test-result-file] [--stage]"
   echo ""
   echo "Options:"
-  echo "  --unique-id                                      Identifier for labeling integ test artifacts, e.g. 'full_run_123'."
-  echo "  --stage                                          The stage used for CDK deployment, default is 'aws-integ'."
+  echo "  --command                                   Provide test command to execute on the Migration Console"
+  echo "  --test-result-file                          The test result file to check for success or failure"
+  echo "  --stage                                     Deployment stage name"
   echo ""
   exit 1
 }
 
 epoch_seconds=$(date +%s)
-UNIQUE_ID="test_${epoch_seconds}_1"
-STAGE='aws-integ'
+unique_id="test_${epoch_seconds}_1"
+test_dir="/root/lib/integ_test/integ_test"
+STAGE="aws-integ"
+TEST_RESULT_FILE="${test_dir}/reports/${unique_id}/report.xml"
+COMMAND="pytest --log-file=${test_dir}/reports/${unique_id}/pytest.log --junitxml=${TEST_RESULT_FILE} ${test_dir}/replayer_tests.py --unique_id ${unique_id} -s"
 
 while [[ $# -gt 0 ]]; do
   case $1 in
-    --unique-id)
-      UNIQUE_ID="$2"
+    --command)
+      COMMAND="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    --test-result-file)
+      TEST_RESULT_FILE="$2"
       shift # past argument
       shift # past value
       ;;
@@ -45,12 +54,11 @@ done
 
 task_arn=$(aws ecs list-tasks --cluster migration-${STAGE}-ecs-cluster --family "migration-${STAGE}-migration-console" | jq --raw-output '.taskArns[0]')
 
-test_dir="/root/lib/integ_test/integ_test"
 # Kickoff integration tests
 set -o xtrace
-unbuffer aws ecs execute-command --cluster "migration-${STAGE}-ecs-cluster" --task "${task_arn}" --container "migration-console" --interactive --command "pytest --log-file=${test_dir}/reports/${UNIQUE_ID}/pytest.log --junitxml=${test_dir}/reports/${UNIQUE_ID}/report.xml ${test_dir}/replayer_tests.py --unique_id ${UNIQUE_ID} -k test_replayer_0004 -s"
+unbuffer aws ecs execute-command --cluster "migration-${STAGE}-ecs-cluster" --task "${task_arn}" --container "migration-console" --interactive --command "${COMMAND}"
+test_output=$(unbuffer aws ecs execute-command --cluster "migration-${STAGE}-ecs-cluster" --task "${task_arn}" --container "migration-console" --interactive --command "awk '/failures/ && /errors/' ${TEST_RESULT_FILE}")
 set +o xtrace
-test_output=$(unbuffer aws ecs execute-command --cluster "migration-${STAGE}-ecs-cluster" --task "${task_arn}" --container "migration-console" --interactive --command "awk '/failures/ && /errors/' /root/lib/integ_test/integ_test/reports/${UNIQUE_ID}/report.xml")
 echo "Integration test generated summary: "
 echo "$test_output"
 failure_output=$(echo "$test_output" | grep -o "failures=\"0\"")
