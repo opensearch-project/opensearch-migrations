e009aea547993d94470365bf4cfe7a88d7b7f15c
Tanner Lewis <lewijacn@amazon.com>
2024-07-17 18:06:09 -0400

    Comment update
    Signed-off-by: Tanner Lewis <lewijacn@amazon.com>

--- jenkins/migrationIntegPipelines/ec2SourceE2EPipeline.groovy
+++ jenkins/migrationIntegPipelines/ec2SourceE2EPipeline.groovy
@@ -1,5 +1,5 @@
 // Note:
-// 1. We are using an existing common VPC that we provide through a 'vpcId' parameter on the pipeline for now until we move
+// 1. We are using an existing common VPC that we provide through a manually created 'VPC_ID' parameter on the pipeline for now until we move
 //    to a proper Jenkins accounts and can create a setup without public subnets as well as request an extension to allow more than 5 VPCs per region
 // 2. There is a still a manual step needed on the EC2 source load balancer to replace its security group rule which allows all traffic (0.0.0.0/0) to
 //    allow traffic for the relevant service security group. This needs a better story around accepting user security groups in our Migration CDK.
