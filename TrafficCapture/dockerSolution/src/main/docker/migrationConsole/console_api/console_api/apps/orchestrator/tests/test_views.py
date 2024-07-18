7bb1613a096654200046e0be0d5afb662b2f2568
Tanner Lewis <lewijacn@amazon.com>
2024-07-18 19:05:56 -0400

    Updates per PR feedback
    Signed-off-by: Tanner Lewis <lewijacn@amazon.com>

--- TrafficCapture/dockerSolution/src/main/docker/migrationConsole/console_api/console_api/apps/orchestrator/tests/test_views.py
+++ TrafficCapture/dockerSolution/src/main/docker/migrationConsole/console_api/console_api/apps/orchestrator/tests/test_views.py
@@ -1,13 +1,14 @@
 import os
+import botocore
 from django.test import Client, SimpleTestCase
 from moto import mock_aws
 from rest_framework import status
 from unittest.mock import patch
 
-
+PIPELINE_NAME = 'test-pipeline'
 VALID_CREATE_PAYLOAD = {
     'PipelineRoleArn': 'arn:aws:iam::123456789012:role/pipeline-access-role',
-    'PipelineName': 'test-pipeline',
+    'PipelineName': PIPELINE_NAME,
     'AwsRegion': 'us-east-1',
     'Tags': ['test-tag=123'],
     'IndexRegexSelections': ['index-test-*', 'stored-*'],
@@ -25,35 +26,50 @@ VALID_CREATE_PAYLOAD = {
     'VpcSecurityGroupIds': ['sg-123456789', 'sg-789012345']
 }
 VALID_UPDATE_PAYLOAD = {
-    'PipelineName': 'test-pipeline'
+    'PipelineName': PIPELINE_NAME
 }
 VALID_ASSUME_ROLE_UPDATE_PAYLOAD = {
-    'PipelineName': 'test-pipeline',
+    'PipelineName': PIPELINE_NAME,
     'PipelineManagerAssumeRoleArn': 'arn:aws:iam::123456789012:role/testRole',
 }
 
+# Original botocore _make_api_call function
+orig = botocore.client.BaseClient._make_api_call
 
-# Moto has not yet implemented mocking for OSI API actions, using @patch for these calls
+
+# Moto has not yet implemented mocking for OSI API actions, so must mock these API calls manually here
+def mock_make_api_call(self, operation_name, kwarg):
+    # For example for the Access Analyzer service
+    # As you can see the operation_name has the list_analyzers snake_case form but
+    # we are using the ListAnalyzers form.
+    # Rationale -> https://github.com/boto/botocore/blob/develop/botocore/client.py#L810:L816
+    if (operation_name == 'CreatePipeline' or operation_name == 'DeletePipeline' or
+            operation_name == 'StartPipeline' or operation_name == 'StopPipeline'):
+        return {'Pipeline':
+            [{
+                'PipelineName': PIPELINE_NAME
+            }
+            ]}
+    # If we don't want to patch the API call
+    return orig(self, operation_name, kwarg)
+
+
 class OrchestratorViewsTest(SimpleTestCase):
 
     def setUp(self):
         self.client = Client()
         os.environ['AWS_DEFAULT_REGION'] = 'us-east-1'
 
-    @patch('console_api.apps.orchestrator.views.create_pipeline_from_json')
-    def test_osi_create_migration(self, mock_create_pipeline_from_json):
-        mock_create_pipeline_from_json.return_value = None
-
+    @patch('botocore.client.BaseClient._make_api_call', new=mock_make_api_call)
+    def test_osi_create_migration(self):
         response = self.client.post('/orchestrator/osi-create-migration', data=VALID_CREATE_PAYLOAD,
                                     content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertIn('Timestamp', response.data)
 
     @mock_aws
-    @patch('console_api.apps.orchestrator.views.create_pipeline_from_json')
-    def test_osi_create_migration_assume_role(self, mock_create_pipeline_from_json):
-        mock_create_pipeline_from_json.return_value = None
-
+    @patch('botocore.client.BaseClient._make_api_call', new=mock_make_api_call)
+    def test_osi_create_migration_assume_role(self):
         payload = dict(VALID_CREATE_PAYLOAD)
         payload['PipelineManagerAssumeRoleArn'] = 'arn:aws:iam::123456789012:role/testRole'
         response = self.client.post('/orchestrator/osi-create-migration', data=payload,
@@ -61,20 +77,24 @@ class OrchestratorViewsTest(SimpleTestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertIn('Timestamp', response.data)
 
-    @patch('console_api.apps.orchestrator.views.create_pipeline_from_json')
-    def test_osi_create_migration_fails_for_missing_field(self, mock_create_pipeline_from_json):
-        mock_create_pipeline_from_json.return_value = None
-
+    @patch('botocore.client.BaseClient._make_api_call', new=mock_make_api_call)
+    def test_osi_create_migration_fails_for_missing_field(self):
         payload = dict(VALID_CREATE_PAYLOAD)
         payload['AwsRegion'] = None
         response = self.client.post('/orchestrator/osi-create-migration', data=payload,
                                     content_type='application/json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-    @patch('console_api.apps.orchestrator.views.start_pipeline')
-    def test_osi_start_migration(self, mock_start_pipeline):
-        mock_start_pipeline.return_value = None
+    @patch('botocore.client.BaseClient._make_api_call', new=mock_make_api_call)
+    def test_osi_create_migration_fails_for_invalid_auth(self):
+        payload = dict(VALID_CREATE_PAYLOAD)
+        payload['SourceDataProvider'].pop('SecretArn')
+        response = self.client.post('/orchestrator/osi-create-migration', data=payload,
+                                    content_type='application/json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
+    @patch('botocore.client.BaseClient._make_api_call', new=mock_make_api_call)
+    def test_osi_start_migration(self):
         response = self.client.post('/orchestrator/osi-start-migration', data=VALID_UPDATE_PAYLOAD,
                                     content_type='application/json')
 
@@ -82,54 +102,40 @@ class OrchestratorViewsTest(SimpleTestCase):
         self.assertIn('Timestamp', response.data)
 
     @mock_aws
-    @patch('console_api.apps.orchestrator.views.start_pipeline')
-    def test_osi_start_migration_assume_role(self, mock_start_pipeline):
-        mock_start_pipeline.return_value = None
-
+    @patch('botocore.client.BaseClient._make_api_call', new=mock_make_api_call)
+    def test_osi_start_migration_assume_role(self):
         response = self.client.post('/orchestrator/osi-start-migration', data=VALID_ASSUME_ROLE_UPDATE_PAYLOAD,
                                     content_type='application/json')
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertIn('Timestamp', response.data)
 
-    @patch('console_api.apps.orchestrator.views.stop_pipeline')
-    def test_osi_stop_migration(self, mock_stop_pipeline):
-        mock_stop_pipeline.return_value = None
-
+    @patch('botocore.client.BaseClient._make_api_call', new=mock_make_api_call)
+    def test_osi_stop_migration(self):
         response = self.client.post('/orchestrator/osi-stop-migration', data=VALID_UPDATE_PAYLOAD,
                                     content_type='application/json')
-
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertIn('Timestamp', response.data)
 
     @mock_aws
-    @patch('console_api.apps.orchestrator.views.stop_pipeline')
-    def test_osi_stop_migration_assume_role(self, mock_stop_pipeline):
-        mock_stop_pipeline.return_value = None
-
+    @patch('botocore.client.BaseClient._make_api_call', new=mock_make_api_call)
+    def test_osi_stop_migration_assume_role(self):
         response = self.client.post('/orchestrator/osi-stop-migration', data=VALID_ASSUME_ROLE_UPDATE_PAYLOAD,
                                     content_type='application/json')
-
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertIn('Timestamp', response.data)
 
-    @patch('console_api.apps.orchestrator.views.delete_pipeline')
-    def test_osi_delete_migration(self, mock_delete_pipeline):
-        mock_delete_pipeline.return_value = None
-
+    @patch('botocore.client.BaseClient._make_api_call', new=mock_make_api_call)
+    def test_osi_delete_migration(self):
         response = self.client.post('/orchestrator/osi-delete-migration', data=VALID_UPDATE_PAYLOAD,
                                     content_type='application/json')
-
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertIn('Timestamp', response.data)
 
     @mock_aws
-    @patch('console_api.apps.orchestrator.views.delete_pipeline')
-    def test_osi_delete_migration_assume_role(self, mock_delete_pipeline):
-        mock_delete_pipeline.return_value = None
-
+    @patch('botocore.client.BaseClient._make_api_call', new=mock_make_api_call)
+    def test_osi_delete_migration_assume_role(self):
         response = self.client.post('/orchestrator/osi-delete-migration', data=VALID_ASSUME_ROLE_UPDATE_PAYLOAD,
                                     content_type='application/json')
-
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertIn('Timestamp', response.data)
