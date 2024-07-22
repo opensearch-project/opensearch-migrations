6556843bcfc61cfe2c6f5f19ac124f41f69a78b2
Andre Kurait <akurait@amazon.com>
2024-07-22 15:33:40 -0500

    Update for PR Comments
    Signed-off-by: Andre Kurait <akurait@amazon.com>

--- TrafficCapture/trafficReplayer/src/main/java/org/opensearch/migrations/transform/SigV4AuthTransformerFactory.java
+++ TrafficCapture/trafficReplayer/src/main/java/org/opensearch/migrations/transform/SigV4AuthTransformerFactory.java
@@ -2,11 +2,15 @@ package org.opensearch.migrations.transform;
 
 import java.nio.ByteBuffer;
 import java.time.Clock;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.function.Supplier;
 
+import org.opensearch.migrations.IHttpMessage;
 import org.opensearch.migrations.aws.SigV4Signer;
 import org.opensearch.migrations.replay.datahandlers.http.HttpJsonMessageWithFaultingPayload;
-import org.opensearch.migrations.replay.datahandlers.http.helpers.IHttpMessageAdapter;
 
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 
@@ -45,5 +49,45 @@ public class SigV4AuthTransformerFactory implements IAuthTransformerFactory {
                 msg.headers().putAll(signatureHeaders);
             }
         };
+    }
+
+    private interface IHttpMessageAdapter {
+        static IHttpMessage toIHttpMessage(HttpJsonMessageWithFaultingPayload message) {
+            return new IHttpMessage() {
+                @Override
+                public String method() {
+                    return (String) message.get(JsonKeysForHttpMessage.METHOD_KEY);
+                }
+
+                @Override
+                public String path() {
+                    return (String) message.get(JsonKeysForHttpMessage.URI_KEY);
+                }
+
+                @Override
+                public String protocol() {
+                    return (String) message.get(JsonKeysForHttpMessage.PROTOCOL_KEY);
+                }
+
+                @Override
+                public Map<String, List<String>> headers() {
+                    Map<String, Object> originalHeaders = message.headers();
+                    Map<String, List<String>> convertedHeaders = new LinkedHashMap<>();
+
+                    for (Map.Entry<String, Object> entry : originalHeaders.entrySet()) {
+                        if (entry.getValue() instanceof List) {
+                            @SuppressWarnings("unchecked")
+                            List<String> values = (List<String>) entry.getValue();
+                            convertedHeaders.put(entry.getKey(), values);
+                        } else if (entry.getValue() != null) {
+                            convertedHeaders.put(entry.getKey(), Collections.singletonList(entry.getValue().toString()));
+                        }
+                    }
+
+                    return Collections.unmodifiableMap(convertedHeaders);
+
+                }
+            };
+        }
     }
 }
