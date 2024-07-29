5d838251ea9c898a321159f36ec82b62abd81ee4
Chris Helma <chelma+github@amazon.com>
2024-07-29 09:39:12 -0500

    Make Spotless happier
    Signed-off-by: Chris Helma <chelma+github@amazon.com>

--- RFS/src/test/java/com/rfs/cms/OpenSearchWorkCoodinatorTest.java
+++ RFS/src/test/java/com/rfs/cms/OpenSearchWorkCoodinatorTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+
 import org.opensearch.migrations.testutils.CloseableLogSetup;
 
 import com.rfs.cms.OpenSearchWorkCoordinator.DocumentModificationResult;
