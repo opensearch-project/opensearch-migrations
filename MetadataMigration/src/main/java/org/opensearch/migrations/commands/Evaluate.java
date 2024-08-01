c88dff68f997e1c916589e5e4192f22cc9b5f32b
Peter Nied <peternied@hotmail.com>
2024-08-01 20:40:13 +0000

    Wire up not implemented evaluate command
    Signed-off-by: Peter Nied <peternied@hotmail.com>
    Signed-off-by: Arun Lakshmanan <arnlaksh@amazon.com>

--- MetadataMigration/src/main/java/org/opensearch/migrations/commands/Evaluate.java
+++ MetadataMigration/src/main/java/org/opensearch/migrations/commands/Evaluate.java
@@ -1,8 +1,12 @@
 package org.opensearch.migrations.commands;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class Evaluate {
 
     public EvaluateResult execute() {
-        return null;
+        log.atError().setMessage("configure is not supported").log();
+        return new EvaluateResult(9999);
     }
 }
