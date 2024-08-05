89394f54bfa4f22d3cdc460fc71bec466632f54e
Chris Helma <chelma+github@amazon.com>
2024-08-05 11:47:01 -0500

    Fixed bug in new RFS entrypoint script
    Signed-off-by: Chris Helma <chelma+github@amazon.com>

--- DocumentsFromSnapshotMigration/docker/entrypoint.sh
+++ DocumentsFromSnapshotMigration/docker/entrypoint.sh
@@ -6,9 +6,18 @@ set -e
 # Print our ENV variables
 echo "RFS_COMMAND: $RFS_COMMAND"
 echo "RFS_TARGET_USER: $RFS_TARGET_USER"
+echo "RFS_TARGET_PASSWORD: <redacted>"
 echo "RFS_TARGET_PASSWORD_ARN: $RFS_TARGET_PASSWORD_ARN"
 
-# Check if the RFS Command already contains a password; only do special work in if does not
+# Check if the RFS Command already contains a username; only do special work if it does not
+if [[ $RFS_COMMAND != *"--target-username"* ]]; then
+    if [[ -n "$RFS_TARGET_USER" ]]; then
+        echo "Using username from ENV variable RFS_TARGET_USER.  Updating RFS Command with username."
+        RFS_COMMAND="$RFS_COMMAND --target-username $RFS_TARGET_USER"
+    fi
+fi
+
+# Check if the RFS Command already contains a password; only do special work if it does not
 if [[ $RFS_COMMAND != *"--target-password"* ]]; then
     PASSWORD_TO_USE=""
 
@@ -16,14 +25,17 @@ if [[ $RFS_COMMAND != *"--target-password"* ]]; then
     if [[ -n "$RFS_TARGET_PASSWORD" ]]; then
         echo "Using plaintext password from ENV variable RFS_TARGET_PASSWORD"
         PASSWORD_TO_USE="$RFS_TARGET_PASSWORD"
-    else
-        echo "Using password from AWS Secrets Manager"
-        PASSWORD_TO_USE=$(aws secretsmanager get-secret-value --secret-id $RFS_TARGET_PASSWORD_ARN --query SecretString --output text)
+    elif [[ -n "$RFS_TARGET_PASSWORD_ARN" ]]; then
+        # Retrieve password from AWS Secrets Manager if ARN is provided
+        echo "Using password from AWS Secrets Manager ARN in ENV variable RFS_TARGET_PASSWORD_ARN"
+        PASSWORD_TO_USE=$(aws secretsmanager get-secret-value --secret-id "$RFS_TARGET_PASSWORD_ARN" --query SecretString --output text)
     fi
 
-    # Append the password to the RFS Command
-    echo "Updated RFS Command with username/password"
-    RFS_COMMAND="$RFS_COMMAND --target-username $RFS_TARGET_USER --target-password $PASSWORD_TO_USE"
+    # Append the username/password to the RFS Command if have an updated password
+    if [[ -n "$PASSWORD_TO_USE" ]]; then
+        echo "Updating RFS Command with password."
+        RFS_COMMAND="$RFS_COMMAND --target-password $PASSWORD_TO_USE"
+    fi
 fi
 
 echo "Executing RFS Command"
