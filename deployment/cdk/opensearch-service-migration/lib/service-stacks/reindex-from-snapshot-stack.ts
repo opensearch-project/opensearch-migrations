64c7a2ca282943c90f6d0b04c2c1cacd54211d8b
Mikayla Thompson <thomika@amazon.com>
2024-08-09 10:20:06 -0600

    Address review comments
    Signed-off-by: Mikayla Thompson <thomika@amazon.com>

--- deployment/cdk/opensearch-service-migration/lib/service-stacks/reindex-from-snapshot-stack.ts
+++ deployment/cdk/opensearch-service-migration/lib/service-stacks/reindex-from-snapshot-stack.ts
@@ -22,7 +22,7 @@ export interface ReindexFromSnapshotProps extends StackPropsExt {
     readonly vpc: IVpc,
     readonly fargateCpuArch: CpuArchitecture,
     readonly extraArgs?: string,
-    readonly otelCollectorEnabled?: boolean,
+    readonly otelCollectorEnabled: boolean,
     readonly clusterAuthDetails: ClusterYaml
 }
 
@@ -68,7 +68,7 @@ export class ReindexFromSnapshotStack extends MigrationServiceCore {
         });
         const s3Uri = `s3://migration-artifacts-${this.account}-${props.stage}-${this.region}/rfs-snapshot-repo`;
         let rfsCommand = `/rfs-app/runJavaWithClasspath.sh com.rfs.RfsMigrateDocuments --s3-local-dir /tmp/s3_files --s3-repo-uri ${s3Uri} --s3-region ${this.region} --snapshot-name rfs-snapshot --lucene-dir '/lucene' --target-host ${osClusterEndpoint}`
-        rfsCommand = props.otelCollectorEnabled ? rfsCommand.concat(` --otelCollectorEndpoint http://localhost:${OtelCollectorSidecar.OTEL_CONTAINER_PORT}`) : rfsCommand
+        rfsCommand = props.otelCollectorEnabled ? rfsCommand.concat(` --otel-collector-endpoint ${OtelCollectorSidecar.getOtelLocalhostEndpoint()}`) : rfsCommand
         rfsCommand = parseAndMergeArgs(rfsCommand, props.extraArgs);
 
         let targetUser = "";
