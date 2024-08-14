a3e2a827777f048e03e71a950880b3630f02bab4
Chris Helma <25470211+chelma@users.noreply.github.com>
2024-08-14 12:32:27 -0500

    Added RFS support for Deletes/Updates on ES 7.10+; _source doc bugfix (#894)
    * Added RFS support for Deletes/Updates on ES 7.10+; _source doc bugfix
    
    Signed-off-by: Chris Helma <chelma+github@amazon.com>
    
    * Make Spotless happier
    
    Signed-off-by: Chris Helma <chelma+github@amazon.com>
    
    * A couple minor fixes per PR
    
    Signed-off-by: Chris Helma <chelma+github@amazon.com>
    
    ---------
    
    Signed-off-by: Chris Helma <chelma+github@amazon.com>

--- RFS/src/main/java/com/rfs/common/DocumentReindexer.java
+++ RFS/src/main/java/com/rfs/common/DocumentReindexer.java
@@ -2,19 +2,21 @@ package com.rfs.common;
 
 import java.util.List;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.lucene.document.Document;
 
 import org.opensearch.migrations.reindexer.tracing.IDocumentMigrationContexts;
 
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+@Slf4j
 @RequiredArgsConstructor
 public class DocumentReindexer {
-    private static final Logger logger = LogManager.getLogger(DocumentReindexer.class);
+    private static final ObjectMapper objectMapper = new ObjectMapper();
     protected final OpenSearchClient client;
     private final int numDocsPerBulkRequest;
     private final int maxConcurrentRequests;
@@ -28,25 +30,32 @@ public class DocumentReindexer {
         return documentStream.map(this::convertDocumentToBulkSection)  // Convert each Document to part of a bulk
                                                                        // operation
             .buffer(numDocsPerBulkRequest) // Collect until you hit the batch size
-            .doOnNext(bulk -> logger.info("{} documents in current bulk request", bulk.size()))
+            .doOnNext(bulk -> log.atInfo().log("{} documents in current bulk request", bulk.size()))
             .map(this::convertToBulkRequestBody)  // Assemble the bulk request body from the parts
             .flatMap(
                 bulkJson -> client.sendBulkRequest(indexName, bulkJson, context.createBulkRequest()) // Send the request
-                    .doOnSuccess(unused -> logger.debug("Batch succeeded"))
-                    .doOnError(error -> logger.error("Batch failed", error))
+                    .doOnSuccess(unused -> log.atDebug().log("Batch succeeded"))
+                    .doOnError(error -> log.atError().log("Batch failed", error))
                     // Prevent the error from stopping the entire stream, retries occurring within sendBulkRequest
                     .onErrorResume(e -> Mono.empty()),
                 maxConcurrentRequests)
-            .doOnComplete(() -> logger.debug("All batches processed"))
+            .doOnComplete(() -> log.atDebug().log("All batches processed"))
             .then();
     }
 
+    @SneakyThrows
     private String convertDocumentToBulkSection(Document document) {
         String id = Uid.decodeId(document.getBinaryValue("_id").bytes);
-        String source = document.getBinaryValue("_source").utf8ToString();
+
         String action = "{\"index\": {\"_id\": \"" + id + "\"}}";
 
-        return action + "\n" + source;
+        // We must ensure the _source document is a "minified" JSON string, otherwise the bulk request will be corrupted.
+        // Specifically, we cannot have any leading or trailing whitespace, and the JSON must be on a single line.
+        String trimmedSource = document.getBinaryValue("_source").utf8ToString().trim();
+        Object jsonObject = objectMapper.readValue(trimmedSource, Object.class);
+        String minifiedSource = objectMapper.writeValueAsString(jsonObject);
+
+        return action + "\n" + minifiedSource;
     }
 
     private String convertToBulkRequestBody(List<String> bulkSections) {
@@ -54,6 +63,8 @@ public class DocumentReindexer {
         for (String section : bulkSections) {
             builder.append(section).append("\n");
         }
+        log.atDebug().log("Bulk request body: \n{}", builder.toString());
+
         return builder.toString();
     }
 }
