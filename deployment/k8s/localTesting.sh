3e7d858b2bfa00a97792a9084160b5425e6fd132
Greg Schohn <greg.schohn@gmail.com>
2024-09-02 07:06:58 -0400

    more k8s & helm experimentation. There's a testObservability chart that will deploy jaeger, prometheus, and grafana - testing values are within the helmValues/localTesting/testObservability.yaml file.
    Signed-off-by: Greg Schohn <greg.schohn@gmail.com>

--- deployment/k8s/localTesting.sh
+++ deployment/k8s/localTesting.sh
@@ -3,14 +3,18 @@ helm repo add prometheus-community https://prometheus-community.github.io/helm-c
 helm repo add grafana https://grafana.github.io/helm-charts
 helm repo add jaegertracing https://jaegertracing.github.io/helm-charts
 helm repo add strimzi https://strimzi.io/charts/
+helm repo add opensearch https://opensearch-project.github.io/helm-charts/
 
 helm repo update
+
+# just the operator, not any clusters
+helm install strimzi-cluster-operator --set replicas=1 --version 0.43.0 oci://quay.io/strimzi-helm/strimzi-kafka-operator
+helm install capture-traffic-kafka-cluster ./captured-traffic-kafka-cluster --set environment=test
 
 helm install prometheus prometheus-community/prometheus
 helm install grafana grafana/grafana
 helm install jaeger jaegertracing/jaeger
-
-helm install strimzi-cluster-operator --set replicas=1 --version 0.43.0 oci://quay.io/strimzi-helm/strimzi-kafka-operator
+helm install target opensearch/opensearch --version 2.21.0 --values ChartValues/localtesting/opensearchTarget.yaml
 
 kubectl apply -f captureproxy.yaml
 kubectl apply -f elasticsearch.yaml
