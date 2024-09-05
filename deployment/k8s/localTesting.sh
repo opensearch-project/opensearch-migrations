10fac7dd30bc3702c77cff4700c04676287439cd
Greg Schohn <greg.schohn@gmail.com>
2024-09-05 11:06:46 -0400

    K8s Checkpoint.  With minimal testing, the proxy, ES/OS, grafana seem to work as expected. Jaeger & Prometheus have been deployed, but I don't have a collector and haven't confirmed that they're behaving as expected. I still need to 1) setup kafka, flip the proxy to write to kafka; 2) setup the replayer 3) setup otel-collectors; 4) setup RFS.
    Signed-off-by: Greg Schohn <greg.schohn@gmail.com>

--- deployment/k8s/localTesting.sh
+++ deployment/k8s/localTesting.sh
@@ -1,28 +1,25 @@
 minikube start
 eval $(minikube docker-env)
 
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm repo add grafana https://grafana.github.io/helm-charts
-helm repo add jaegertracing https://jaegertracing.github.io/helm-charts
 helm repo add strimzi https://strimzi.io/charts/
-helm repo add opensearch https://opensearch-project.github.io/helm-charts/
-
 helm repo update
+
+helm dependency build  mockCustomerClusters # not sure if this is required
+helm upgrade mock-context  mockCustomerClusters --values ../helmValues/localTesting/sourceElasticsearchCluster.yaml --values ../helmValues/localTesting/targetOpenSearchCluster.yaml --values ../helmValues/localTesting/captureProxy.yaml
+kubectl port-forward service/capture-proxy 9200:9200 &
+kubectl port-forward service/elasticsearch 19200:9200 &
+kubectl port-forward service/opensearch 29200:9200 &
+
+helm dependency build  testObervability # not sure if this is required
+helm install observability ./testObervability --values ../helmValues/localTesting/grafana.yaml --values ../helmValues/localTesting/jaeger.yaml
+# kubectl get secret observability-grafana -o jsonpath="{.data.admin-password}" | base64 --decode ; echo
+kc port-forward service/observability-grafana 3000:80
+
+# this hasn't been tested recently and will be folded into packages
 
 # just the operator, not any clusters
 helm install strimzi-cluster-operator --set replicas=1 --version 0.43.0 oci://quay.io/strimzi-helm/strimzi-kafka-operator
 helm install capture-traffic-kafka-cluster ./capturedTrafficKafkaCluster --set environment=test
 helm install replayer ./replayer
 
-helm install prometheus prometheus-community/prometheus
-helm install grafana grafana/grafana
-helm install jaeger jaegertracing/jaeger
 helm install target opensearch/opensearch --version 2.21.0 --values ChartValues/localtesting/opensearchTarget.yaml
-
-kubectl apply -f captureproxy.yaml
-kubectl apply -f elasticsearch.yaml
-kubectl apply -f opensearch.yaml
-kubectl port-forward service/capture-proxy 9200:9200 &
-kubectl port-forward service/elasticsearch 19200:9200 &
-kubectl port-forward service/opensearch 29200:9200 &
-tail -f /dev/null
