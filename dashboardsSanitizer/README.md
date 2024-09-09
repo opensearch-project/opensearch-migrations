b5662f9fd25f0835340d52daa1e44846ae9d5687
Milan Gyalai <gyalai@amazon.com>
2024-09-09 23:49:22 +0200

    chore: include more documentation
    Signed-off-by: Milan Gyalai <gyalai@amazon.com>

--- dashboardsSanitizer/README.md
+++ dashboardsSanitizer/README.md
@@ -5,12 +5,34 @@ If the Kibana objects are exported from 7.10.2 or later version, it may not be l
 
 ### How to use this tool?
 
+Clone the repository and navigate to the root directory of the repository.
+```shell
+git clone https://github.com/opensearch-project/opensearch-migrations.git
+```
+
 From the parent project, run the following command. The input file should be in *ndjson* format.
 ```shell
 ./gradlew dashboardsSanitizer:run --args='--source <<Your kibana object file location>> [--output <<output file location>>]'
 ```
 
+#### Example
+```shell
+./gradlew dashboardsSanitizer:run --args='--source opensearch-export.ndjson --output sanitized.ndjson'
+```
+
 ### Known limitations
 
-* This has not been tested with Kibana version 8.x onwards
+* This has not been tested with Kibana version 8.7.0 onwards
 * It removes the summary line in the output
+* It does not attempt to convert non-compatible kibana objects like, *lens*, *canvas*, etc...
+
+### Compatibility
+
+|Saved Object Type|Supported Versions|Notes|
+|---|---|---|
+|dashboard|7.10.2 ... 8.8.0| lens, map, canvas-workpad, canvas-element, graph-workspace, connector, rule, action are not supported and will be omitted |
+|visualization|7.10.2 ... 8.8.0| |
+|search|7.10.2 ... 8.8.0| |
+|index-pattern|7.10.2 ... 8.8.0| |
+|url|7.10.2 ... 8.8.0| Only legacyUrl type is supported |
+|query|7.10.2 ... 8.8.0| |
---
+++ dashboardsSanitizer/src/test/java/org/opensearch/migrations/dashboards/util/StatsTest.java
@@ -0,0 +1,60 @@
+package org.opensearch.migrations.dashboards.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class StatsTest {
+    private Stats stats;
+
+    @BeforeEach
+    public void setup() {
+        stats = new Stats();
+    }
+
+    @Test
+    public void testRegisterSkipped() {
+        stats.registerSkipped("dashboard");
+        assertEquals(1, stats.getSkipped().getCount());
+        assertEquals(1, stats.getTotal());
+        assertEquals(1, stats.getSkipped().getDetails().get("dashboard"));
+
+        stats.registerSkipped("visualization");
+        stats.registerSkipped("visualization");
+        assertEquals(3, stats.getSkipped().getCount());
+        assertEquals(3, stats.getTotal());
+        assertEquals(2, stats.getSkipped().getDetails().get("visualization"));
+    }
+
+    @Test
+    public void testRegisterProcessed() {
+        stats.registerProcessed();
+        assertEquals(1, stats.getProcessed());
+        assertEquals(1, stats.getTotal());
+
+        stats.registerProcessed();
+        stats.registerProcessed();
+        assertEquals(3, stats.getProcessed());
+        assertEquals(3, stats.getTotal());
+    }
+
+    @Test
+    public void testPrintStats() throws JsonProcessingException {
+        stats.registerSkipped("dashboard");
+        stats.registerSkipped("visualization");
+        stats.registerProcessed();
+
+        String expectedJson = new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(
+            new ObjectMapper().createObjectNode()
+                .put("total", 3)
+                .put("processed", 1)
+                .set("skipped", new ObjectMapper().valueToTree(stats.getSkipped()))
+        );
+
+        assertEquals(expectedJson, stats.printStats());
+    }
+}
