2a81a1835a75485c9d02aa67f2d5afac512376a9
Andre Kurait <akurait@amazon.com>
2024-09-10 13:14:04 -0500

    Spotless Apply
    Signed-off-by: Andre Kurait <akurait@amazon.com>

--- coreUtilities/src/test/java/org/opensearch/migrations/tracing/LoggingVerificationTest.java
+++ coreUtilities/src/test/java/org/opensearch/migrations/tracing/LoggingVerificationTest.java
@@ -1,9 +1,11 @@
 package org.opensearch.migrations.tracing;
 
 import java.util.concurrent.atomic.AtomicInteger;
-import lombok.extern.slf4j.Slf4j;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class LoggingVerificationTest {
