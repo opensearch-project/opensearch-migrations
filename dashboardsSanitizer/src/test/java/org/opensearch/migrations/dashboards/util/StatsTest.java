18edadd5d8518d6989d5d60b0b146ab72c9eb700
Milan Gyalai <gyalai@amazon.com>
2024-09-10 17:30:03 +0200

    fix: missing import and spotless findings
    Signed-off-by: Milan Gyalai <gyalai@amazon.com>

--- dashboardsSanitizer/src/test/java/org/opensearch/migrations/dashboards/util/StatsTest.java
+++ dashboardsSanitizer/src/test/java/org/opensearch/migrations/dashboards/util/StatsTest.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class StatsTest {
