a5c4b7900b7e64b17b724fb7dc05456ca29d94e7
Mikayla Thompson <thomika@amazon.com>
2024-09-18 11:20:02 -0600

    Improve migration console cli.py test coverage (#968)
    * Improve migration console cli.py test coverage
    
    Signed-off-by: Mikayla Thompson <thomika@amazon.com>

--- TrafficCapture/dockerSolution/src/main/docker/migrationConsole/lib/console_link/console_link/cli.py
+++ TrafficCapture/dockerSolution/src/main/docker/migrationConsole/lib/console_link/console_link/cli.py
@@ -7,6 +7,7 @@ import console_link.middleware.backfill as backfill_
 import console_link.middleware.snapshot as snapshot_
 import console_link.middleware.metadata as metadata_
 import console_link.middleware.replay as replay_
+import console_link.middleware.kafka as kafka_
 
 from console_link.models.utils import ExitCode
 from console_link.environment import Environment
@@ -397,7 +398,7 @@ def kafka_group(ctx):
 @click.option('--topic-name', default="logging-traffic-topic", help='Specify a topic name to create')
 @click.pass_obj
 def create_topic_cmd(ctx, topic_name):
-    result = ctx.env.kafka.create_topic(topic_name=topic_name)
+    result = kafka_.create_topic(ctx.env.kafka, topic_name=topic_name)
     click.echo(result.value)
 
 
@@ -408,13 +409,13 @@ def create_topic_cmd(ctx, topic_name):
 @click.pass_obj
 def delete_topic_cmd(ctx, acknowledge_risk, topic_name):
     if acknowledge_risk:
-        result = ctx.env.kafka.delete_topic(topic_name=topic_name)
+        result = kafka_.delete_topic(ctx.env.kafka, topic_name=topic_name)
         click.echo(result.value)
     else:
         if click.confirm('Deleting a topic will irreversibly delete all captured traffic records stored in that '
                          'topic. Are you sure you want to continue?'):
             click.echo(f"Performing delete topic operation on {topic_name} topic...")
-            result = ctx.env.kafka.delete_topic(topic_name=topic_name)
+            result = kafka_.delete_topic(ctx.env.kafka, topic_name=topic_name)
             click.echo(result.value)
         else:
             click.echo("Aborting command.")
@@ -424,7 +425,7 @@ def delete_topic_cmd(ctx, acknowledge_risk, topic_name):
 @click.option('--group-name', default="logging-group-default", help='Specify a group name to describe')
 @click.pass_obj
 def describe_group_command(ctx, group_name):
-    result = ctx.env.kafka.describe_consumer_group(group_name=group_name)
+    result = kafka_.describe_consumer_group(ctx.env.kafka, group_name=group_name)
     click.echo(result.value)
 
 
@@ -432,7 +433,7 @@ def describe_group_command(ctx, group_name):
 @click.option('--topic-name', default="logging-traffic-topic", help='Specify a topic name to describe')
 @click.pass_obj
 def describe_topic_records_cmd(ctx, topic_name):
-    result = ctx.env.kafka.describe_topic_records(topic_name=topic_name)
+    result = kafka_.describe_topic_records(ctx.env.kafka, topic_name=topic_name)
     click.echo(result.value)
 
 # ##################### UTILITIES ###################
