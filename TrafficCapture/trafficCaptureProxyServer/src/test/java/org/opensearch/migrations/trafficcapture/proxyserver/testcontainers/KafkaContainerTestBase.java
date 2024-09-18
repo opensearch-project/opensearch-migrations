8c315393e791c8c5b36dd56f2f472835149161e6
Andre Kurait <akurait@amazon.com>
2024-09-18 15:42:32 -0500

    Use Same Kafka Version
    Signed-off-by: Andre Kurait <akurait@amazon.com>

--- TrafficCapture/trafficCaptureProxyServer/src/test/java/org/opensearch/migrations/trafficcapture/proxyserver/testcontainers/KafkaContainerTestBase.java
+++ TrafficCapture/trafficCaptureProxyServer/src/test/java/org/opensearch/migrations/trafficcapture/proxyserver/testcontainers/KafkaContainerTestBase.java
@@ -6,7 +6,7 @@ import org.testcontainers.utility.DockerImageName;
 public class KafkaContainerTestBase extends TestContainerTestBase<KafkaContainer> {
 
     private static final KafkaContainer kafka = new KafkaContainer(
-        DockerImageName.parse("confluentinc/cp-kafka:latest")
+        DockerImageName.parse("confluentinc/cp-kafka:7.5.0")
     );
 
     public KafkaContainer getContainer() {
