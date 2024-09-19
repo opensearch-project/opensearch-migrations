569821d5608ee8656e34432a056b6ca885648b2c
Peter Nied <peternied@hotmail.com>
2024-09-19 18:33:32 +0000

    Fix code smells in metadata commands
    - Rename this constant name to match the regular expression [...]
    - Replace these toUpperCase()/toLowerCase() and equals() calls with a single equalsIgnoreCase() call.
    
    Signed-off-by: Peter Nied <peternied@hotmail.com>

--- MetadataMigration/src/main/java/org/opensearch/migrations/MetadataCommands.java
+++ MetadataMigration/src/main/java/org/opensearch/migrations/MetadataCommands.java
@@ -3,14 +3,14 @@ package org.opensearch.migrations;
 /** The list of supported commands for the metadata tool */
 public enum MetadataCommands {
     /** Migrates items from a source and recreates them on the target cluster */
-    Migrate,
+    MIGRATE,
 
     /** Inspects items from a source to determine which can be placed on a target cluster */
-    Evaluate;
+    EVALUATE;
 
     public static MetadataCommands fromString(String s) {
         for (var command : values()) {
-            if (command.name().toLowerCase().equals(s.toLowerCase())) {
+            if (command.name().equalsIgnoreCase(s)) {
                 return command;
             }
         }
