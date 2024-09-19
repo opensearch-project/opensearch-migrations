cd54b1a41429b134a199794929347b273c9f2554
Andre Kurait <akurait@amazon.com>
2024-09-19 10:42:14 -0500

    Use shared constants for test docker image names
    Signed-off-by: Andre Kurait <akurait@amazon.com>

--- TrafficCapture/trafficCaptureProxyServer/src/test/java/org/opensearch/migrations/trafficcapture/proxyserver/testcontainers/HttpdContainerTestBase.java
+++ TrafficCapture/trafficCaptureProxyServer/src/test/java/org/opensearch/migrations/trafficcapture/proxyserver/testcontainers/HttpdContainerTestBase.java
@@ -1,11 +1,13 @@
 package org.opensearch.migrations.trafficcapture.proxyserver.testcontainers;
 
+import org.opensearch.migrations.testutils.SharedDockerImageNames;
+
 import org.testcontainers.containers.GenericContainer;
 
 public class HttpdContainerTestBase extends TestContainerTestBase<GenericContainer<?>> {
 
-    private static final GenericContainer<?> httpd = new GenericContainer("httpd:alpine").withExposedPorts(80); // Container
-                                                                                                                // Port
+    private static final GenericContainer<?> httpd = new GenericContainer(SharedDockerImageNames.HTTPD)
+        .withExposedPorts(80); // Container Port
 
     public GenericContainer<?> getContainer() {
         return httpd;
--- TrafficCapture/trafficCaptureProxyServer/src/test/java/org/opensearch/migrations/trafficcapture/proxyserver/testcontainers/KafkaContainerTestBase.java
+++ TrafficCapture/trafficCaptureProxyServer/src/test/java/org/opensearch/migrations/trafficcapture/proxyserver/testcontainers/KafkaContainerTestBase.java
@@ -1,13 +1,12 @@
 package org.opensearch.migrations.trafficcapture.proxyserver.testcontainers;
 
+import org.opensearch.migrations.testutils.SharedDockerImageNames;
+
 import org.testcontainers.containers.KafkaContainer;
-import org.testcontainers.utility.DockerImageName;
 
 public class KafkaContainerTestBase extends TestContainerTestBase<KafkaContainer> {
 
-    private static final KafkaContainer kafka = new KafkaContainer(
-        DockerImageName.parse("confluentinc/cp-kafka:7.5.0")
-    );
+    private static final KafkaContainer kafka = new KafkaContainer(SharedDockerImageNames.KAFKA);
 
     public KafkaContainer getContainer() {
         return kafka;
