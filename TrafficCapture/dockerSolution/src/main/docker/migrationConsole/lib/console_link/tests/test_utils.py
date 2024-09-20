346ef9c3ae45e192489ea50aae07d3e61bf16f44
Tanner Lewis <lewijacn@amazon.com>
2024-09-20 15:45:27 -0500

    Add tests for create boto3 client
    Signed-off-by: Tanner Lewis <lewijacn@amazon.com>

--- TrafficCapture/dockerSolution/src/main/docker/migrationConsole/lib/console_link/tests/test_utils.py
+++ TrafficCapture/dockerSolution/src/main/docker/migrationConsole/lib/console_link/tests/test_utils.py
@@ -1,8 +1,22 @@
-from console_link.models.utils import append_user_agent_header_for_requests
+from console_link.models.client_options import ClientOptions
+from console_link.models.utils import append_user_agent_header_for_requests,create_boto3_client
 import requests.utils
+
 
 USER_AGENT_EXTRA = "test-user-agent-v1.0"
 
+
+def test_create_boto3_client_no_user_agent():
+    client = create_boto3_client(aws_service_name="ecs")
+    user_agent_for_client = client.meta.config.user_agent
+    assert "Boto3" in user_agent_for_client
+
+def test_create_boto3_client_with_user_agent():
+    client_options = ClientOptions(config={"user_agent_extra": USER_AGENT_EXTRA})
+    client = create_boto3_client(aws_service_name="ecs", client_options=client_options)
+    user_agent_for_client = client.meta.config.user_agent
+    assert "Boto3" in user_agent_for_client
+    assert  USER_AGENT_EXTRA in user_agent_for_client
 
 def test_append_user_agent_header_for_requests_no_headers():
     expected_headers = {"User-Agent": f"{requests.utils.default_user_agent()} {USER_AGENT_EXTRA}"}
