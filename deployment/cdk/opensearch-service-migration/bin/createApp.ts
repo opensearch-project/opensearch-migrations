3cc59c0d834c01484c6373826ac0f261ff2debcb
Tanner Lewis <lewijacn@amazon.com>
2024-09-20 14:44:29 -0500

    Fix user agent from merge conflict
    Signed-off-by: Tanner Lewis <lewijacn@amazon.com>

--- deployment/cdk/opensearch-service-migration/bin/createApp.ts
+++ deployment/cdk/opensearch-service-migration/bin/createApp.ts
@@ -11,15 +11,22 @@ export function createApp(): App {
   const account = process.env.CDK_DEFAULT_ACCOUNT;
   const region = process.env.CDK_DEFAULT_REGION;
   const migrationsAppRegistryARN = process.env.MIGRATIONS_APP_REGISTRY_ARN;
-  const customReplayerUserAgent = process.env.CUSTOM_REPLAYER_USER_AGENT;
 
   if (migrationsAppRegistryARN) {
     console.info(`App Registry mode is enabled for CFN stack tracking. Will attempt to import the App Registry application from the MIGRATIONS_APP_REGISTRY_ARN env variable of ${migrationsAppRegistryARN} and looking in the configured region of ${region}`);
   }
 
+  // Temporarily allow both means for providing an additional migrations User Agent, but remove CUSTOM_REPLAYER_USER_AGENT
+  // in future change
+  let migrationsUserAgent = undefined
+  if (process.env.CUSTOM_REPLAYER_USER_AGENT)
+    migrationsUserAgent = process.env.CUSTOM_REPLAYER_USER_AGENT
+  if (process.env.MIGRATIONS_USER_AGENT)
+    migrationsUserAgent = process.env.MIGRATIONS_USER_AGENT
+
   new StackComposer(app, {
     migrationsAppRegistryARN: migrationsAppRegistryARN,
-    customReplayerUserAgent: customReplayerUserAgent,
+    migrationsUserAgent: migrationsUserAgent,
     migrationsSolutionVersion: version,
     env: { account: account, region: region }
   });
