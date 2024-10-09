08a988b92b49406acfc8e2fa858420f3a64cbcca
Peter Nied <peternied@hotmail.com>
2024-10-09 20:56:42 +0000

    Fix spotless import ordering
    Signed-off-by: Peter Nied <peternied@hotmail.com>

--- RFS/src/test/java/org/opensearch/migrations/cluster/ClusterProviderRegistryTest.java
+++ RFS/src/test/java/org/opensearch/migrations/cluster/ClusterProviderRegistryTest.java
@@ -1,13 +1,13 @@
 package org.opensearch.migrations.cluster;
 
-import org.junit.jupiter.api.Test;
-
 import org.opensearch.migrations.Version;
 import org.opensearch.migrations.bulkload.common.SourceRepo;
 import org.opensearch.migrations.bulkload.common.http.ConnectionContext;
 import org.opensearch.migrations.bulkload.models.DataFilterArgs;
 import org.opensearch.migrations.bulkload.version_es_6_8.SnapshotReader_ES_6_8;
 import org.opensearch.migrations.bulkload.version_os_2_11.RemoteWriter_OS_2_11;
+
+import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.MatcherAssert.assertThat;
