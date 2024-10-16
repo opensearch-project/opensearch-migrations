032227b9509530aa2c78fe95cd82e4d0d348a598
Tanner Lewis <lewijacn@amazon.com>
2024-10-16 15:27:13 -0500

    Basic working Replayer and RFS in local k8
    Signed-off-by: Tanner Lewis <lewijacn@amazon.com>

---
+++ deployment/k8/localSetupMini.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# To have running during local development
+
+# Allow executing this script from any dir
+script_abs_path=$(readlink -f "$0")
+script_dir_abs_path=$(dirname "$script_abs_path")
+cd "$script_dir_abs_path" || exit
+
+cd ../.. || exit
+
+minikube mount .:/opensearch-migrations > /dev/null 2>&1 &
+minikube tunnel > /dev/null 2>&1 &
