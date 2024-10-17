98d13180ea614d512834728786ce0124fae966f1
Tanner Lewis <lewijacn@amazon.com>
2024-10-17 16:02:03 -0400

    Switch integ test level to INFO and minor top level cleanup (#1078)
    Signed-off-by: Tanner Lewis <lewijacn@amazon.com>

--- TrafficCapture/dockerSolution/src/main/docker/migrationConsole/lib/integ_test/integ_test/conftest.py
+++ TrafficCapture/dockerSolution/src/main/docker/migrationConsole/lib/integ_test/integ_test/conftest.py
@@ -5,13 +5,10 @@ import logging
 
 
 def pytest_configure(config):
-    # Configure logging
-    logging.basicConfig(level=logging.DEBUG,
+    # Configure logging, avoid sensitive data at lower logs levels
+    logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s - %(levelname)s - %(message)s',
                         datefmt='%Y-%m-%d %H:%M:%S')
-
-    # This line ensures that log messages are displayed on the console during test runs
-    logging.getLogger().setLevel(logging.DEBUG)
 
 
 def pytest_addoption(parser):
