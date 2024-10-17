d7224ce7f88595de2380216b89ac4b3910d082b6
Peter Nied <petern@amazon.com>
2024-10-17 17:07:59 -0500

    Expand target support to  OpenSearch v1.3 (#1074)
    * Expand target support to  OpenSearch v1.3
    
    - Updated the java end to end test cases to read from the same list for
    target platforms.
    - Update ParallelDocumentMigrationsTest to verify behavior on all supported target cluster types
    
    Signed-off-by: Peter Nied <peternied@hotmail.com>
    Signed-off-by: Peter Nied <petern@amazon.com>

---
+++ RFS/src/testFixtures/java/org/opensearch/migrations/bulkload/SupportedClusters.java
@@ -0,0 +1,31 @@
+package org.opensearch.migrations.bulkload;
+
+import java.util.List;
+
+import org.opensearch.migrations.bulkload.framework.SearchClusterContainer;
+import org.opensearch.migrations.bulkload.framework.SearchClusterContainer.ContainerVersion;
+
+import lombok.experimental.UtilityClass;
+
+/**
+ * Defines all supported clusters
+ */
+@UtilityClass
+public class SupportedClusters {
+
+    public static List<ContainerVersion> sources() {
+        return List.of(
+            SearchClusterContainer.ES_V6_8_23,
+            SearchClusterContainer.ES_V7_10_2,
+            SearchClusterContainer.ES_V7_17,
+            SearchClusterContainer.OS_V1_3_16
+        );
+    }
+
+    public static List<ContainerVersion> targets() {
+        return List.of(
+            SearchClusterContainer.OS_V1_3_16,
+            SearchClusterContainer.OS_V2_14_0
+        );
+    }
+}
