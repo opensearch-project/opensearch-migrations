3d8a2cef109e39c17ca1f147497137e53e9dca14
Tanner Lewis <lewijacn@amazon.com>
2024-11-07 13:16:46 -0600

    Validate endpoints for source and target and add default ports if missing
    Signed-off-by: Tanner Lewis <lewijacn@amazon.com>

--- deployment/cdk/opensearch-service-migration/lib/network-stack.ts
+++ deployment/cdk/opensearch-service-migration/lib/network-stack.ts
@@ -42,33 +42,6 @@ export class NetworkStack extends Stack {
     public readonly albTargetProxyTG: IApplicationTargetGroup;
     public readonly albSourceClusterTG: IApplicationTargetGroup;
 
-    // Validate a proper url string is provided and return an url string which contains a protocol, host name, and port.
-    // If a port is not provided, the default protocol port (e.g. 443, 80) will be explicitly added
-    static validateAndReturnFormattedHttpURL(urlString: string) {
-        // URL will throw error if the urlString is invalid
-        const url = new URL(urlString);
-        if (url.protocol !== "http:" && url.protocol !== "https:") {
-            throw new Error(`Invalid url protocol for target endpoint: ${urlString} was expecting 'http' or 'https'`)
-        }
-        if (url.pathname !== "/") {
-            throw new Error(`Provided target endpoint: ${urlString} must not contain a path: ${url.pathname}`)
-        }
-        // URLs that contain the default protocol port (e.g. 443, 80) will not show in the URL toString()
-        let formattedUrlString = url.toString()
-        if (formattedUrlString.endsWith("/")) {
-            formattedUrlString = formattedUrlString.slice(0, -1)
-        }
-        if (!url.port) {
-            if (url.protocol === "http:") {
-                formattedUrlString = formattedUrlString.concat(":80")
-            }
-            else {
-                formattedUrlString = formattedUrlString.concat(":443")
-            }
-        }
-        return formattedUrlString
-    }
-
     private validateVPC(vpc: IVpc) {
         let uniqueAzPrivateSubnets: string[] = []
         if (vpc.privateSubnets.length > 0) {
@@ -114,7 +87,7 @@ export class NetworkStack extends Stack {
             InterfaceVpcEndpointAwsService.XRAY, // X-Ray Traces
             isStackInGovCloud(this) ?
                 InterfaceVpcEndpointAwsService.ELASTIC_FILESYSTEM_FIPS : // EFS Control Plane GovCloud
-                InterfaceVpcEndpointAwsService.ELASTIC_FILESYSTEM, // EFS Control Plane 
+                InterfaceVpcEndpointAwsService.ELASTIC_FILESYSTEM, // EFS Control Plane
 
         ];
         interfaceEndpoints.forEach(service => createInterfaceVpcEndpoint(service));
@@ -288,9 +261,8 @@ export class NetworkStack extends Stack {
             });
 
             if (props.targetClusterEndpoint) {
-                const formattedClusterEndpoint = NetworkStack.validateAndReturnFormattedHttpURL(props.targetClusterEndpoint);
                 const deployId = props.addOnMigrationDeployId ? props.addOnMigrationDeployId : props.defaultDeployId;
-                createMigrationStringParameter(this, formattedClusterEndpoint, {
+                createMigrationStringParameter(this, props.targetClusterEndpoint, {
                     stage: props.stage,
                     defaultDeployId: deployId,
                     parameter: MigrationSSMParameter.OS_CLUSTER_ENDPOINT
