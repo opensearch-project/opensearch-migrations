ac162492ca2a00794e82bbddf9deeaca2575a25e
Tanner Lewis <lewijacn@amazon.com>
2024-11-22 18:02:35 -0600

    Update to main repo branch and url
    Signed-off-by: Tanner Lewis <lewijacn@amazon.com>

--- vars/solutionsCFNTest.groovy
+++ vars/solutionsCFNTest.groovy
@@ -4,8 +4,8 @@ def call(Map config = [:]) {
         agent { label config.workerAgent ?: 'Jenkins-Default-Agent-X64-C5xlarge-Single-Host' }
 
         parameters {
-            string(name: 'GIT_REPO_URL', defaultValue: 'https://github.com/lewijacn/opensearch-migrations.git', description: 'Git repository url')
-            string(name: 'GIT_BRANCH', defaultValue: 'sol-pipeline', description: 'Git branch to use for repository')
+            string(name: 'GIT_REPO_URL', defaultValue: 'https://github.com/opensearch-project/opensearch-migrations.git', description: 'Git repository url')
+            string(name: 'GIT_BRANCH', defaultValue: 'main', description: 'Git branch to use for repository')
             string(name: 'STAGE', defaultValue: "sol-integ", description: 'Stage name for deployment environment')
         }
 
