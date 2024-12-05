04801d2a22b51700cb37b7697636dabd0a85dfab
Peter Nied <peternied@hotmail.com>
2024-12-05 21:43:37 +0000

    Fix broken unit test with API change
    Signed-off-by: Peter Nied <peternied@hotmail.com>

--- RFS/src/test/java/org/opensearch/migrations/bulkload/version_os_2_11/GlobalMetadataCreator_OS_2_11Test.java
+++ RFS/src/test/java/org/opensearch/migrations/bulkload/version_os_2_11/GlobalMetadataCreator_OS_2_11Test.java
@@ -7,12 +7,12 @@ import java.util.Optional;
 import org.opensearch.migrations.MigrationMode;
 import org.opensearch.migrations.bulkload.common.OpenSearchClient;
 import org.opensearch.migrations.bulkload.models.GlobalMetadata;
-import org.opensearch.migrations.bulkload.version_os_2_11.GlobalMetadataCreator_OS_2_11.TemplateTypes;
 import org.opensearch.migrations.metadata.CreationResult;
 import org.opensearch.migrations.metadata.CreationResult.CreationFailureType;
 import org.opensearch.migrations.metadata.tracing.IMetadataMigrationContexts.IClusterMetadataContext;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -22,7 +22,6 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
@@ -46,15 +45,18 @@ public class GlobalMetadataCreator_OS_2_11Test {
         doReturn(filledOptional).when(client).createIndexTemplate(any(), any(), any());
         doReturn(filledOptional).when(client).createLegacyTemplate(any(), any(), any());
 
-        var creator = spy(new GlobalMetadataCreator_OS_2_11(client, List.of("lit1"), List.of(), null));
-        doReturn(Map.of("lit1", obj, "lit2", obj, ".lits", obj)).when(creator).getAllTemplates(any(), eq(TemplateTypes.LEGACY_INDEX_TEMPLATE));
-        doReturn(Map.of("it1", obj, ".its", obj)).when(creator).getAllTemplates(any(), eq(TemplateTypes.INDEX_TEMPLATE));
-        doReturn(Map.of("ct1", obj, ".cts", obj)).when(creator).getAllTemplates(any(), eq(TemplateTypes.COMPONENT_TEMPLATE));
-
         var globalMetadata = mock(GlobalMetadata.class);
-        doReturn(obj).when(globalMetadata).getComponentTemplates();
-        doReturn(obj).when(globalMetadata).getIndexTemplates();
-        doReturn(obj).when(globalMetadata).getTemplates();
+        var componentTemplates = mapper.createObjectNode().put("type", "component");
+        var indexTemplates = mapper.createObjectNode().put("type", "index");
+        var legacyTemplates = mapper.createObjectNode().put("type", "legacy");
+        doReturn(componentTemplates).when(globalMetadata).getComponentTemplates();
+        doReturn(indexTemplates).when(globalMetadata).getIndexTemplates();
+        doReturn(legacyTemplates).when(globalMetadata).getTemplates();
+
+        var creator = spy(new GlobalMetadataCreator_OS_2_11(client, List.of("lit1"), List.of(), null));
+        doReturn(Map.of("lit1", obj, "lit2", obj, ".lits", obj)).when(creator).getAllTemplates(legacyTemplates);
+        doReturn(Map.of("it1", obj, ".its", obj)).when(creator).getAllTemplates(indexTemplates);
+        doReturn(Map.of("ct1", obj, ".cts", obj)).when(creator).getAllTemplates(componentTemplates);
 
         var results = creator.create(globalMetadata, MigrationMode.PERFORM, context);
         assertThat(results.fatalIssueCount(), equalTo(0L));
