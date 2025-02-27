d1dd0be55e7565d97857e0fef8f5ff27ca1ef68b
Tanner Lewis <lewijacn@amazon.com>
2025-02-27 16:34:42 -0600

    [K8s] Generate migration_services.yaml from ConfigMaps (#1307)
    This change removes the static migration_services.yaml we were using temporarily for K8s to now utilize the groundwork we had in the K8s config map utils library for generating this file from ConfigMap values.
    
    Note: While several attempts were made to keep the existing /etc/migration_services.yaml path on the Migration Console container, K8s does not seem to make it easy to mount only a single file like was done with the Docker compose setup, but rather requires a good bit of misdirection which I didn't want to cause more complexity here. The new path that is utilized here is /config/migration_services.yaml with the entire config directory being mounted on the Migration Console container.
    
    ---------
    
    Signed-off-by: Tanner Lewis <lewijacn@amazon.com>

--- TrafficCapture/dockerSolution/src/main/docker/k8sConfigMapUtilScripts/configmap2yaml/config_watcher.py
+++ TrafficCapture/dockerSolution/src/main/docker/k8sConfigMapUtilScripts/configmap2yaml/config_watcher.py
@@ -1,17 +1,20 @@
 import argparse
-from format_services_yaml import YAMLTemplateConverter
-from io import StringIO
-from kubernetes import client, config, watch
 import logging
 import os
 import signal
 import tempfile
-from typing import Dict, Any
+import time
 import sys
 import yaml
 
+from io import StringIO
+from kubernetes import client, config, watch
+from typing import Dict, Any
 
-logging.basicConfig(level=logging.INFO)
+from configmap2yaml.format_services_yaml import YAMLTemplateConverter
+
+
+logging.basicConfig(format='%(asctime)s [%(levelname)s] %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
@@ -21,7 +24,7 @@ class ConfigMapWatcher:
         self.namespace = namespace
         self.output_file = output_file
         self.current_data: Dict[str, Any] = {}
-        self.formatter = YAMLTemplateConverter()
+        self.formatter = YAMLTemplateConverter(namespace=namespace)
 
         # Validate output file path
         output_dir = os.path.dirname(output_file)
@@ -36,7 +39,7 @@ class ConfigMapWatcher:
             logger.warning("Unable to load in-cluster config, falling back to local kubeconfig")
             config.load_kube_config()
 
-        self.v1 = client.CoreV1Api()
+        self.k8s_client = client.CoreV1Api()
 
     def update_yaml_file(self) -> None:
         """Update the output YAML file with new ConfigMap data"""
@@ -44,7 +47,8 @@ class ConfigMapWatcher:
             # Create a temporary file in the same directory as the target file
             output_dir = os.path.dirname(self.output_file)
             with tempfile.NamedTemporaryFile(mode='w', dir=output_dir, delete=False) as temp_file:
-                YAMLTemplateConverter().convert(StringIO(yaml.safe_dump(self.current_data)), temp_file)
+                YAMLTemplateConverter(namespace=self.namespace).convert(StringIO(yaml.safe_dump(self.current_data)),
+                                                                        temp_file)
                 temp_file.flush()
                 os.fsync(temp_file.fileno())  # Ensure all data is written to disk
 
@@ -61,46 +65,57 @@ class ConfigMapWatcher:
                     pass
             raise
 
-    def watch_configmaps(self) -> None:
-        """Watch ConfigMaps for changes and write the contents at startup and upon an configMap changes"""
-        w = watch.Watch()
-
-        # First, get existing ConfigMaps
-        logger.info(f"Loading existing ConfigMaps for {self.namespace} and {self.label_selector}")
-        existing_configmaps = self.v1.list_namespaced_config_map(
+    def init_config_map_data(self) -> None:
+        """Write initial contents of existing ConfigMaps"""
+        logger.info(f"Loading existing ConfigMaps for '{self.namespace}' namespace and "
+                    f"{self.label_selector if self.label_selector else 'no'} label selector")
+        existing_configmaps = self.k8s_client.list_namespaced_config_map(
             namespace=self.namespace,
             label_selector=self.label_selector
         )
-        logger.info(f"Got configmaps: {existing_configmaps}")
+        logger.debug(f"Got configmaps: {existing_configmaps}")
         for configmap in existing_configmaps.items:
-            logger.info(f"configmap={configmap}")
+            logger.debug(f"configmap={configmap}")
             self.current_data[configmap.metadata.name] = configmap.data if configmap.data else {}
 
         self.update_yaml_file()
 
-        # Then watch for changes
-        try:
-            for event in w.stream(
-                    self.v1.list_namespaced_config_map,
-                    namespace=self.namespace,
-                    label_selector=self.label_selector
-            ):
-                configmap = event['object']
-                event_type = event['type']
+    def watch_configmaps(self) -> None:
+        """Watch ConfigMaps for changes and write the contents upon any configMap changes"""
+        w = watch.Watch()
 
-                if event_type in ['ADDED', 'MODIFIED']:
-                    self.current_data[configmap.metadata.name] = configmap.data if configmap.data else {}
-                elif event_type == 'DELETED':
-                    name = configmap.metadata.name
-                    if name in self.current_data:
-                        logger.info(f"Removing ConfigMap: {name}")
-                        del self.current_data[name]
+        while True:
+            try:
+                self.process_events(k8s_watch=w)
+            except Exception as error:
+                if "Expired" in str(error):
+                    logger.info("Resource version expired, restarting watch...")
+                    time.sleep(5)
+                    continue
+                else:
+                    logger.error(f"Error watching ConfigMaps: {error}")
+                    raise
 
-                self.update_yaml_file()
+    def process_events(self, k8s_watch):
+        for event in k8s_watch.stream(
+                self.k8s_client.list_namespaced_config_map,
+                namespace=self.namespace,
+                label_selector=self.label_selector,
+        ):
+            configmap = event['object']
+            event_type = event['type']
+            logger.info(f"Received {event_type} event for {configmap.metadata.name}")
+            logger.debug(f"Incoming event: {event}")
 
-        except Exception as e:
-            logger.error(f"Error watching ConfigMaps: {e}")
-            raise
+            if event_type in ['ADDED', 'MODIFIED']:
+                self.current_data[configmap.metadata.name] = configmap.data if configmap.data else {}
+            elif event_type == 'DELETED':
+                name = configmap.metadata.name
+                if name in self.current_data:
+                    logger.info(f"Removing ConfigMap: {name}")
+                    del self.current_data[name]
+
+            self.update_yaml_file()
 
 
 def parse_args():
@@ -119,8 +134,8 @@ def parse_args():
     )
     parser.add_argument(
         '--namespace',
-        default=os.getenv('NAMESPACE', 'default'),
-        help='Kubernetes namespace (default: default)'
+        default=os.getenv('NAMESPACE', 'ma'),
+        help='Kubernetes namespace (default: ma)'
     )
     return parser.parse_args()
 
@@ -141,6 +156,7 @@ if __name__ == "__main__":
             namespace=args.namespace,
             output_file=args.outfile
         )
+        watcher.init_config_map_data()
         watcher.watch_configmaps()
     except KeyboardInterrupt:
         # Handle Ctrl+C cleanly too
