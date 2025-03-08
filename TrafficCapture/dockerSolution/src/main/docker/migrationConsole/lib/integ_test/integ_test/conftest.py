0b3881929225d4922c3f4379e4f7a60523fcf085
Tanner Lewis <lewijacn@amazon.com>
2025-03-07 19:57:15 -0600

    Updates for passing test id filtering
    Signed-off-by: Tanner Lewis <lewijacn@amazon.com>

--- TrafficCapture/dockerSolution/src/main/docker/migrationConsole/lib/integ_test/integ_test/conftest.py
+++ TrafficCapture/dockerSolution/src/main/docker/migrationConsole/lib/integ_test/integ_test/conftest.py
@@ -2,6 +2,7 @@
 import pytest
 import uuid
 import logging
+from typing import List
 from .cluster_version import ClusterVersion
 from .test_cases.ma_test_base import ClusterVersionCombinationUnsupported
 from .test_cases.basic_tests import *
@@ -13,8 +14,13 @@ logger = logging.getLogger(__name__)
 
 # Dynamically collect all test case classes
 ALL_TEST_CASES = [cls for cls in globals().values() if isinstance(cls, type)
-                  and cls.__module__.startswith("integ_test.integ_test.test_cases")
+                  and cls.__module__.__contains__("integ_test.test_cases")
                   and cls.__name__.startswith("Test")]
+
+
+def _split_test_ids(option_str: str):
+    # Split the string by comma and strip whitespace
+    return [tid.strip() for tid in option_str.split(",")]
 
 
 def pytest_addoption(parser):
@@ -22,6 +28,8 @@ def pytest_addoption(parser):
     parser.addoption("--stage", action="store", default="dev")
     parser.addoption("--source_version", action="store", default="ES_7.10")
     parser.addoption("--target_version", action="store", default="OS_2.x")
+    parser.addoption("--test_ids", action="store", default=[], type=_split_test_ids,
+                     help="Specify test IDs like '0001,0003' to filter tests to execute")
     parser.addoption("--config_file_path", action="store", default="/config/migration_services.yaml",
                      help="Path to config file for console library")
     parser.addoption("--source_proxy_alb_endpoint", action="store", default=None,
@@ -39,16 +47,29 @@ def pytest_generate_tests(metafunc):
         console_config_path = metafunc.config.getoption("config_file_path")
         console_link_env: Environment = Context(console_config_path).env
         unique_id = metafunc.config.getoption("unique_id")
-        test_cases_param = _generate_test_cases(source_version, target_version, console_config_path, console_link_env, unique_id)
+        test_ids_list = metafunc.config.getoption("test_ids")
+        test_cases_param = _generate_test_cases(source_version, target_version, console_config_path, console_link_env,
+                                                unique_id, test_ids_list)
         metafunc.parametrize("test_cases", test_cases_param)
 
 
+def _filter_test_cases(test_ids_list: List[str]) -> List:
+    if not test_ids_list:
+        return ALL_TEST_CASES
+    filtered_cases = []
+    for case in ALL_TEST_CASES:
+        if test_ids_list and any(tid in str(case) for tid in test_ids_list):
+            filtered_cases.append(case)
+    return filtered_cases
+
+
 def _generate_test_cases(source_version: ClusterVersion, target_version: ClusterVersion, console_config_path: str,
-                         console_link_env: Environment, unique_id: str):
+                         console_link_env: Environment, unique_id: str, test_ids_list: List[str]):
     parallel_test_cases_to_run = []
     isolated_test_cases_to_run = []
     unsupported_test_cases = []
-    for test_case in ALL_TEST_CASES:
+    cases = _filter_test_cases(test_ids_list)
+    for test_case in cases:
         try:
             valid_case = test_case(source_version=source_version, target_version=target_version,
                                    console_config_path=console_config_path, console_link_env=console_link_env,
