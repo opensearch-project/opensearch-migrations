d4912c155f5fc40a9f0237c37931c85b171dee0e
Peter Nied <peternied@hotmail.com>
2025-03-12 17:35:39 +0000

    Remove OS 2.14 references
    Signed-off-by: Peter Nied <peternied@hotmail.com>

--- DocumentsFromSnapshotMigration/src/test/java/org/opensearch/migrations/bulkload/UpgradeTest.java
+++ DocumentsFromSnapshotMigration/src/test/java/org/opensearch/migrations/bulkload/UpgradeTest.java
@@ -36,9 +36,9 @@ public class UpgradeTest extends SourceTestBase {
 
     private static Stream<Arguments> scenarios() {
         var scenarios = Stream.<Arguments>builder();
-        scenarios.add(Arguments.of(SearchClusterContainer.ES_V2_4_6, SearchClusterContainer.ES_V5_6_16, SearchClusterContainer.OS_V2_14_0));
-        scenarios.add(Arguments.of(SearchClusterContainer.ES_V5_6_16, SearchClusterContainer.ES_V6_8_23, SearchClusterContainer.OS_V2_14_0));
-        scenarios.add(Arguments.of(SearchClusterContainer.ES_V6_8_23, SearchClusterContainer.ES_V7_10_2, SearchClusterContainer.OS_V2_14_0));
+        scenarios.add(Arguments.of(SearchClusterContainer.ES_V2_4_6, SearchClusterContainer.ES_V5_6_16, SearchClusterContainer.OS_LATEST));
+        scenarios.add(Arguments.of(SearchClusterContainer.ES_V5_6_16, SearchClusterContainer.ES_V6_8_23, SearchClusterContainer.OS_LATEST));
+        scenarios.add(Arguments.of(SearchClusterContainer.ES_V6_8_23, SearchClusterContainer.ES_V7_10_2, SearchClusterContainer.OS_LATEST));
         return scenarios.build();
     }
 
