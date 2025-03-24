5d274f57bd5ddfb9d9dbf46268c1d0dff2877c45
Andre Kurait <akurait@amazon.com>
2025-03-24 14:39:10 -0500

    Close localJsonTransformer resources when PacketToTransformingHttpHandlerFactory is closed
    Signed-off-by: Andre Kurait <akurait@amazon.com>

--- TrafficCapture/trafficReplayer/src/main/java/org/opensearch/migrations/replay/PacketToTransformingHttpHandlerFactory.java
+++ TrafficCapture/trafficReplayer/src/main/java/org/opensearch/migrations/replay/PacketToTransformingHttpHandlerFactory.java
@@ -1,5 +1,7 @@
 package org.opensearch.migrations.replay;
 
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Supplier;
 
 import org.opensearch.migrations.replay.datahandlers.IPacketFinalizingConsumer;
@@ -15,11 +17,12 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class PacketToTransformingHttpHandlerFactory
-    implements
-        PacketConsumerFactory<TransformedOutputAndResult<ByteBufList>> {
+    implements PacketConsumerFactory<TransformedOutputAndResult<ByteBufList>>, AutoCloseable {
 
     // Using ThreadLocal to ensure thread safety with the json transformers which will be reused
     private final ThreadLocal<IJsonTransformer> localJsonTransformer;
+    private final Set<AutoCloseable> closeableResources = ConcurrentHashMap.newKeySet();
+
     // The authTransformerFactory is ThreadSafe and getAuthTransformer will be called for every request
     private final IAuthTransformerFactory authTransformerFactory;
 
@@ -42,5 +45,13 @@ public class PacketToTransformingHttpHandlerFactory
             new TransformedPacketReceiver(),
             httpTransactionContext
         );
+    }
+
+    @Override
+    public void close() throws Exception {
+        for (AutoCloseable resource : closeableResources) {
+            resource.close();
+        }
+        localJsonTransformer.remove();
     }
 }
