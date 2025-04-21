84022223c5df080e83c6efb13d450ec19fe63d41
Tanner Lewis <lewijacn@amazon.com>
2025-04-21 16:03:49 -0500

    Update default passing for git
    Signed-off-by: Tanner Lewis <lewijacn@amazon.com>

--- jenkins/migrationIntegPipelines/elasticsearch8xK8sLocalTestCover.groovy
+++ jenkins/migrationIntegPipelines/elasticsearch8xK8sLocalTestCover.groovy
@@ -1,9 +1,12 @@
-def gitBranch = params.GIT_BRANCH ?: 'main'
-def gitUrl = params.GIT_REPO_URL ?: 'https://github.com/opensearch-project/opensearch-migrations.git'
+def gitBranch = params.GIT_BRANCH ?: 'es-8-k8s-pipeline'
+def gitUrl = params.GIT_REPO_URL ?: 'https://github.com/lewijacn/opensearch-migrations.git'
 
 library identifier: "migrations-lib@${gitBranch}", retriever: modernSCM(
         [$class: 'GitSCMSource',
          remote: "${gitUrl}"])
 
 // Shared library function (location from root: vars/elasticsearch8xK8sLocalTest.groovy)
-elasticsearch8xK8sLocalTest()
+elasticsearch8xK8sLocalTest(
+    gitUrl: gitUrl,
+    gitBranch: gitBranch
+)
--- vars/elasticsearch8xK8sLocalTest.groovy
+++ vars/elasticsearch8xK8sLocalTest.groovy
@@ -1,5 +1,7 @@
 def call(Map config = [:]) {
     k8sLocalDeployment(
+            gitUrl: config.gitUrl,
+            gitBranch: config.gitBranch,
             jobName: 'elasticsearch-8x-k8s-local-test',
             sourceVersion: 'ES_8.11',
             targetVersion: 'OS_2.19'
