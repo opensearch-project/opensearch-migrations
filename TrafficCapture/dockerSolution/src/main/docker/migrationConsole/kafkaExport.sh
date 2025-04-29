243a59d532b51e23a1c7b82a971edc27873bac48
Tanner Lewis <lewijacn@amazon.com>
2025-04-29 13:48:57 -0500

    Improve Kafka export and loading workflow (#1481)
    This change fixes the currently broken kafka export script and improves it to store both the original connection id and the actual kafka record value.
    
    This also adds a small utility which will take this kafka export gzip file as an argument and load the contents of the export script into Kafka via a Kafka broker endpoint and using the Kafka Producer pattern. Also included are instructions for taking a Kafka export file and loading it into a local K8s environment.
    
    ---------
    
    Signed-off-by: Tanner Lewis <lewijacn@amazon.com>

--- TrafficCapture/dockerSolution/src/main/docker/migrationConsole/kafkaExport.sh
+++ TrafficCapture/dockerSolution/src/main/docker/migrationConsole/kafkaExport.sh
@@ -1,31 +1,33 @@
 #!/bin/bash
 
 broker_endpoints="${MIGRATION_KAFKA_BROKER_ENDPOINTS}"
-msk_auth_settings=""
-kafka_command_settings=""
+kafka_command_config=""
+kafka_consumer_config=""
 s3_bucket_name=""
-partition_offsets=""
-partition_limits=""
 
 if [ -n "$ECS_AGENT_URI" ]; then
-    msk_auth_settings="--kafka-traffic-enable-msk-auth"
-    kafka_command_settings="--command-config aws/msk-iam-auth.properties"
+    kafka_command_config="--command-config aws/msk-iam-auth.properties"
+    kafka_consumer_config="--consumer.config aws/msk-iam-auth.properties"
     account_id=$(aws sts get-caller-identity --query Account --output text)
     s3_bucket_name="migration-artifacts-$account_id-$MIGRATION_STAGE-$AWS_REGION"
 fi
 
+script_dir="$(dirname "$0")"
+abs_script_dir="$(cd "$script_dir" && pwd)"
+output_dir="$abs_script_dir"
 topic="logging-traffic-topic"
 timeout_seconds=60
 enable_s3=false
 
 usage() {
   echo ""
-  echo "Utility script for exporting all currently detected Kafka records to a gzip file, and allowing the option to store this archive file on S3."
+  echo "Utility script for exporting Kafka records to a gzip file, including the option to store this archive file on S3."
   echo ""
   echo "Usage: "
   echo "  ./kafkaExport.sh [OPTIONS] [-- EXTRA_ARGS]"
   echo ""
   echo "Options:"
+  echo "  --output-dir <string>               Specify the output directory the export GZIP file will be written to, e.g. '/shared-logs-output'. Default is the same directory of the script."
   echo "  --timeout-seconds <seconds>         Timeout for how long the process will try to collect the Kafka records. Default is 60 seconds."
   echo "  --enable-s3                         Option to store the created archive on S3."
   echo "  --s3-bucket-name <bucket_name>      Option to specify a given S3 bucket to store the archive."
@@ -35,7 +37,7 @@ usage() {
   echo ""
   echo "Examples:"
   echo "  To export 2 messages from a topic from the beginning"
-  echo "    ./kafkaExport.sh -- --group foo2 --max-messages 2 --from-beginning"
+  echo "    ./kafkaExport.sh -- --max-messages 2 --from-beginning"
   echo ""
   echo "  To export messages from a partition from a specific offset"
   echo "    ./kafkaExport.sh -- --partition 0  --offset 30"
@@ -46,6 +48,10 @@ EXTRA_ARGS=()
 while [[ $# -gt 0 ]]; do
     key="$1"
     case $key in
+        --output-dir)
+            output_dir="$2"
+            shift 2
+            ;;
         --timeout-seconds)
             timeout_seconds="$2"
             shift 2
@@ -78,38 +84,49 @@ while [[ $# -gt 0 ]]; do
 done
 
 # Printing existing offsets in topic
-all_consumers_partition_offsets=$(./kafka/bin/kafka-run-class.sh kafka.tools.GetOffsetShell --broker-list "$broker_endpoints" --topic "$topic" --time -1 $(echo "$kafka_command_settings"))
+all_consumers_partition_offsets=$(./kafka/bin/kafka-run-class.sh kafka.tools.GetOffsetShell --broker-list "$broker_endpoints" --topic "$topic" --time -1 $(echo "$kafka_command_config"))
 comma_sep_all_consumers_partition_offsets="${all_consumers_partition_offsets// /,}"
 echo "Existing offsets from current Kafka topic across all consumer groups: "
 echo "$comma_sep_all_consumers_partition_offsets"
+echo ""
 
 epoch_ts=$(date +%s)
-dir_name="kafka_export_$epoch_ts"
-mkdir -p $dir_name
 archive_name="kafka_export_from_migration_console_$epoch_ts.proto.gz"
+archive_path="$output_dir/$archive_name"
+s3_bucket_uri="s3://$s3_bucket_name/kafka-exports/$archive_name"
 group="exportFromMigrationConsole_$(hostname -s)_$$_$epoch_ts"
-echo "Group name: $group"
 
-SCRIPT_DIR="$(dirname "$0")"
-ABS_SCRIPT_DIR="$(cd "$SCRIPT_DIR" && pwd)"
-export CLASSPATH="$CLASSPATH:${ABS_SCRIPT_DIR}/kafkaCommandLineFormatter.jar"
+export CLASSPATH="$CLASSPATH:$(find "$abs_script_dir" -name 'kafkaCommandLineFormatter-*.jar' | head -n 1)"
+
+echo "Starting kafka export to $archive_path"
+# Build the command as an array
+cmd=(
+  timeout "$timeout_seconds"
+  ./kafka/bin/kafka-console-consumer.sh
+  --bootstrap-server "$broker_endpoints"
+  --topic "$topic"
+  --group "$group"
+  --property print.key=false
+  --property print.timestamp=false
+  --formatter org.opensearch.migrations.utils.kafka.Base64Formatter
+  --property print.value=true
+  --property value.deserializer=org.apache.kafka.common.serialization.ByteArrayDeserializer
+)
+
+# Add dynamic parts
+read -ra kafka_args <<< "$kafka_consumer_config"
+cmd+=("${kafka_args[@]}")
+cmd+=("${EXTRA_ARGS[@]}")
+
+# Print the full command as one string
+echo "Running command:"
+printf '%q ' "${cmd[@]}"
+echo "| gzip -c -9 > \"$archive_path\""
+echo ""
 
 # Execute the command
-set -o xtrace
-timeout "$timeout_seconds" \
-./kafka/bin/kafka-console-consumer.sh \
---bootstrap-server "$broker_endpoints" \
---topic "$topic" \
---property print.key=false \
---property print.timestamp=false \
---formatter org.opensearch.migrations.utils.kafka.Base64Formatter \
---property print.value=true \
---property value.deserializer=org.apache.kafka.common.serialization.ByteArrayDeserializer \
-$(echo "$kafka_command_settings") \
-"${EXTRA_ARGS[@]}" | \
-gzip -c -9 > "$dir_name/$archive_name"
-set +o xtrace
+"${cmd[@]}" | gzip -c -9 > "$archive_path"
 
 if [ "$enable_s3" = true ]; then
-  aws s3 mv "$dir_name/$archive_name" "s3://$s3_bucket_name" && echo "Export has been created: s3://$s3_bucket_name/$archive_name"
+  aws s3 mv "$archive_path" "$s3_bucket_uri" && echo "Export moved to S3: $s3_bucket_uri"
 fi
--- libraries/kafkaCommandLineFormatter/src/main/java/org/opensearch/migrations/utils/kafka/Base64Formatter.java
+++ libraries/kafkaCommandLineFormatter/src/main/java/org/opensearch/migrations/utils/kafka/Base64Formatter.java
@@ -2,14 +2,19 @@ package org.opensearch.migrations.utils.kafka;
 
 
 import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.common.MessageFormatter;
 
 public class Base64Formatter implements MessageFormatter {
+    private static final String DELIMITER = "|";
+
     @Override
     public void writeTo(ConsumerRecord<byte[], byte[]> kafkaRecord, PrintStream out) {
-        out.println(Base64.getEncoder().encodeToString(kafkaRecord.value()));
+        String key = new String(kafkaRecord.key(), StandardCharsets.UTF_8);
+        String value = Base64.getEncoder().encodeToString(kafkaRecord.value());
+        out.println(key + DELIMITER + value);
     }
 }
