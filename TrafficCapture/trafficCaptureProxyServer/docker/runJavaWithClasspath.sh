7caf0e380c39c36fba7f98023a859266b7259db7
Tanner Lewis <lewijacn@amazon.com>
2025-05-13 09:38:39 -0500

    Checkpoint may13
    Signed-off-by: Tanner Lewis <lewijacn@amazon.com>

---
+++ TrafficCapture/trafficCaptureProxyServer/docker/runJavaWithClasspath.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+java -XX:MaxRAMPercentage=80.0 -XX:+ExitOnOutOfMemoryError -XshowSettings:vm -cp "@/app/jib-classpath-file" org.opensearch.migrations.trafficcapture.proxyserver.CaptureProxy "$@"
