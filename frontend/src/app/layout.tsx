4640ff8a39cdb401b4a163322f2f9aa2d22d1d32
Peter Nied <peternied@hotmail.com>
2025-05-20 16:44:46 +0000

    Website navigation sidebar
    Include a side bar with help links related to the OpenSearch Migration
    project.  In the code there are references for adding page references
    when there are additional pages to add into the project.
    
    Signed-off-by: Peter Nied <peternied@hotmail.com>

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,11 +1,77 @@
+"use client";
+
+import {
+  AppLayout,
+  ContentLayout,
+  SideNavigation,
+} from "@cloudscape-design/components";
+import { useState } from "react";
+
+const sideNav = (
+  <SideNavigation
+    header={{
+      href: "/",
+      text: "Migration Assistant",
+      logo: { src: "/migrations-icon-160x160.png", alt: "" },
+    }}
+    items={[
+      // Reference for future links
+      // { type: 'link', href: '/about', text: 'About'}
+      // { type: 'divider'},
+      {
+        type: "section-group",
+        title: "Help",
+        items: [
+          {
+            type: "link",
+            text: "Documentation",
+            href: "https://opensearch.org/docs/latest/migration-assistant/",
+            external: true,
+            externalIconAriaLabel: "Opens in a new tab",
+          },
+          {
+            type: "link",
+            text: "Report an Issue",
+            href: "https://github.com/opensearch-project/opensearch-migrations/issues",
+            external: true,
+            externalIconAriaLabel: "Opens in a new tab",
+          },
+          {
+            type: "link",
+            text: "Source Code",
+            href: "https://github.com/opensearch-project/opensearch-migrations",
+            external: true,
+            externalIconAriaLabel: "Opens in a new tab",
+          },
+        ],
+      },
+    ]}
+  ></SideNavigation>
+);
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [navigationOpen, setNavigationOpen] = useState(true);
+  const toggleNavigation = () => setNavigationOpen((prev) => !prev);
+
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <AppLayout
+          navigationOpen={navigationOpen}
+          onNavigationChange={toggleNavigation}
+          navigation={sideNav}
+          toolsHide={true}
+          content={
+            <ContentLayout>
+              <div className="contentPlaceholder">{children}</div>
+            </ContentLayout>
+          }
+        />
+      </body>
     </html>
   );
 }
