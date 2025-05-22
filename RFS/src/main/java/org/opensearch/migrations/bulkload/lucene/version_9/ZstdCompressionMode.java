2763aada34421c076f1bb2b029ef5bb7fca5a2fd
Jugal Chauhan <jugaldc@amazon.com>
2025-05-22 17:57:41 -0500

    Bring compressionMode for zstd, zstd no dict and copy lucene912storedfieldsformat
    Signed-off-by: Jugal Chauhan <jugaldc@amazon.com>

--- RFS/src/main/java/org/opensearch/migrations/bulkload/lucene/version_9/ZstdCompressionMode.java
+++ RFS/src/main/java/org/opensearch/migrations/bulkload/lucene/version_9/ZstdCompressionMode.java
@@ -16,78 +16,97 @@ import shadow.lucene9.org.apache.lucene.store.DataOutput;
 import shadow.lucene9.org.apache.lucene.util.ArrayUtil;
 import shadow.lucene9.org.apache.lucene.util.BytesRef;
 
-/** Zstandard Compression Mode */
+/**
+ * Zstandard Compression Mode
+ */
 public class ZstdCompressionMode extends CompressionMode {
 
     private static final int NUM_SUB_BLOCKS = 10;
     private static final int DICT_SIZE_FACTOR = 6;
-    private static final int DEFAULT_COMPRESSION_LEVEL = 3;
 
     private final int compressionLevel;
 
-    /** default constructor */
-    public ZstdCompressionMode() {
+    /**
+     * default constructor
+     */
+    protected ZstdCompressionMode() {
         this.compressionLevel = DEFAULT_COMPRESSION_LEVEL;
     }
 
-    /** parameterized constructor */
-    public ZstdCompressionMode(int compressionLevel) {
+    /**
+     * Creates a new instance.
+     *
+     * @param compressionLevel The compression level to use.
+     */
+    protected ZstdCompressionMode(int compressionLevel) {
         this.compressionLevel = compressionLevel;
     }
 
+    /**
+     * Creates a new compressor instance.
+     */
     @Override
     public Compressor newCompressor() {
         return new ZstdCompressor(compressionLevel);
     }
 
+    /**
+     * Creates a new decompressor instance.
+     */
     @Override
     public Decompressor newDecompressor() {
         return new ZstdDecompressor();
     }
 
+    /**
+     * zstandard compressor
+     */
     private static final class ZstdCompressor extends Compressor {
+
         private final int compressionLevel;
         private byte[] compressedBuffer;
 
+        /**
+         * compressor with a given compresion level
+         */
         public ZstdCompressor(int compressionLevel) {
             this.compressionLevel = compressionLevel;
-            this.compressedBuffer = BytesRef.EMPTY_BYTES;
+            compressedBuffer = BytesRef.EMPTY_BYTES;
         }
 
+        /*resuable compress function*/
         private void doCompress(byte[] bytes, int offset, int length, ZstdCompressCtx cctx, DataOutput out) throws IOException {
             if (length == 0) {
                 out.writeVInt(0);
                 return;
             }
-
-            int maxCompressedLength = (int) Zstd.compressBound(length);
+            final int maxCompressedLength = (int) Zstd.compressBound(length);
             compressedBuffer = ArrayUtil.growNoCopy(compressedBuffer, maxCompressedLength);
 
-            int compressedSize = cctx.compressByteArray(
-                    compressedBuffer, 0, compressedBuffer.length,
-                    bytes, offset, length
-            );
+            int compressedSize = cctx.compressByteArray(compressedBuffer, 0, compressedBuffer.length, bytes, offset, length);
 
             out.writeVInt(compressedSize);
             out.writeBytes(compressedBuffer, compressedSize);
         }
 
         private void compress(byte[] bytes, int offset, int length, DataOutput out) throws IOException {
-            int dictLength = length / (NUM_SUB_BLOCKS * DICT_SIZE_FACTOR);
-            int blockLength = (length - dictLength + NUM_SUB_BLOCKS - 1) / NUM_SUB_BLOCKS;
+            assert offset >= 0 : "offset value must be greater than 0";
 
+            final int dictLength = length / (NUM_SUB_BLOCKS * DICT_SIZE_FACTOR);
+            final int blockLength = (length - dictLength + NUM_SUB_BLOCKS - 1) / NUM_SUB_BLOCKS;
             out.writeVInt(dictLength);
             out.writeVInt(blockLength);
 
-            int end = offset + length;
+            final int end = offset + length;
+            assert end >= 0 : "buffer read size must be greater than 0";
 
             try (ZstdCompressCtx cctx = new ZstdCompressCtx()) {
                 cctx.setLevel(compressionLevel);
 
+                // dictionary compression first
                 doCompress(bytes, offset, dictLength, cctx, out);
-
-                try (ZstdDictCompress dict = new ZstdDictCompress(bytes, offset, dictLength, compressionLevel)) {
-                    cctx.loadDict(dict);
+                try (ZstdDictCompress dictCompress = new ZstdDictCompress(bytes, offset, dictLength, compressionLevel)) {
+                    cctx.loadDict(dictCompress);
 
                     for (int start = offset + dictLength; start < end; start += blockLength) {
                         int l = Math.min(blockLength, end - start);
@@ -98,70 +117,83 @@ public class ZstdCompressionMode extends CompressionMode {
         }
 
         @Override
-        public void compress(ByteBuffersDataInput input, DataOutput out) throws IOException {
-            int length = (int) input.length();
+        public void compress(ByteBuffersDataInput buffersInput, DataOutput out) throws IOException {
+            final int length = (int) buffersInput.length();
             byte[] bytes = new byte[length];
-            input.readBytes(bytes, 0, length);
+            buffersInput.readBytes(bytes, 0, length);
             compress(bytes, 0, length, out);
         }
 
         @Override
-        public void close() {}
+        public void close() throws IOException {
+        }
     }
 
+    /**
+     * zstandard decompressor
+     */
     private static final class ZstdDecompressor extends Decompressor {
+
         private byte[] compressedBuffer;
 
+        /**
+         * default decompressor
+         */
         public ZstdDecompressor() {
             compressedBuffer = BytesRef.EMPTY_BYTES;
         }
 
-        private void doDecompress(DataInput in, ZstdDecompressCtx dctx, BytesRef bytes, int expectedLen) throws IOException {
-            int compressedLen = in.readVInt();
-            if (compressedLen == 0) return;
-
-            compressedBuffer = ArrayUtil.growNoCopy(compressedBuffer, compressedLen);
-            in.readBytes(compressedBuffer, 0, compressedLen);
-
-            bytes.bytes = ArrayUtil.grow(bytes.bytes, bytes.length + expectedLen);
-            int actualLen = dctx.decompressByteArray(bytes.bytes, bytes.length, expectedLen, compressedBuffer, 0, compressedLen);
-
-            if (expectedLen != actualLen) {
-                throw new IOException("Decompressed size mismatch: expected=" + expectedLen + " actual=" + actualLen);
+        /*resuable decompress function*/
+        private void doDecompress(DataInput in, ZstdDecompressCtx dctx, BytesRef bytes, int decompressedLen) throws IOException {
+            final int compressedLength = in.readVInt();
+            if (compressedLength == 0) {
+                return;
             }
 
-            bytes.length += actualLen;
+            compressedBuffer = ArrayUtil.growNoCopy(compressedBuffer, compressedLength);
+            in.readBytes(compressedBuffer, 0, compressedLength);
+
+            bytes.bytes = ArrayUtil.grow(bytes.bytes, bytes.length + decompressedLen);
+            int uncompressed = dctx.decompressByteArray(bytes.bytes, bytes.length, decompressedLen, compressedBuffer, 0, compressedLength);
+
+            if (decompressedLen != uncompressed) {
+                throw new IllegalStateException(decompressedLen + " " + uncompressed);
+            }
+            bytes.length += uncompressed;
         }
 
         @Override
         public void decompress(DataInput in, int originalLength, int offset, int length, BytesRef bytes) throws IOException {
+            assert offset + length <= originalLength : "buffer read size must be within limit";
+
             if (length == 0) {
                 bytes.length = 0;
                 return;
             }
-
-            int dictLength = in.readVInt();
-            int blockLength = in.readVInt();
-
+            final int dictLength = in.readVInt();
+            final int blockLength = in.readVInt();
             bytes.bytes = ArrayUtil.growNoCopy(bytes.bytes, dictLength);
             bytes.offset = bytes.length = 0;
 
             try (ZstdDecompressCtx dctx = new ZstdDecompressCtx()) {
-                doDecompress(in, dctx, bytes, dictLength);
 
-                try (ZstdDictDecompress dict = new ZstdDictDecompress(bytes.bytes, 0, dictLength)) {
-                    dctx.loadDict(dict);
+                // decompress dictionary first
+                doDecompress(in, dctx, bytes, dictLength);
+                try (ZstdDictDecompress dictDecompress = new ZstdDictDecompress(bytes.bytes, 0, dictLength)) {
+                    dctx.loadDict(dictDecompress);
 
                     int offsetInBlock = dictLength;
                     int offsetInBytesRef = offset;
 
+                    // Skip unneeded blocks
                     while (offsetInBlock + blockLength < offset) {
-                        int skipLen = in.readVInt();
-                        in.skipBytes(skipLen);
+                        final int compressedLength = in.readVInt();
+                        in.skipBytes(compressedLength);
                         offsetInBlock += blockLength;
                         offsetInBytesRef -= blockLength;
                     }
 
+                    // Read blocks that intersect with the interval we need
                     while (offsetInBlock < offset + length) {
                         int l = Math.min(blockLength, originalLength - offsetInBlock);
                         doDecompress(in, dctx, bytes, l);
@@ -171,7 +203,7 @@ public class ZstdCompressionMode extends CompressionMode {
                     bytes.offset = offsetInBytesRef;
                     bytes.length = length;
 
-                    assert bytes.isValid() : "Invalid decompressed BytesRef";
+                    assert bytes.isValid() : "decompression output is corrupted";
                 }
             }
         }
--- RFS/src/main/java/org/opensearch/migrations/bulkload/lucene/version_9/ZstdStoredFields814Format.java
+++ RFS/src/main/java/org/opensearch/migrations/bulkload/lucene/version_9/ZstdStoredFields814Format.java
@@ -1,36 +1,134 @@
-package org.opensearch.migrations.bulkload.lucene.version_9;
+package org.opensearch.index.codec.customcodecs.backward_codecs.lucene912;
 
 import java.io.IOException;
+import java.util.Objects;
 
 import shadow.lucene9.org.apache.lucene.codecs.StoredFieldsFormat;
 import shadow.lucene9.org.apache.lucene.codecs.StoredFieldsReader;
 import shadow.lucene9.org.apache.lucene.codecs.StoredFieldsWriter;
+import shadow.lucene9.org.apache.lucene.codecs.compressing.CompressionMode;
 import shadow.lucene9.org.apache.lucene.codecs.lucene90.compressing.Lucene90CompressingStoredFieldsFormat;
-import shadow.lucene9.org.apache.lucene.index.*;
+import shadow.lucene9.org.apache.lucene.index.FieldInfos;
+import shadow.lucene9.org.apache.lucene.index.SegmentInfo;
 import shadow.lucene9.org.apache.lucene.store.Directory;
 import shadow.lucene9.org.apache.lucene.store.IOContext;
 
+import static org.opensearch.index.codec.customcodecs.backward_codecs.lucene99.Lucene99CustomCodec.DEFAULT_COMPRESSION_LEVEL;
+
+/** Stored field format used by pluggable codec */
 public class ZstdStoredFields814Format extends StoredFieldsFormat {
 
+    /** A key that we use to map to a mode */
+    public static final String MODE_KEY = ZstdStoredFields814Format.class.getSimpleName() + ".mode";
+
+    protected static final int ZSTD_BLOCK_LENGTH = 10 * 48 * 1024;
+    protected static final int ZSTD_MAX_DOCS_PER_BLOCK = 4096;
+    protected static final int ZSTD_BLOCK_SHIFT = 10;
+
+    private final CompressionMode zstdCompressionMode;
+    private final CompressionMode zstdNoDictCompressionMode;
+
+    private final Lucene912CustomCodec.Mode mode;
+    private final int compressionLevel;
+
+    /** default constructor */
     public ZstdStoredFields814Format() {
-        System.out.println(">>> Loaded ZstdStoredFields814Format as a fallback stored fields decoder.");
+        this(Lucene912CustomCodec.Mode.ZSTD, DEFAULT_COMPRESSION_LEVEL);
     }
 
+    /**
+     * Creates a new instance.
+     *
+     * @param mode The mode represents ZSTD or ZSTDNODICT
+     */
+    public ZstdStoredFields814Format(Lucene912CustomCodec.Mode mode) {
+        this(mode, DEFAULT_COMPRESSION_LEVEL);
+    }
+
+    /**
+     * Creates a new instance with the specified mode and compression level.
+     *
+     * @param mode The mode represents ZSTD or ZSTDNODICT
+     * @param compressionLevel The compression level for the mode.
+     */
+    public ZstdStoredFields814Format(Lucene912CustomCodec.Mode mode, int compressionLevel) {
+        this.mode = Objects.requireNonNull(mode);
+        this.compressionLevel = compressionLevel;
+        zstdCompressionMode = new ZstdCompressionMode(compressionLevel) {
+        };
+        zstdNoDictCompressionMode = new ZstdNoDictCompressionMode(compressionLevel) {
+        };
+    }
+
+    /**
+     * Returns a {@link StoredFieldsReader} to load stored fields.
+     * @param directory The index directory.
+     * @param si The SegmentInfo that stores segment information.
+     * @param fn The fieldInfos.
+     * @param context The IOContext that holds additional details on the merge/search context.
+     */
     @Override
     public StoredFieldsReader fieldsReader(Directory directory, SegmentInfo si, FieldInfos fn, IOContext context) throws IOException {
-        System.out.println(">>> Attempting to decode stored fields using ZstdStoredFields814Format for segment: " + si.name);
-        // TODO: Replace with real reader implementation
-        return new Lucene90CompressingStoredFieldsFormat(
-                "ZstdStoredFields814",                         // Codec name (used in .fdt header)
-                new ZstdStoredFields814CompressionMode(),      // Your custom mode
-                10 * 48 * 1024,  // block size (same as Zstd)
-                4096,            // max docs per block
-                10               // block shift
-        ).fieldsReader(directory, si, fn, context);
+        if (si.getAttribute(MODE_KEY) != null) {
+            String value = si.getAttribute(MODE_KEY);
+            Lucene912CustomCodec.Mode mode = Lucene912CustomCodec.Mode.valueOf(value);
+            return impl(mode).fieldsReader(directory, si, fn, context);
+        } else {
+            throw new IllegalStateException("missing value for " + MODE_KEY + " for segment: " + si.name);
+        }
     }
 
+    /**
+     * Returns a {@link StoredFieldsReader} to write stored fields.
+     * @param directory The index directory.
+     * @param si The SegmentInfo that stores segment information.
+     * @param context The IOContext that holds additional details on the merge/search context.
+     */
     @Override
     public StoredFieldsWriter fieldsWriter(Directory directory, SegmentInfo si, IOContext context) throws IOException {
-        throw new UnsupportedOperationException("ZstdStoredFields814Format: writing not supported");
+        String previous = si.putAttribute(MODE_KEY, mode.name());
+        if (previous != null && previous.equals(mode.name()) == false) {
+            throw new IllegalStateException(
+                    "found existing value for " + MODE_KEY + " for segment: " + si.name + " old = " + previous + ", new = " + mode.name()
+            );
+        }
+        return impl(mode).fieldsWriter(directory, si, context);
     }
+
+    StoredFieldsFormat impl(Lucene912CustomCodec.Mode mode) {
+        switch (mode) {
+            case ZSTD:
+                return getCustomCompressingStoredFieldsFormat("CustomStoredFieldsZstd", this.zstdCompressionMode);
+            case ZSTD_NO_DICT:
+                return getCustomCompressingStoredFieldsFormat("CustomStoredFieldsZstdNoDict", this.zstdNoDictCompressionMode);
+            default:
+                throw new IllegalStateException("Unsupported compression mode: " + mode);
+        }
+    }
+
+    private StoredFieldsFormat getCustomCompressingStoredFieldsFormat(String formatName, CompressionMode compressionMode) {
+        return new Lucene90CompressingStoredFieldsFormat(
+                formatName,
+                compressionMode,
+                ZSTD_BLOCK_LENGTH,
+                ZSTD_MAX_DOCS_PER_BLOCK,
+                ZSTD_BLOCK_SHIFT
+        );
+    }
+
+    public Lucene912CustomCodec.Mode getMode() {
+        return mode;
+    }
+
+    /**
+     * Returns the compression level.
+     */
+    public int getCompressionLevel() {
+        return compressionLevel;
+    }
+
+    public CompressionMode getCompressionMode() {
+        return mode == Lucene912CustomCodec.Mode.ZSTD_NO_DICT ? zstdNoDictCompressionMode : zstdCompressionMode;
+    }
+
 }
