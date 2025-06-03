93e3dafa4357ba8f7f027c349fe4e5b38b95c40a
Jugal Chauhan <jugaldc@amazon.com>
2025-06-03 11:35:58 -0500

    Use helper functions for version matching
    Signed-off-by: Jugal Chauhan <jugaldc@amazon.com>

--- transformation/src/main/java/org/opensearch/migrations/VersionMatchers.java
+++ transformation/src/main/java/org/opensearch/migrations/VersionMatchers.java
@@ -3,21 +3,20 @@ package org.opensearch.migrations;
 import java.util.function.Predicate;
 
 import lombok.experimental.UtilityClass;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @UtilityClass
 public class VersionMatchers {
+    private static final Logger log = LoggerFactory.getLogger(VersionMatchers.class);
+
     public static final Predicate<Version> isES_2_X = VersionMatchers.matchesMajorVersion(Version.fromString("ES 2.4"));
     public static final Predicate<Version> isES_5_X = VersionMatchers.matchesMajorVersion(Version.fromString("ES 5.6"));
     public static final Predicate<Version> isES_6_X = VersionMatchers.matchesMajorVersion(Version.fromString("ES 6.8"));
     public static final Predicate<Version> isES_7_X = VersionMatchers.matchesMajorVersion(Version.fromString("ES 7.10"));
     public static final Predicate<Version> isES_7_10 = VersionMatchers.matchesMinorVersion(Version.fromString("ES 7.10.2"));
     public static final Predicate<Version> isES_8_X = VersionMatchers.matchesMajorVersion(Version.fromString("ES 8.17"));
-    public static final Predicate<Version> isES_7_0_to_7_9 =
-            v -> v != null
-                    && v.getFlavor() == Flavor.ELASTICSEARCH
-                    && v.getMajor() == 7
-                    && v.getMinor() >= 0
-                    && v.getMinor() < 10;
+    public static final Predicate<Version> isES_7_0_to_7_9 = VersionMatchers.inclusiveVersionRange("ES 7.0", "ES 7.9");
     public static final Predicate<Version> equalOrGreaterThanES_7_10 = VersionMatchers.equalOrGreaterThanMinorVersion(Version.fromString("ES 7.10"));
 
     public static final Predicate<Version> isOS_1_X = VersionMatchers.matchesMajorVersion(Version.fromString("OS 1.0.0"));
@@ -54,6 +53,29 @@ public class VersionMatchers {
         return other -> matchesMajorVersion(version)
             .and(other2 -> version.getMinor() == other2.getMinor())
             .test(other);
+    }
+
+    static int compareVersions(Version v1, Version v2) {
+        if (v1.getMajor() != v2.getMajor()) {
+            return Integer.compare(v1.getMajor(), v2.getMajor());
+        }
+        if (v1.getMinor() != v2.getMinor()) {
+            return Integer.compare(v1.getMinor(), v2.getMinor());
+        }
+        return Integer.compare(v1.getPatch(), v2.getPatch());
+    }
+
+    static Predicate<Version> inclusiveVersionRange(String minVersionInclusiveStr, String maxVersionInclusiveStr) {
+        Version minVersion = Version.fromString(minVersionInclusiveStr);
+        Version maxVersion = Version.fromString(maxVersionInclusiveStr);
+        return v -> {
+            if (v == null) {
+                return false;
+            }
+            int compareToMin = compareVersions(v, minVersion);
+            int compareToMax = compareVersions(v, maxVersion);
+            return compareToMin >= 0 && compareToMax <= 0;
+        };
     }
 
     static Predicate<Version> equalOrGreaterThanMinorVersion(final Version version) {
