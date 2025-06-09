445514255f24620dd8fa6fa949b480cdeb7ec43f
Andre Kurait <akurait@amazon.com>
2025-06-09 07:45:38 -0500

    Update CONNECT/HEAD Response handling logic to only operate on responses
    Signed-off-by: Andre Kurait <akurait@amazon.com>

--- TrafficCapture/trafficReplayer/src/main/java/org/opensearch/migrations/replay/HttpByteBufFormatter.java
+++ TrafficCapture/trafficReplayer/src/main/java/org/opensearch/migrations/replay/HttpByteBufFormatter.java
@@ -258,8 +258,8 @@ public class HttpByteBufFormatter {
         try {
             byteBufStream.forEachOrdered(b -> channel.writeInbound(b.retainedDuplicate()));
             T output = channel.readInbound();
-            if (output == null) {
-                log.debug("HTTP message was not processed after decoding all bytes." +
+            if (output == null && HttpMessageType.RESPONSE.equals(msgType)) {
+                log.debug("HTTP response was not processed after decoding all bytes." +
                         "Manually writing empty last content to the channel to signal end of stream." +
                         "This will happen for HEAD and CONNECT responses since we" +
                         "are not decoding the request in the same channel.");
