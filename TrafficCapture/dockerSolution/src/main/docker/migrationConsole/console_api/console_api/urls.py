287fbb1e03872449c60888456e7bd27d68f35fa9
Peter Nied <peternied@hotmail.com>
2025-07-02 20:13:42 +0000

    Remove unused import
    Signed-off-by: Peter Nied <peternied@hotmail.com>

--- TrafficCapture/dockerSolution/src/main/docker/migrationConsole/console_api/console_api/urls.py
+++ TrafficCapture/dockerSolution/src/main/docker/migrationConsole/console_api/console_api/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import include, path
+from django.urls import path
 from .apps.healthcheck import views as healthcheck_views
 
 urlpatterns = [
