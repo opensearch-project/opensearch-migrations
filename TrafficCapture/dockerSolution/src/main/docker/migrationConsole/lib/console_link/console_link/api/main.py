08adcac162ef2d39d0a8a1f4c83b6bbec5dc546d
Peter Nied <peternied@hotmail.com>
2025-07-17 18:03:22 +0000

    Updates to api generation and ensure cors is configured
    Signed-off-by: Peter Nied <peternied@hotmail.com>

--- TrafficCapture/dockerSolution/src/main/docker/migrationConsole/lib/console_link/console_link/api/main.py
+++ TrafficCapture/dockerSolution/src/main/docker/migrationConsole/lib/console_link/console_link/api/main.py
@@ -1,11 +1,26 @@
 import os
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from console_link.api.system import system_router
 
 app = FastAPI(
     title="Migration Assistant API",
     version="0.0.1",
     root_path=os.getenv("FASTAPI_ROOT_PATH", "")
+)
+
+origins = [
+    # Enable development environments by default
+    "http://127.0.0.1:3000",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(system_router)
