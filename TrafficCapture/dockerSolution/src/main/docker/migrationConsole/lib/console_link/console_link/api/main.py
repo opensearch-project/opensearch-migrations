394e97d78a8755d7546a28ab241ff0cb3fa4aef7
Peter Nied <peternied@hotmail.com>
2025-07-24 17:44:28 +0000

    Remove accidental api name change
    Signed-off-by: Peter Nied <peternied@hotmail.com>

--- TrafficCapture/dockerSolution/src/main/docker/migrationConsole/lib/console_link/console_link/api/main.py
+++ TrafficCapture/dockerSolution/src/main/docker/migrationConsole/lib/console_link/console_link/api/main.py
@@ -5,7 +5,7 @@ from console_link.api.system import system_router
 from console_link.api.sessions import session_router
 
 app = FastAPI(
-    title="Assistant API",
+    title="Migration Assistant API",
     version="0.0.1",
     root_path=os.getenv("FASTAPI_ROOT_PATH", "")
 )
