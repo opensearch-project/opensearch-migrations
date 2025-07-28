8d03566fb580e1738f6029e2e8e71d6ab4d03321
Jugal Chauhan <jugaldc@amazon.com>
2025-07-28 14:20:04 -0500

    fix FileSystemRepo
    Signed-off-by: Jugal Chauhan <jugaldc@amazon.com>

--- RFS/src/main/java/org/opensearch/migrations/bulkload/common/FileSystemRepo.java
+++ RFS/src/main/java/org/opensearch/migrations/bulkload/common/FileSystemRepo.java
@@ -1,11 +1,6 @@
 package org.opensearch.migrations.bulkload.common;
 
-import java.io.IOException;
-import java.nio.file.DirectoryStream;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.opensearch.migrations.bulkload.models.ShardMetadata;
 
@@ -15,35 +10,6 @@ import lombok.ToString;
 public class FileSystemRepo implements SourceRepo {
     private final Path repoRootDir;
     private final SnapshotFileFinder fileFinder;
-
-    private Path findRepoFile() {
-        // The directory may contain multiple of these files, but we want the highest versioned one
-        Pattern pattern = Pattern.compile("^index-(\\d+)$");
-        Path highestVersionedFile = null;
-        int highestVersion = -1;
-
-        try (DirectoryStream<Path> stream = Files.newDirectoryStream(repoRootDir)) {
-            for (Path entry : stream) {
-                if (Files.isRegularFile(entry)) {
-                    Matcher matcher = pattern.matcher(entry.getFileName().toString());
-                    if (matcher.matches()) {
-                        int version = Integer.parseInt(matcher.group(1));
-                        if (version > highestVersion) {
-                            highestVersion = version;
-                            highestVersionedFile = entry;
-                        }
-                    }
-                }
-            }
-        } catch (IOException e) {
-            throw new CantOpenRepoDirectory(e);
-        }
-
-        if (highestVersionedFile == null) {
-            throw new CantFindRepoIndexFile();
-        }
-        return highestVersionedFile;
-    }
 
     public FileSystemRepo(Path repoRootDir, SnapshotFileFinder fileFinder) {
         this.repoRootDir = repoRootDir;
@@ -57,54 +23,41 @@ public class FileSystemRepo implements SourceRepo {
 
     @Override
     public Path getSnapshotRepoDataFilePath() {
-        Path relativePath = fileFinder.getSnapshotRepoDataFilePath();
-        return relativePath != null ? repoRootDir.resolve(relativePath) : findRepoFile(); // fallback
+        return fileFinder.getSnapshotRepoDataFilePath();
     }
 
     @Override
     public Path getGlobalMetadataFilePath(String snapshotId) {
-        return repoRootDir.resolve(fileFinder.getGlobalMetadataFilePath(snapshotId));
+        return fileFinder.getGlobalMetadataFilePath(snapshotId);
     }
 
     @Override
     public Path getSnapshotMetadataFilePath(String snapshotId) {
-        return repoRootDir.resolve(fileFinder.getSnapshotMetadataFilePath(snapshotId));
+        return fileFinder.getSnapshotMetadataFilePath(snapshotId);
     }
 
     @Override
     public Path getIndexMetadataFilePath(String indexId, String indexFileId) {
-        return repoRootDir.resolve(fileFinder.getIndexMetadataFilePath(indexId, indexFileId));
+        return fileFinder.getIndexMetadataFilePath(indexId, indexFileId);
     }
 
     @Override
     public Path getShardDirPath(String indexId, int shardId) {
-        return repoRootDir.resolve(fileFinder.getShardDirPath(indexId, shardId));
+        return fileFinder.getShardDirPath(indexId, shardId);
     }
 
     @Override
     public Path getShardMetadataFilePath(String snapshotId, String indexId, int shardId) {
-        return repoRootDir.resolve(fileFinder.getShardMetadataFilePath(snapshotId, indexId, shardId));
+        return fileFinder.getShardMetadataFilePath(snapshotId, indexId, shardId);
     }
 
     @Override
     public Path getBlobFilePath(String indexId, int shardId, String blobName) {
-        return repoRootDir.resolve(fileFinder.getBlobFilePath(indexId, shardId, blobName));
+        return fileFinder.getBlobFilePath(indexId, shardId, blobName);
     }
 
     @Override
     public void prepBlobFiles(ShardMetadata shardMetadata) {
         // No work necessary for local filesystem
-    }
-
-    public static class CantOpenRepoDirectory extends RfsException {
-        public CantOpenRepoDirectory(Throwable cause) {
-            super("Couldn't open the repo directory for some reason", cause);
-        }
-    }
-
-    public static class CantFindRepoIndexFile extends RfsException {
-        public CantFindRepoIndexFile() {
-            super("Can't find the repo index file in the repo directory");
-        }
     }
 }
