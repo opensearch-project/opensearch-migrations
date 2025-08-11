e50073f7a2549440b749a7b294275722ab936f55
Jugal Deepak Chauhan <43086157+jugal-chauhan@users.noreply.github.com>
2025-08-11 17:32:53 -0500

    Extend support to all minor versions of elasticsearch 7x and 8x (#1740)
    Signed-off-by: Jugal Chauhan <jugaldc@amazon.com>
    Signed-off-by: Jugal Deepak Chauhan <43086157+jugal-chauhan@users.noreply.github.com>

---
+++ RFS/src/main/java/org/opensearch/migrations/bulkload/lucene/version_9/IgnoreBloomFilter87.java
@@ -0,0 +1,7 @@
+package org.opensearch.migrations.bulkload.lucene.version_9;
+
+public class IgnoreBloomFilter87 extends IgnoreBloomFilter {
+    public IgnoreBloomFilter87() {
+        super("ES87BloomFilter");
+    }
+}
