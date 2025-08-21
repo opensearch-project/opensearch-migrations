6b09100139dd58ed77c54411414209129218f0fb
Tanner Lewis <lewijacn@amazon.com>
2025-08-21 17:47:13 -0500

    Update k8s pipelines to have consistent repo and branch default
    Signed-off-by: Tanner Lewis <lewijacn@amazon.com>

--- vars/elasticsearch8xK8sLocalTest.groovy
+++ vars/elasticsearch8xK8sLocalTest.groovy
@@ -1,9 +1,7 @@
 def call(Map config = [:]) {
     k8sLocalDeployment(
-            gitUrl: config.gitUrl,
-            gitBranch: config.gitBranch,
             jobName: 'elasticsearch-8x-k8s-local-test',
             sourceVersion: 'ES_8.x',
-            targetVersion: 'OS_2.x'
+            targetVersion: 'OS_2.19'
     )
 }
--- vars/k8sLocalDeployment.groovy
+++ vars/k8sLocalDeployment.groovy
@@ -15,8 +15,8 @@ def call(Map config = [:]) {
         agent { label config.workerAgent ?: 'Jenkins-Default-Agent-X64-C5xlarge-Single-Host' }
 
         parameters {
-            string(name: 'GIT_REPO_URL', defaultValue: "${gitDefaultUrl}", description: 'Git repository url')
-            string(name: 'GIT_BRANCH', defaultValue: "${gitDefaultBranch}", description: 'Git branch to use for repository')
+            string(name: 'GIT_REPO_URL', defaultValue: 'https://github.com/opensearch-project/opensearch-migrations.git', description: 'Git repository url')
+            string(name: 'GIT_BRANCH', defaultValue: 'main', description: 'Git branch to use for repository')
         }
 
         options {
