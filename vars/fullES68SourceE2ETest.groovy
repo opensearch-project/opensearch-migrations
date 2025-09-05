40a98932e79cbb07f33a291f0b95475e8c01c63d
Jugal Deepak Chauhan <43086157+jugal-chauhan@users.noreply.github.com>
2025-09-05 09:51:22 -0500

    Replace OpenSearch version 2.11 with latest 2.19  (#1808)
    Signed-off-by: Jugal Chauhan <jugaldc@amazon.com>

--- vars/fullES68SourceE2ETest.groovy
+++ vars/fullES68SourceE2ETest.groovy
@@ -59,7 +59,7 @@ def call(Map config = [:]) {
           "full-migration": {
             "stage": "<STAGE>",
             "vpcId": "<VPC_ID>",
-            "engineVersion": "OS_2.11",
+            "engineVersion": "OS_2.19",
             "domainName": "os-cluster-<STAGE>",
             "dataNodeCount": 2,
             "openAccessPolicyEnabled": true,
