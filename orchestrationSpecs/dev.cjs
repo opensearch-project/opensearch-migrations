04b2176e11373109e0bb9e5af2b7cf3376621e49
Greg Schohn <greg.schohn@gmail.com>
2025-09-19 13:54:31 -0400

    Switch npm scripts to use tsgo for type checking and tsx for running. That makes for less overall config to worry about.
    Signed-off-by: Greg Schohn <greg.schohn@gmail.com>

--- orchestrationSpecs/dev.cjs
+++ orchestrationSpecs/dev.cjs
@@ -1,14 +1,17 @@
-require('ts-node').register({
-    transpileOnly: false, // Do full type-checking!
-    files: true           // for .d.ts are picked up
-});
-
-require('tsconfig-paths/register');
-
+// dev.cjs
 const fs = require('fs');
+
+// Register the .sh extension handler FIRST
 require.extensions['.sh'] = (module, filename) => {
     const content = fs.readFileSync(filename, 'utf8');
     module._compile('module.exports = ' + JSON.stringify(content), filename);
 };
 
+// Register tsx for TypeScript support
+require('tsx/cjs/api').register();
+
+// Load tsconfig paths
+require('tsconfig-paths/register');
+
+// Now require your main file
 require('./src/index.ts');
