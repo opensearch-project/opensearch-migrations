f5a999186b250b934cfb2cb654f467352352efc8
Tanner Lewis <lewijacn@amazon.com>
2025-09-30 15:05:57 -0500

    Add Import VPC Cloudformation Template for EKS (#1850)
    Description
    Adds an import VPC path for EKS deployment.
    
    Limitations
    Currently requires users to specify at least one security group
    We cannot validate subnets or security groups are in the selected import VPC until after deployment
    Ongoing Testing
    1. What are minimum number of subnets needed
    2. Will isolated subnets work
    
    Current requirement is that The imported VPC must provide at least two private or two public subnets, with each subnet being in a different Availability Zone
    
    EKS cluster requirement: at least two subnets in different AZs
    Outbound internet access currently required to pull public images. This requirement could be reconsidered if all images used by MA as well as its Helm chart dependencies were in a private EFS, but this doesn't seem trivial and should be considered for later.
    
    ---------
    
    Signed-off-by: Tanner Lewis <lewijacn@amazon.com>

---
+++ deployment/migration-assistant-solution/test/solutions-stack-eks.test.ts
@@ -0,0 +1,122 @@
+import { describe, test } from '@jest/globals';
+import {Template} from 'aws-cdk-lib/assertions';
+import { App } from 'aws-cdk-lib';
+import {SolutionsInfrastructureEKSStack} from "../lib/solutions-stack-eks";
+
+describe('Solutions stack', () => {
+    const defaultProperties = {
+        solutionId: 'SO0000',
+        solutionName: 'test-solution',
+        solutionVersion: '0.0.1',
+        codeBucket: 'test-bucket',
+        createVPC: true,
+        env: {
+            region: 'us-west-1'
+        }
+    };
+
+    test('Generate migration assistant stack with create VPC', () => {
+        const stack = new SolutionsInfrastructureEKSStack(new App(), 'TestMigrationAssistantStack', defaultProperties);
+        const template = Template.fromStack(stack);
+        verifyResources(template, {
+            vpcCount: 1,
+            vpcEndpointCount: 5,
+            subnetCount: 4,
+            natGatewayCount: 2
+        });
+        verifyParameters(template, {
+            vpcIdEnabled: false,
+            vpcSubnetsEnabled: false,
+            vpcSecurityGroupsEnabled: false
+        })
+    });
+
+    test('Generate migration assistant stack with imported VPC', () => {
+        const stack = new SolutionsInfrastructureEKSStack(new App(), 'TestMigrationAssistantStack', {
+            ...defaultProperties,
+            createVPC: false
+        });
+        const template = Template.fromStack(stack);
+        verifyResources(template, {
+            vpcCount: 0,
+            vpcEndpointCount: 0,
+            subnetCount: 0,
+            natGatewayCount: 0
+        });
+        verifyParameters(template, {
+            vpcIdEnabled: true,
+            vpcSubnetsEnabled: true,
+            vpcSecurityGroupsEnabled: true
+        })
+    });
+
+    test('Generate migration assistant stack with create VPC in Gov Region', () => {
+        const stack = new SolutionsInfrastructureEKSStack(new App(), 'TestMigrationAssistantStack',  {
+            ...defaultProperties,
+            env: {
+                region : "us-gov-east-1",
+            },
+        });
+        const template = Template.fromStack(stack);
+        verifyResources(template, {
+            vpcCount: 1,
+            vpcEndpointCount: 5,
+            subnetCount: 4,
+            natGatewayCount: 2
+        });
+        verifyParameters(template, {
+            vpcIdEnabled: false,
+            vpcSubnetsEnabled: false,
+            vpcSecurityGroupsEnabled: false
+        })
+    });
+
+    test('Migration stack with new VPC matches snapshot', () => {
+        const stack = new SolutionsInfrastructureEKSStack(new App(), 'TestMigrationAssistantStack', defaultProperties);
+        const template = Template.fromStack(stack).toJSON();
+        expect(template).toMatchSnapshot();
+    });
+
+    test('Migration stack with import VPC matches snapshot', () => {
+        const stack = new SolutionsInfrastructureEKSStack(new App(), 'TestMigrationAssistantStack', {
+            ...defaultProperties,
+            createVPC: false
+        });
+        const template = Template.fromStack(stack).toJSON();
+        expect(template).toMatchSnapshot();
+    });
+
+    function verifyResources(template: Template, props: { vpcCount: number, vpcEndpointCount: number,
+        subnetCount: number, natGatewayCount: number }) {
+        template.resourceCountIs('AWS::EC2::VPC', props.vpcCount);
+        template.resourceCountIs('AWS::EC2::VPCEndpoint', props.vpcEndpointCount);
+        template.resourceCountIs('AWS::EC2::Subnet', props.subnetCount);
+        template.resourceCountIs('AWS::EC2::NatGateway', props.natGatewayCount);
+        template.resourceCountIs('AWS::ServiceCatalogAppRegistry::Application', 1);
+        template.resourceCountIs('AWS::EKS::Cluster', 1);
+        template.resourceCountIs('AWS::IAM::Role', 3);
+    }
+
+    function verifyParameters(template: Template, props: { vpcIdEnabled: boolean, vpcSubnetsEnabled: boolean,
+        vpcSecurityGroupsEnabled: boolean }) {
+        template.hasParameter('Stage', {
+            Type: 'String',
+            Default: "dev",
+        });
+        if (props.vpcIdEnabled) {
+            template.hasParameter('VPCId', {
+                Type: 'AWS::EC2::VPC::Id'
+            });
+        }
+        if (props.vpcSubnetsEnabled) {
+            template.hasParameter('VPCSubnetIds', {
+                Type: 'List<AWS::EC2::Subnet::Id>'
+            });
+        }
+        if (props.vpcSecurityGroupsEnabled) {
+            template.hasParameter('VPCSecurityGroupIds', {
+                Type: 'List<AWS::EC2::SecurityGroup::Id>'
+            });
+        }
+    }
+});
