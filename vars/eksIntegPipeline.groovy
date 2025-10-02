770196580844110da0744a121b670cf8ea18a9d5
Tanner Lewis <lewijacn@amazon.com>
2025-10-02 14:46:50 -0500

    Minor edits 2
    Signed-off-by: Tanner Lewis <lewijacn@amazon.com>

--- vars/eksIntegPipeline.groovy
+++ vars/eksIntegPipeline.groovy
@@ -226,7 +226,7 @@ def call(Map config = [:]) {
                             script {
                                 withCredentials([string(credentialsId: 'migrations-test-account-id', variable: 'MIGRATIONS_TEST_ACCOUNT_ID')]) {
                                     withAWS(role: 'JenkinsDeploymentRole', roleAccount: "${MIGRATIONS_TEST_ACCOUNT_ID}", region: "us-east-1", duration: 3600, roleSessionName: 'jenkins-session') {
-                                        sh "./aws-bootstrap.sh --skip-git-pull --base-dir '../../..' --use-public-images false"
+                                        sh "./aws-bootstrap.sh --skip-git-pull --base-dir \"../../..\" --use-public-images false"
                                     }
                                 }
                             }
@@ -269,6 +269,7 @@ def call(Map config = [:]) {
                                 withCredentials([string(credentialsId: 'migrations-test-account-id', variable: 'MIGRATIONS_TEST_ACCOUNT_ID')]) {
                                     withAWS(role: 'JenkinsDeploymentRole', roleAccount: "${MIGRATIONS_TEST_ACCOUNT_ID}", region: "us-east-1", duration: 3600, roleSessionName: 'jenkins-session') {
                                         sh "aws eks update-kubeconfig --region us-east-1 --name ${env.eksClusterName}"
+                                        sh "kubectl -n ma config current-context"
                                         sh "pipenv run app --delete-only"
                                         sh "kubectl -n ma delete namespace ma"
                                     }
