7b7202ad7a8fb5c6dec8d7367717dfa0bf994452
Greg Schohn <greg.schohn@gmail.com>
2025-10-10 09:24:39 -0400

    Introduce a wrapper for JCommander that can take json configurations as a single arg or a filename.
    If the command line does NOT specify one of the new "---..." flags, then we revert to using JCommander as we always have - otherwise, we do all of the command line parsing with jackson.
    I don't want to lump JCommander and Jackson into our inner-most core library's set of dependencies, but we can clean that up later.
    
    Signed-off-by: Greg Schohn <greg.schohn@gmail.com>

--- MetadataMigration/src/main/java/org/opensearch/migrations/MetadataMigration.java
+++ MetadataMigration/src/main/java/org/opensearch/migrations/MetadataMigration.java
@@ -8,7 +8,7 @@ import org.opensearch.migrations.arguments.ArgLogUtils;
 import org.opensearch.migrations.arguments.ArgNameConstants;
 import org.opensearch.migrations.cli.OutputFormat;
 import org.opensearch.migrations.commands.*;
-import org.opensearch.migrations.jcommander.EnvVarParameterPuller;
+import org.opensearch.migrations.jcommander.JsonCommandLineParser;
 import org.opensearch.migrations.metadata.tracing.RootMetadataMigrationContext;
 import org.opensearch.migrations.tracing.ActiveContextTracker;
 import org.opensearch.migrations.tracing.ActiveContextTrackerByActivityType;
@@ -37,16 +37,15 @@ public class MetadataMigration {
                 args,
                 ArgNameConstants.joinLists(ArgNameConstants.CENSORED_SOURCE_ARGS, ArgNameConstants.CENSORED_TARGET_ARGS)
         )));
-        var metadataArgs = EnvVarParameterPuller.injectFromEnv(new MetadataArgs(), "METADATA_");
-        var migrateArgs = EnvVarParameterPuller.injectFromEnv(new MigrateArgs(), "METADATA_");
-        var evaluateArgs = EnvVarParameterPuller.injectFromEnv(new EvaluateArgs(), "METADATA_");
-        var jCommander = JCommander.newBuilder()
+        var metadataArgs = new MetadataArgs();
+        var migrateArgs = new MigrateArgs();
+        var evaluateArgs = new EvaluateArgs();
+        var argsParser = JsonCommandLineParser.newBuilder()
             .addObject(metadataArgs)
             .addCommand(migrateArgs)
             .addCommand(evaluateArgs)
             .build();
-        jCommander.parse(args);
-
+        argsParser.parse(args);
 
         if (migrateArgs.outputFormat == OutputFormat.JSON || evaluateArgs.outputFormat == OutputFormat.JSON) {
             outputFormat.set(OutputFormat.JSON);
@@ -54,17 +53,17 @@ public class MetadataMigration {
             outputFormat.set(OutputFormat.HUMAN_READABLE);
         }
 
-        if (metadataArgs.help || jCommander.getParsedCommand() == null) {
-            printTopLevelHelp(jCommander);
+        if (metadataArgs.help || argsParser.getParsedCommand() == null) {
+            printTopLevelHelp(argsParser.getJCommander());
             return;
         }
 
         if (migrateArgs.help || evaluateArgs.help) {
-            printCommandUsage(jCommander);
+            printCommandUsage(argsParser.getJCommander());
             return;
         }
 
-        var result = runCommand(jCommander, metadataArgs, migrateArgs, evaluateArgs);
+        var result = runCommand(argsParser.getJCommander(), metadataArgs, migrateArgs, evaluateArgs);
 
         // Output format determines which version is printed to the user
         writeOutput(result.asCliOutput());
