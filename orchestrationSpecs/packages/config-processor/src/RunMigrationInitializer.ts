1983ef6cb0ef4e7dce2a40441d18a3da971e0669
Andre Kurait <akurait@amazon.com>
2025-10-21 11:11:15 -0500

    In RunMigrationInitializer fallback to ETCD_SERVICE_HOST and ETCD_SERVICE_PORT_CLIENT if ETCD_ENDPOINTS not available
    Signed-off-by: Andre Kurait <akurait@amazon.com>

--- orchestrationSpecs/packages/config-processor/src/RunMigrationInitializer.ts
+++ orchestrationSpecs/packages/config-processor/src/RunMigrationInitializer.ts
@@ -56,7 +56,11 @@ async function main() {
     }
 
     // Parse command line arguments
+    // Construct ETCD_ENDPOINTS from ETCD_SERVICE_HOST and ETCD_SERVICE_PORT_CLIENT if available
     let etcdEndpoints = process.env.ETCD_ENDPOINTS;
+    if (!etcdEndpoints && process.env.ETCD_SERVICE_HOST && process.env.ETCD_SERVICE_PORT_CLIENT) {
+        etcdEndpoints = `http://${process.env.ETCD_SERVICE_HOST}:${process.env.ETCD_SERVICE_PORT_CLIENT}`;
+    }
     let etcdUser = process.env.ETCD_USER;
     let etcdPassword = process.env.ETCD_PASSWORD;
     let prefix = process.env.PREFIX || "ma";
