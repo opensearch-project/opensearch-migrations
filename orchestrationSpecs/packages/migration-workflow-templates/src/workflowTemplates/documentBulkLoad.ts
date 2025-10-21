591e776ac5e0afb6785a857f39aefb52a8d75e2c
Andre Kurait <akurait@amazon.com>
2025-10-21 07:18:04 -0500

    Move from maxDuration to cap in argo retry strategy, cap sets the retry duration to the specified interval if exceeds, maxDuration fails the retry if backoff would make it exceed the interval
    Signed-off-by: Andre Kurait <akurait@amazon.com>

--- orchestrationSpecs/packages/migration-workflow-templates/src/workflowTemplates/documentBulkLoad.ts
+++ orchestrationSpecs/packages/migration-workflow-templates/src/workflowTemplates/documentBulkLoad.ts
@@ -187,7 +187,7 @@ export const DocumentBulkLoad = WorkflowBuilder.create({
         .addRetryParameters({
             limit: "200",
             retryPolicy: "Always",
-            backoff: {duration: "5", factor: "2", maxDuration: "300"}
+            backoff: {duration: "5", factor: "2", cap: "300"}
         })
     )
 
