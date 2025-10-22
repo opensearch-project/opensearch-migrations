7feb20b5fd5d0249f382de3f8beabba36b3834f3
Andre Kurait <akurait@amazon.com>
2025-10-22 11:34:31 -0500

    fix(workflow): Add sample.yaml to test resources and update fixture
    Add sample.yaml to test resources/scripts directory and update pytest fixture
    to set CONFIG_PROCESSOR_DIR to test resources. Also remove WORKFLOW_SCRIPT_DIR
    environment variable from Dockerfile.
    
    Changes:
    - Copy sample.yaml to test resources/scripts for tests
    - Update conftest.py fixture to point to test resources/scripts
    - Remove WORKFLOW_SCRIPT_DIR and WORKFLOW_TEMPLATE_PATH from Dockerfile
    
    This ensures tests can find both the sample.yaml and the mock scripts they need.
    The 5 failing tests (transform_config, init_workflow, submit_workflow) were
    already broken as they require index.js and createMigrationWorkflowFromUserConfiguration.sh
    which don't exist in test resources.
    Signed-off-by: Andre Kurait <akurait@amazon.com>
    
    Signed-off-by: Andre Kurait <akurait@amazon.com>

--- TrafficCapture/dockerSolution/src/main/docker/migrationConsole/lib/console_link/tests/workflow-tests/conftest.py
+++ TrafficCapture/dockerSolution/src/main/docker/migrationConsole/lib/console_link/tests/workflow-tests/conftest.py
@@ -26,8 +26,8 @@ def pytest_configure(config):
 
 @pytest.fixture(autouse=True)
 def set_config_processor_dir(monkeypatch):
-    """Set CONFIG_PROCESSOR_DIR to test resources for all tests."""
-    # Get the path to test resources/scripts directory
+    """Set CONFIG_PROCESSOR_DIR to test resources/scripts for all tests."""
+    # Get the path to test resources/scripts directory which contains mock scripts
     test_dir = Path(__file__).parent
     scripts_dir = test_dir / "resources" / "scripts"
     
