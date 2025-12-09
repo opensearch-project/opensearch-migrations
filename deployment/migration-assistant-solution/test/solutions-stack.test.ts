c625aa41fc7d39f68de7d7da1f4d87162eb8f2f0
akurait <akurait@amazon.com>
2025-12-09 18:35:30 +0000

    Clean up: Remove redundant functional tests
    - Remove old functional tests since snapshot tests cover the same functionality
    - Keep only snapshot tests for both ECS and EKS templates
    - Cleaner, more maintainable test suite
    
    Signed-off-by: akurait <akurait@amazon.com>

--- deployment/migration-assistant-solution/test/solutions-stack.test.ts
+++ deployment/migration-assistant-solution/test/solutions-stack.test.ts
@@ -15,47 +15,6 @@ describe('Solutions stack', () => {
         }
     };
 
-    test('Generate migration assistant stack with create VPC', () => {
-        const stack = new SolutionsInfrastructureStack(new App(), 'TestMigrationAssistantStack', defaultProperties);
-        const template = Template.fromStack(stack);
-        verifyResources(template, {
-            vpcCount: 1,
-            vpcEndpointCount: 5,
-            subnetCount: 4,
-            natGatewayCount: 2
-        });
-    });
-
-    test('Generate migration assistant stack with create VPC in Gov Region', () => {
-        const stack = new SolutionsInfrastructureStack(new App(), 'TestMigrationAssistantStack',  {
-            ...defaultProperties,
-            env: {
-                region : "us-gov-east-1",
-            },
-        });
-        const template = Template.fromStack(stack);
-        verifyResources(template, {
-            vpcCount: 1,
-            vpcEndpointCount: 5,
-            subnetCount: 4,
-            natGatewayCount: 2
-        });
-    });
-
-    test('Generate migration assistant stack with imported VPC', () => {
-        const stack = new SolutionsInfrastructureStack(new App(), 'TestMigrationAssistantStack', {
-            ...defaultProperties,
-            createVPC: false
-        });
-        const template = Template.fromStack(stack);
-        verifyResources(template, {
-            vpcCount: 0,
-            vpcEndpointCount: 0,
-            subnetCount: 0,
-            natGatewayCount: 0
-        });
-    });
-
     test('ECS stack with new VPC matches snapshot', () => {
         const stack = new SolutionsInfrastructureStack(new App(), 'TestMigrationAssistantStack', defaultProperties);
         const template = Template.fromStack(stack).toJSON();
@@ -70,24 +29,4 @@ describe('Solutions stack', () => {
         const template = Template.fromStack(stack).toJSON();
         expect(template).toMatchSnapshot();
     });
-
-    function verifyResources(template: Template, props: { vpcCount: number, vpcEndpointCount: number,
-        subnetCount: number, natGatewayCount: number }) {
-
-        template.resourceCountIs('AWS::EC2::VPC', props.vpcCount);
-        template.resourceCountIs('AWS::EC2::VPCEndpoint', props.vpcEndpointCount);
-        template.resourceCountIs('AWS::EC2::Subnet', props.subnetCount);
-        template.resourceCountIs('AWS::EC2::NatGateway', props.natGatewayCount);
-        template.resourceCountIs('AWS::ServiceCatalogAppRegistry::Application', 1);
-        template.hasResourceProperties('AWS::EC2::Instance', {
-            InstanceType: "t3.large"
-        });
-        template.hasResourceProperties('AWS::EC2::LaunchTemplate', {
-            LaunchTemplateData: {
-                MetadataOptions: {
-                    HttpTokens: "required"
-                }
-            }
-        });
-    }
 });
