95bc255cd8b1662ef753a09bee1f9f25ed917b3e
Andre Kurait <akurait@amazon.com>
2025-12-11 13:28:48 -0600

    Limit ES 5.x k8s test to test IDs 0001, 0004, 0005
    Signed-off-by: Andre Kurait <akurait@amazon.com>

--- vars/elasticsearch5xK8sLocalTest.groovy
+++ vars/elasticsearch5xK8sLocalTest.groovy
@@ -2,6 +2,7 @@ def call(Map config = [:]) {
     k8sLocalDeployment(
             jobName: 'elasticsearch-5x-k8s-local-test',
             sourceVersion: 'ES_5.6',
-            targetVersion: 'OS_2.19'
+            targetVersion: 'OS_2.19',
+            testIds: '0001,0004,0005'
     )
 }
