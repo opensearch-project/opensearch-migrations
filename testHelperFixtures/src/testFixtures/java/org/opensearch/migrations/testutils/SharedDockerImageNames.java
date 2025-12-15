d0d0cbb2221ff74ff4995eb41ef7c80ebfa58159
Andre Kurait <akurait@amazon.com>
2025-12-15 14:55:58 -0600

    Pin httpd image to 2.4.66-alpine
    Signed-off-by: Andre Kurait <akurait@amazon.com>

--- testHelperFixtures/src/testFixtures/java/org/opensearch/migrations/testutils/SharedDockerImageNames.java
+++ testHelperFixtures/src/testFixtures/java/org/opensearch/migrations/testutils/SharedDockerImageNames.java
@@ -4,6 +4,6 @@ import org.testcontainers.utility.DockerImageName;
 
 public interface SharedDockerImageNames {
     DockerImageName KAFKA = DockerImageName.parse("confluentinc/cp-kafka:7.5.0");
-    DockerImageName HTTPD = DockerImageName.parse("httpd:alpine");
+    DockerImageName HTTPD = DockerImageName.parse("httpd:2.4.66-alpine");
 
 }
