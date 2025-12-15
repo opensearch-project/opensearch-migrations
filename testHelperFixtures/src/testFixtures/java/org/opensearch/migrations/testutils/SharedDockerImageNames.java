15a8ad3b47f4f4af2e01819bfe3de8a7ccc5b72d
Andre Kurait <akurait@amazon.com>
2025-12-15 15:53:36 -0600

    Remove unused HTTPD_EXPECTED_RESPONSE constant
    Signed-off-by: Andre Kurait <akurait@amazon.com>

--- testHelperFixtures/src/testFixtures/java/org/opensearch/migrations/testutils/SharedDockerImageNames.java
+++ testHelperFixtures/src/testFixtures/java/org/opensearch/migrations/testutils/SharedDockerImageNames.java
@@ -5,5 +5,4 @@ import org.testcontainers.utility.DockerImageName;
 public interface SharedDockerImageNames {
     DockerImageName KAFKA = DockerImageName.parse("confluentinc/cp-kafka:7.5.0");
     DockerImageName HTTPD = DockerImageName.parse("httpd:2.4.66-alpine");
-    String HTTPD_EXPECTED_RESPONSE = "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01//EN\" \"http://www.w3.org/TR/html4/strict.dtd\">\n<html>\n<head>\n<title>It works! Apache httpd</title>\n</head>\n<body>\n<p>It works!</p>\n</body>\n</html>\n";
 }
