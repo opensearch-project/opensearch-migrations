19cd6114d20976d9fb9837608195eaf0568fc0b5
Greg Schohn <greg.schohn@gmail.com>
2025-12-17 22:40:25 -0500

    Hardcode the resources values for the migration console in the helm chart.
    There are too many other values that would need to shift too - it isn't worth giving the user that amount of rope to trip on.
    I've also changed the list of t3 instances to just be a class of 't' instances.  I have the resource limits there for the pod, so we should settle in on a medium or greater node - shouldn't matter if it's amd or arm or what generation.
    Notice that now the dedicated node pool is always on for EKS clusters - though we still fall back to the general one if the dedicated pool doesn't have anything that works.
    
    Signed-off-by: Greg Schohn <greg.schohn@gmail.com>

--- orchestrationSpecs/packages/schemas/src/userSchemas.ts
+++ orchestrationSpecs/packages/schemas/src/userSchemas.ts
@@ -128,7 +128,9 @@ export const CONTAINER_RESOURCES = {
 export const RESOURCE_REQUIREMENTS = z.object({
     limits: z.object(CONTAINER_RESOURCES).describe("Resource upper bound for a container"),
     requests: z.object(CONTAINER_RESOURCES).describe("Resource lower bound for a container")
-}).describe("Compute resource requirements for a container");
+}).describe("Compute resource requirements for a container." +
+    "There are implications to how a task will be evicted if the resources and limits are different.  " +
+    "See https://kubernetes.io/docs/concepts/workloads/pods/pod-qos/#guaranteed for more information.");
 
 export type ResourceRequirementsType = z.infer<typeof RESOURCE_REQUIREMENTS>;
 
