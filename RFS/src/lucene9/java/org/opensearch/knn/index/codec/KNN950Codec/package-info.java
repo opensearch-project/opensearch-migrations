5a388273bc6a819f434569a4aa8156ed5da8ad15
Andre Kurait <akurait@amazon.com>
2026-01-13 19:00:08 -0600

    Add package-info.java with S120 suppression for KNN codec packages
    Signed-off-by: Andre Kurait <akurait@amazon.com>

---
+++ RFS/src/lucene9/java/org/opensearch/knn/index/codec/KNN950Codec/package-info.java
@@ -0,0 +1,2 @@
+@SuppressWarnings("java:S120") // Package name must match OpenSearch KNN plugin for Lucene codec SPI
+package org.opensearch.knn.index.codec.KNN950Codec;
---
+++ RFS/src/lucene9/java/org/opensearch/knn/index/codec/backward_codecs/KNN9120Codec/package-info.java
@@ -0,0 +1,2 @@
+@SuppressWarnings("java:S120") // Package name must match OpenSearch KNN plugin for Lucene codec SPI
+package org.opensearch.knn.index.codec.backward_codecs.KNN9120Codec;
