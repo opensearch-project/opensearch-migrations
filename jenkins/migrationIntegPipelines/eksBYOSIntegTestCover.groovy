76cc5f52af0791a048796e31c743a1674a7f2609
Jugal Chauhan <jugaldc@amazon.com>
2026-01-15 12:38:58 -0600

    Add code comments for reference
    Signed-off-by: Jugal Chauhan <jugaldc@amazon.com>

--- jenkins/migrationIntegPipelines/eksBYOSIntegTestCover.groovy
+++ jenkins/migrationIntegPipelines/eksBYOSIntegTestCover.groovy
@@ -1,3 +1,7 @@
+// BYOS (Bring Your Own Snapshot) Integration Test Pipeline
+// Tests migration from pre-existing S3 snapshots to OpenSearch target clusters.
+// See vars/eksBYOSIntegPipeline.groovy for implementation details.
+
 def gitBranch = params.GIT_BRANCH ?: 'jenkins-pipeline-eks-large-migration'
 def gitUrl = params.GIT_REPO_URL ?: 'https://github.com/jugal-chauhan/opensearch-migrations.git'
 
@@ -5,5 +9,4 @@ library identifier: "migrations-lib@${gitBranch}", retriever: modernSCM(
         [$class: 'GitSCMSource',
          remote: "${gitUrl}"])
 
-// Shared library function (location from root: vars/eksBYOSIntegPipeline.groovy)
 eksBYOSIntegPipeline()
