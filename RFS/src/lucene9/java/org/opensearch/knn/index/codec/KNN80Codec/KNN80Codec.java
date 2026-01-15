c27826b8c38ce468bb9518239d075a62733d6e16
Andre Kurait <akurait@amazon.com>
2026-01-15 12:07:11 -0600

    Fix KNN codecs to use correct Lucene backward-codecs versions
    ES/Lucene version mapping:
    - ES 7.4-7.8 (Lucene 8.2-8.5): Use Lucene80Codec (Lucene70SegmentInfoFormat)
    - ES 7.9 (Lucene 8.6): Use Lucene86Codec (Lucene86SegmentInfoFormat)
    - ES 7.10 (Lucene 8.7): Use Lucene87Codec (Lucene87StoredFieldsFormat)
    
    KNN codec fixes:
    - KNN80Codec: Lucene87Codec -> Lucene80Codec
    - KNN84Codec: Lucene87Codec -> Lucene80Codec
    - KNN86Codec: Lucene87Codec -> Lucene86Codec (previous commit)
    
    Signed-off-by: Andre Kurait <akurait@amazon.com>

--- RFS/src/lucene9/java/org/opensearch/knn/index/codec/KNN80Codec/KNN80Codec.java
+++ RFS/src/lucene9/java/org/opensearch/knn/index/codec/KNN80Codec/KNN80Codec.java
@@ -3,7 +3,7 @@
  */
 package org.opensearch.knn.index.codec.KNN80Codec;
 
-import org.apache.lucene.backward_codecs.lucene87.Lucene87Codec;
+import org.apache.lucene.backward_codecs.lucene80.Lucene80Codec;
 import org.apache.lucene.codecs.CompoundFormat;
 import org.apache.lucene.codecs.DocValuesFormat;
 import org.apache.lucene.codecs.FilterCodec;
@@ -11,7 +11,7 @@ import org.apache.lucene.codecs.FilterCodec;
 @SuppressWarnings("java:S120") // Package name must match OpenSearch KNN plugin for Lucene codec SPI
 public class KNN80Codec extends FilterCodec {
     private static final String NAME = "KNN80Codec";
-    public KNN80Codec() { super(NAME, new Lucene87Codec()); }
+    public KNN80Codec() { super(NAME, new Lucene80Codec()); }
     @Override public DocValuesFormat docValuesFormat() { return new KNN80DocValuesFormat(delegate.docValuesFormat()); }
     @Override public CompoundFormat compoundFormat() { return new KNN80CompoundFormat(delegate.compoundFormat()); }
 }
