7e1fdce9fa2184d57231b64c4d849f985d771531
Jugal Chauhan <jugaldc@amazon.com>
2026-02-17 12:48:42 -0600

    Revert branch name to main
    Signed-off-by: Jugal Chauhan <jugaldc@amazon.com>

--- jenkins/migrationIntegPipelines/eksIntegTestCover.groovy
+++ jenkins/migrationIntegPipelines/eksIntegTestCover.groovy
@@ -1,4 +1,4 @@
-def gitBranch = params.GIT_BRANCH ?: 'eks-pipeline'
+def gitBranch = params.GIT_BRANCH ?: 'main'
 def gitUrl = params.GIT_REPO_URL ?: 'https://github.com/opensearch-project/opensearch-migrations.git'
 
 library identifier: "migrations-lib@${gitBranch}", retriever: modernSCM(
