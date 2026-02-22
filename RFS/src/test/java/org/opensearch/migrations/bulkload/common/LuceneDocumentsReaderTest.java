2bdcc820483e14cd106c7fd934e836c2fb784eaf
Andre Kurait <andrekurait@gmail.com>
2026-02-22 09:35:25 +0000

    Fix testParallelReading: mock utf8Value() instead of utf8ToStringValue() for byte[] source
    Signed-off-by: Andre Kurait <andrekurait@gmail.com>

--- RFS/src/test/java/org/opensearch/migrations/bulkload/common/LuceneDocumentsReaderTest.java
+++ RFS/src/test/java/org/opensearch/migrations/bulkload/common/LuceneDocumentsReaderTest.java
@@ -253,7 +253,7 @@ public class LuceneDocumentsReaderTest {
                 when(field1.asUid()).thenReturn("doc" + invocation.getArgument(0));
                 var field2 = mock(LuceneField.class);
                 when(field2.name()).thenReturn("_source");
-                when(field2.utf8ToStringValue()).thenReturn("{\"field\":\"value\"}");
+                when(field2.utf8Value()).thenReturn("{\"field\":\"value\"}".getBytes(java.nio.charset.StandardCharsets.UTF_8));
                 when(doc.getFields()).thenAnswer(inv -> List.of(field1, field2));
 
                 return doc;
