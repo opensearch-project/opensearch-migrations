535bc16dbd2081e9be04b3af3ed40ee400aa7e8c
Andre Kurait <andrekurait@gmail.com>
2026-02-22 08:56:09 +0000

    Fix SonarQube issues: suppress record false positives, add @Deprecated args, make field transient
    Signed-off-by: Andre Kurait <andrekurait@gmail.com>

--- SnapshotReader/src/main/java/org/opensearch/migrations/bulkload/common/DefaultSourceRepoAccessor.java
+++ SnapshotReader/src/main/java/org/opensearch/migrations/bulkload/common/DefaultSourceRepoAccessor.java
@@ -4,7 +4,7 @@ package org.opensearch.migrations.bulkload.common;
  * @deprecated Use {@link SourceRepoAccessor#SourceRepoAccessor(SourceRepo)} directly.
  * This class exists only for backward compatibility.
  */
-@Deprecated
+@Deprecated(since = "0.1.0", forRemoval = true)
 public class DefaultSourceRepoAccessor extends SourceRepoAccessor {
     public DefaultSourceRepoAccessor(SourceRepo repo) {
         super(repo);
