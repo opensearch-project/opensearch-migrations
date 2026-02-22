563f7c500a6fba98dd981e2f6b8eb43226f6e48a
Andre Kurait <andrekurait@gmail.com>
2026-02-22 09:04:27 +0000

    Suppress SonarQube S1133 on DefaultSourceRepoAccessor
    Signed-off-by: Andre Kurait <andrekurait@gmail.com>

--- SnapshotReader/src/main/java/org/opensearch/migrations/bulkload/common/DefaultSourceRepoAccessor.java
+++ SnapshotReader/src/main/java/org/opensearch/migrations/bulkload/common/DefaultSourceRepoAccessor.java
@@ -5,6 +5,7 @@ package org.opensearch.migrations.bulkload.common;
  * This class exists only for backward compatibility.
  */
 @Deprecated(since = "0.1.0", forRemoval = true)
+@SuppressWarnings("java:S1133")
 public class DefaultSourceRepoAccessor extends SourceRepoAccessor {
     public DefaultSourceRepoAccessor(SourceRepo repo) {
         super(repo);
