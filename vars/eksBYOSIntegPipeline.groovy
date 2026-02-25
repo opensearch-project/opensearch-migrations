52558a6dd8a7b1afb981d8f792520a68d1e538b3
Andre Kurait <andrekurait@gmail.com>
2026-02-25 16:57:28 +0000

    Remove version field from target cluster configmap
    Target cluster schema does not accept a version field. The Argo
    workflow template already strips it with del(.version), but it
    should not be written to the configmap in the first place.
    
    Signed-off-by: Andre Kurait <andrekurait@gmail.com>

--- vars/eksBYOSIntegPipeline.groovy
+++ vars/eksBYOSIntegPipeline.groovy
@@ -253,8 +253,7 @@ def call(Map config = [:]) {
                                             sigv4: [
                                                     region: params.REGION,
                                                     service: "es"
-                                            ],
-                                            version: env.targetVer
+                                            ]
                                     ]
                                     sh """
                                       kubectl create configmap target-${targetVersionExpanded}-migration-config \
