0962d4286064b0171e3645a3e47eee04c0dba5e9
Jugal Chauhan <jugaldc@amazon.com>
2026-02-26 15:53:48 -0600

    (Refactor) extract waitForProcessExit helper into SourceTestBase
    Signed-off-by: Jugal Chauhan <jugaldc@amazon.com>

--- DocumentsFromSnapshotMigration/src/test/java/org/opensearch/migrations/bulkload/ProcessLifecycleTest.java
+++ DocumentsFromSnapshotMigration/src/test/java/org/opensearch/migrations/bulkload/ProcessLifecycleTest.java
@@ -196,18 +196,7 @@ public class ProcessLifecycleTest extends SourceTestBase {
         );
 
         var process = runAndMonitorProcess(processBuilder);
-        boolean finished = process.waitFor(timeoutSeconds, TimeUnit.SECONDS);
-        if (!finished) {
-            log.atError().setMessage("Process timed out, attempting to kill it...").log();
-            process.destroy(); // Try to be nice about things first...
-            if (!process.waitFor(10, TimeUnit.SECONDS)) {
-                log.atError().setMessage("Process still running, attempting to force kill it...").log();
-                process.destroyForcibly();
-            }
-            Assertions.fail("The process did not finish within the timeout period (" + timeoutSeconds + " seconds).");
-        }
-
-        return process.exitValue();
+        return waitForProcessExit(process, timeoutSeconds);
     }
 
     @SneakyThrows
