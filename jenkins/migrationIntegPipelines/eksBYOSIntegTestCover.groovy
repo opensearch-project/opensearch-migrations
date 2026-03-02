b86e8f7d80100cd38b4edf08279fc32d8944a281
Andre Kurait <andrekurait@gmail.com>
2026-03-02 05:08:55 +0000

    Set BYOS cover defaults to AndreKurait fork and jenkins-pipeline-eks-large-migration branch
    Signed-off-by: Andre Kurait <andrekurait@gmail.com>

--- jenkins/migrationIntegPipelines/eksBYOSIntegTestCover.groovy
+++ jenkins/migrationIntegPipelines/eksBYOSIntegTestCover.groovy
@@ -1,5 +1,5 @@
-def gitBranch = (params.GIT_BRANCH ?: 'main').replaceAll('[^\\x20-\\x7E]', '').trim()
-def gitUrl = (params.GIT_REPO_URL ?: 'https://github.com/opensearch-project/opensearch-migrations.git').trim()
+def gitBranch = (params.GIT_BRANCH ?: 'jenkins-pipeline-eks-large-migration').replaceAll('[^\\x20-\\x7E]', '').trim()
+def gitUrl = (params.GIT_REPO_URL ?: 'https://github.com/AndreKurait/opensearch-migrations.git').trim()
 
 library identifier: "migrations-lib@${gitBranch}", retriever: modernSCM(
         [$class: 'GitSCMSource',
