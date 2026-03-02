20dd60b775e07cb261e14d30e617bfab050b34fd
Andre Kurait <andrekurait@gmail.com>
2026-03-02 05:06:26 +0000

    Sanitize GIT_BRANCH parameter to strip invisible characters
    Build #146 failed because the GIT_BRANCH parameter contained a
    zero-width space (U+200B) at the start, making git unable to find
    the branch. Strip all non-printable-ASCII characters from the
    parameter value.
    
    Signed-off-by: Andre Kurait <andrekurait@gmail.com>

--- jenkins/migrationIntegPipelines/eksBYOSIntegTestCover.groovy
+++ jenkins/migrationIntegPipelines/eksBYOSIntegTestCover.groovy
@@ -1,5 +1,5 @@
-def gitBranch = params.GIT_BRANCH ?: 'main'
-def gitUrl = params.GIT_REPO_URL ?: 'https://github.com/opensearch-project/opensearch-migrations.git'
+def gitBranch = (params.GIT_BRANCH ?: 'main').replaceAll('[^\\x20-\\x7E]', '').trim()
+def gitUrl = (params.GIT_REPO_URL ?: 'https://github.com/opensearch-project/opensearch-migrations.git').trim()
 
 library identifier: "migrations-lib@${gitBranch}", retriever: modernSCM(
         [$class: 'GitSCMSource',
