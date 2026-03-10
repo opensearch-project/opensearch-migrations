45ada74737e3b8e16b56970e13507d506e49e90a
Andre Kurait <andrekurait@gmail.com>
2026-03-10 17:29:32 +0000

    Replace generic RuntimeException with RfsException to fix SonarQube S112
    Signed-off-by: Andre Kurait <andrekurait@gmail.com>

--- RFS/src/main/java/org/opensearch/migrations/bulkload/version_os_2_11/IndexCreator_OS_2_11.java
+++ RFS/src/main/java/org/opensearch/migrations/bulkload/version_os_2_11/IndexCreator_OS_2_11.java
@@ -7,6 +7,7 @@ import org.opensearch.migrations.MigrationMode;
 import org.opensearch.migrations.bulkload.common.IncompatibleReplicaCountException;
 import org.opensearch.migrations.bulkload.common.InvalidResponse;
 import org.opensearch.migrations.bulkload.common.OpenSearchClient;
+import org.opensearch.migrations.bulkload.common.RfsException;
 import org.opensearch.migrations.bulkload.models.IndexMetadata;
 import org.opensearch.migrations.metadata.CreationResult;
 import org.opensearch.migrations.metadata.CreationResult.CreationFailureType;
@@ -165,7 +166,7 @@ public class IndexCreator_OS_2_11 implements IndexCreator {
             }
         }
         log.warn("Unexpected exception type during index creation: {} - {}", e.getClass().getName(), e.getMessage());
-        throw new RuntimeException(e);
+        throw new RfsException("Unexpected exception during index creation", e);
     }
 
     private void handleInvalidResponse(InvalidResponse invalidResponse, String indexName, ObjectNode settings) throws IncompatibleReplicaCountException {
