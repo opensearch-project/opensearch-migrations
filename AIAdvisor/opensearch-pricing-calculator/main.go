d523e268244b9358b53dcd93ea23caa5ec3e56e0
Arun Lakshmanan <arnlaksh@amazon.com>
2026-03-14 13:47:46 -0500

    Add OpenSearch pricing calculator to AIAdvisor
    Signed-off-by: Arun Lakshmanan <arnlaksh@amazon.com>

---
+++ AIAdvisor/opensearch-pricing-calculator/main.go
@@ -0,0 +1,42 @@
+// Copyright OpenSearch Contributors
+// SPDX-License-Identifier: Apache-2.0
+
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/opensearch-project/opensearch-pricing-calculator/assistant"
+	_ "github.com/opensearch-project/opensearch-pricing-calculator/docs"
+
+	"go.uber.org/zap"
+)
+
+const (
+	defaultPort    = 5050
+	defaultMCPPort = 8081
+
+	ServiceName    = "opensearch-pricing-calculator"
+	ServiceVersion = "2.0.0"
+)
+
+type application struct {
+	Domain           string
+	logger           *zap.Logger
+	assistantHandler *assistant.Handler
+}
+
+//	@title			OpenSearch Pricing Calculator API
+//	@version		1.0
+//	@description	Cost estimation APIs for OpenSearch Managed Clusters and Serverless Collections.
+
+// @contact.name	OpenSearch Contributors
+// @contact.url	https://github.com/opensearch-project/opensearch-pricing-calculator
+func main() {
+	initCLI()
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
