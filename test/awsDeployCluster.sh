0182534c88e6105d637ea40024e8573e1fcd8d90
Jugal Chauhan <jugaldc@amazon.com>
2026-03-27 16:34:17 -0500

    Address comments on the remaining cdk command
    Signed-off-by: Jugal Chauhan <jugaldc@amazon.com>

--- test/awsDeployCluster.sh
+++ test/awsDeployCluster.sh
@@ -129,7 +129,7 @@ cp -f "$PROVIDED_CONTEXT_FILE_PATH" "$CLUSTER_CDK_CONTEXT_FILE_PATH"
 cd amazon-opensearch-service-sample-cdk
 
 if [[ "$DESTROY" == true ]]; then
-  cdk destroy "*" --force
+  npx cdk destroy "*" --force
 else
   CDK_OUTPUTS_FILE=$(mktemp)
   npx cdk deploy "*" --require-approval never --concurrency 3 --outputs-file "$CDK_OUTPUTS_FILE"
