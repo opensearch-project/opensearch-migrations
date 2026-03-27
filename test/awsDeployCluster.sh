ec9f79c26ff6831697f99dd08472afdb333abbee
Jugal Chauhan <jugaldc@amazon.com>
2026-03-27 14:22:43 -0500

    Resolve issue due to merge conflict
    Signed-off-by: Jugal Chauhan <jugaldc@amazon.com>

--- test/awsDeployCluster.sh
+++ test/awsDeployCluster.sh
@@ -127,14 +127,12 @@ cd ..
 cp -f "$PROVIDED_CONTEXT_FILE_PATH" "$CLUSTER_CDK_CONTEXT_FILE_PATH"
 
 cd amazon-opensearch-service-sample-cdk
-CDK_OUTPUTS_FILE=$(mktemp)
-npx cdk deploy "*" --require-approval never --concurrency 3 --outputs-file "$CDK_OUTPUTS_FILE"
 
 if [[ "$DESTROY" == true ]]; then
   cdk destroy "*" --force
 else
   CDK_OUTPUTS_FILE=$(mktemp)
-  cdk deploy "*" --require-approval never --concurrency 3 --outputs-file "$CDK_OUTPUTS_FILE"
+  npx cdk deploy "*" --require-approval never --concurrency 3 --outputs-file "$CDK_OUTPUTS_FILE"
 
   CLUSTER_DETAILS_OUTPUT_FILE_PATH="$ROOT_REPO_PATH/test/tmp/cluster-details-${STAGE}.json"
   mkdir -p "$(dirname "$CLUSTER_DETAILS_OUTPUT_FILE_PATH")"
