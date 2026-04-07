13bea2e60b94f80ddccc0538136e833f9c42ba1d
Andre Kurait <andrekurait@gmail.com>
2026-04-07 03:33:58 +0000

    Hijack elasticsearch-5x-k8s-local-test to run Solr test
    Temporarily point the ES 5.x k8s test job at SOLR_8.11/Solr0001 so the
    existing Jenkins pipeline exercises the new Solr Argo cluster template
    and test case. Revert the jenkins_tests.yml change since pull_request_target
    runs from the base branch.
    
    Signed-off-by: Andre Kurait <andrekurait@gmail.com>

--- vars/elasticsearch5xK8sLocalTest.groovy
+++ vars/elasticsearch5xK8sLocalTest.groovy
@@ -1,8 +1,8 @@
 def call(Map config = [:]) {
     k8sLocalDeployment(
             jobName: config.jobName ?: 'elasticsearch-5x-k8s-local-test',
-            sourceVersion: 'ES_5.6',
-            targetVersion: 'OS_3.1',
-            testIds: '0001,0002,0004,0005'
+            sourceVersion: 'SOLR_8.11',
+            targetVersion: 'OS_2.19',
+            testIds: 'Solr0001'
     )
 }
