f8703ccecea0d1c05324b731cf8bcc6078954f17
Greg Schohn <schohn@amazon.com>
2026-04-07 09:59:51 -0400

    Shift how packets are generated for a replayer request to go through an intermediary for every http request.
    Before we passed a static set of ByteBufs through the code that were processed (ideally) 1s before they were scheduled to be sent.
    We still do that today, but the interfaces have changed.
    Now the RequestSenderOrchestrator calls a refcounted ByteBufListProducer to get those packets for every request.
    That allows the upstream producer to determine what can be precomputed and what should be generated on the fly... possibly taking the current time into consideration.
    For now, TransformedPacketReceiver just does the same thing that it always has, wrapping the static ByteBufs in the new interface, but i'm hoping to make it more capable in future commits.
    
    Signed-off-by: Greg Schohn <schohn@amazon.com>

--- TrafficCapture/trafficReplayer/src/main/java/org/opensearch/migrations/replay/http/retries/IRetryVisitorFactory.java
+++ TrafficCapture/trafficReplayer/src/main/java/org/opensearch/migrations/replay/http/retries/IRetryVisitorFactory.java
@@ -2,12 +2,12 @@ package org.opensearch.migrations.replay.http.retries;
 
 import org.opensearch.migrations.replay.IRequestResponsePacketPair;
 import org.opensearch.migrations.replay.RequestSenderOrchestrator;
-import org.opensearch.migrations.replay.datatypes.ByteBufList;
+import org.opensearch.migrations.replay.datatypes.ByteBufListProducer;
 import org.opensearch.migrations.replay.datatypes.TransformedOutputAndResult;
 import org.opensearch.migrations.utils.TrackedFuture;
 
 public interface IRetryVisitorFactory<T> {
     RequestSenderOrchestrator.RetryVisitor<T>
-    getRetryCheckVisitor(TransformedOutputAndResult<ByteBufList> transformedResult,
+    getRetryCheckVisitor(TransformedOutputAndResult<ByteBufListProducer> transformedResult,
                          TrackedFuture<String, ? extends IRequestResponsePacketPair> finishedAccumulatingResponseFuture);
 }
