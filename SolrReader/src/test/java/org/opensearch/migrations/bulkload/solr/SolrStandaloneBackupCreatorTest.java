c14a92c1ce7b241bd8b123213a527c2a8d91b297
Andre Kurait <andrekurait@gmail.com>
2026-04-08 21:22:30 +0000

    Remove unused nullValue import
    Signed-off-by: Andre Kurait <andrekurait@gmail.com>

--- SolrReader/src/test/java/org/opensearch/migrations/bulkload/solr/SolrStandaloneBackupCreatorTest.java
+++ SolrReader/src/test/java/org/opensearch/migrations/bulkload/solr/SolrStandaloneBackupCreatorTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.nullValue;
 
 class SolrStandaloneBackupCreatorTest {
 
