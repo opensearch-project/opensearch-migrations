64d57020338f96a0418f9c039eebc24cfdd57920
Jugal Chauhan <jugaldc@amazon.com>
2026-04-12 21:22:29 -0500

    Cleanup branch, comment on speedup factor, switch github url for jenkins
    Signed-off-by: Jugal Chauhan <jugaldc@amazon.com>

--- vars/eksCdcIntegPipeline.groovy
+++ vars/eksCdcIntegPipeline.groovy
@@ -26,8 +26,8 @@ def call(Map config = [:]) {
         agent { label config.workerAgent ?: 'Jenkins-Default-Agent-X64-C5xlarge-Single-Host' }
 
         parameters {
-            string(name: 'GIT_REPO_URL', defaultValue: 'https://github.com/jugal-chauhan/opensearch-migrations.git', description: 'Git repository url')
-            string(name: 'GIT_BRANCH', defaultValue: 'initial-eks-cdc-test', description: 'Git branch to use for repository')
+            string(name: 'GIT_REPO_URL', defaultValue: 'https://github.com/opensearch-project/opensearch-migrations.git', description: 'Git repository url')
+            string(name: 'GIT_BRANCH', defaultValue: 'main', description: 'Git branch to use for repository')
             string(name: 'GIT_COMMIT', defaultValue: '', description: '(Optional) Specific commit to checkout after cloning branch')
             string(name: 'TEST_IDS', defaultValue: "${defaultTestIds}", description: 'Comma-separated test IDs to run (e.g. 0030)')
             string(name: 'STAGE', defaultValue: "${defaultStageId}", description: 'Stage name for deployment environment')
