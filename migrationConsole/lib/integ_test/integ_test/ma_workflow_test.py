739581818de7e260075d91c869a9b1e6b4c04675
Andre Kurait <andrekurait@gmail.com>
2026-04-14 15:50:07 +0000

    Revert ma_workflow_test.py to main
    Signed-off-by: Andre Kurait <andrekurait@gmail.com>

--- migrationConsole/lib/integ_test/integ_test/ma_workflow_test.py
+++ migrationConsole/lib/integ_test/integ_test/ma_workflow_test.py
@@ -1,5 +1,4 @@
 import logging
-
 import pytest
 
 from .test_cases.ma_argo_test_base import MATestBase
@@ -27,6 +26,7 @@ def setup_and_teardown(request, keep_workflows, test_case: MATestBase):
         if request.node.rep_call and request.node.rep_call.failed:
             logger.info(f"Test failed - printing workflow details for {test_case.workflow_name}")
             test_case.argo_service.print_workflow_details(workflow_name=test_case.workflow_name)
+            # Save detailed diagnostics to logs directory (archived by Jenkins)
             test_case.argo_service.save_namespace_diagnostics("./logs")
         if not keep_workflows:
             test_case.argo_service.delete_workflow(workflow_name=test_case.workflow_name)
