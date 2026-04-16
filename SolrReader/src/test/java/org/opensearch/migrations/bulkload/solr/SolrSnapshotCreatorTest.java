1d3b804d7765edf18d028e3c44a4b36839e81a81
Andre Kurait <andrekurait@gmail.com>
2026-04-16 18:57:40 +0000

    Fix spotless import ordering in SolrSnapshotCreatorTest
    Signed-off-by: Andre Kurait <andrekurait@gmail.com>

--- SolrReader/src/test/java/org/opensearch/migrations/bulkload/solr/SolrSnapshotCreatorTest.java
+++ SolrReader/src/test/java/org/opensearch/migrations/bulkload/solr/SolrSnapshotCreatorTest.java
@@ -1,8 +1,8 @@
 package org.opensearch.migrations.bulkload.solr;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SolrSnapshotCreatorTest {
 
