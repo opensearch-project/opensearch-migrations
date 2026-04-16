a281ec368a959369281c2075e13fa35fc56d891f
Jugal Chauhan <jugaldc@amazon.com>
2026-04-16 16:15:05 -0500

    Remove Test0031 CDC TLS test and associated pipeline/workflow
    Signed-off-by: Jugal Chauhan <jugaldc@amazon.com>

--- migrationConsole/lib/integ_test/integ_test/conftest.py
+++ migrationConsole/lib/integ_test/integ_test/conftest.py
@@ -25,7 +25,6 @@ from .test_cases.backfill_tests import *
 from .test_cases.snapshot_only_tests import *
 from .test_cases.cdc_tests import *
 from .test_cases.cdc_generate_data_tests import *
-from .test_cases.cdc_tls_tests import *
 from .test_cases.cdc_mixed_operations_tests import *
 from .test_cases.cdc_simple_bulk_e2e_tests import *
 from .test_cases.cdc_aoss_tests import *
