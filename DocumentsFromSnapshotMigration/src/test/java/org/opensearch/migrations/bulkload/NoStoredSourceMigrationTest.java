fe4646e480dc42af4822db614956ebb834485efb
Andre Kurait <andrekurait@gmail.com>
2026-04-23 17:05:28 +0000

    spotlessApply: drop unused assertTrue import
    Signed-off-by: Andre Kurait <andrekurait@gmail.com>

--- DocumentsFromSnapshotMigration/src/test/java/org/opensearch/migrations/bulkload/NoStoredSourceMigrationTest.java
+++ DocumentsFromSnapshotMigration/src/test/java/org/opensearch/migrations/bulkload/NoStoredSourceMigrationTest.java
@@ -36,7 +36,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Matrix end-to-end test: for each (source ES/OS version, target OS version) pair, creates one
