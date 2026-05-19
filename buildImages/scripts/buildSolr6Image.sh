48e033d7de78f1711e77c47c524abe617c926a7a
awagen <awagen@posteo.net>
2026-05-19 17:05:34 +0200

    Changing solr 6 archive source
    Signed-off-by: awagen <awagen@posteo.net>

--- buildImages/scripts/buildSolr6Image.sh
+++ buildImages/scripts/buildSolr6Image.sh
@@ -3,5 +3,8 @@
 set -eo pipefail
 
 # script assumes registry is up and running and listening on port 5001 (see fillLocalRegistry.sh)
-docker build . -f ../custom-solr-images/dockerfiles/Dockerfile  -t localhost:5001/custom-solr:6.6.6
-docker push localhost:5001/custom-solr:6.6.6
+CWD=$(pwd)
+cd ../custom-solr-images/dockerfiles
+docker build . -t localhost:5001/custom-solr:6.6.0
+docker push localhost:5001/custom-solr:6.6.0
+cd $CWD
