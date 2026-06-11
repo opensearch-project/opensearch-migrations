bdefe8784c4b830b03e8e2daa092437fd6bbf9df
awagen <awagen@posteo.net>
2026-06-11 12:50:48 +0200

    Removing jenkins config for solr 6, solr 9
    Signed-off-by: awagen <awagen@posteo.net>

--- vars/solr6xK8sLocalTest.groovy
+++
@@ -1,8 +0,0 @@
-def call(Map config = [:]) {
-    k8sLocalDeployment(
-            jobName: config.jobName ?: 'solr-6x-k8s-local-test',
-            sourceVersion: 'SOLR_6.6',
-            targetVersion: 'OS_2.19',
-            testIds: 'Solr0001'
-    )
-}
--- vars/solr9xK8sLocalTest.groovy
+++
@@ -1,8 +0,0 @@
-def call(Map config = [:]) {
-    k8sLocalDeployment(
-            jobName: config.jobName ?: 'solr-9x-k8s-local-test',
-            sourceVersion: 'SOLR_9.8',
-            targetVersion: 'OS_2.19',
-            testIds: 'Solr0001'
-    )
-}
