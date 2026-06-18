eb1827e3f992cc6713598b315936e456f3257ba7
Andre Kurait <akurait@amazon.com>
2026-06-18 02:12:14 -0500

    Fix Jest ESM parse failure for testcontainers 12.x integ tests
    testcontainers 12.0.2 transitively pulls archiver 8.0 which (along with
    compress-commons, crc32-stream, zip-stream, is-stream, get-port) ships as
    "type": "module". Jest in CJS mode cannot parse these without explicit
    transform configuration. Add transformIgnorePatterns negative lookahead
    and a JS transform rule to the integ config so these ESM deps are
    transpiled during test runs.
    
    Signed-off-by: Andre Kurait <akurait@amazon.com>

--- orchestrationSpecs/packages/config-processor/jest.integ.config.js
+++ orchestrationSpecs/packages/config-processor/jest.integ.config.js
@@ -7,6 +7,13 @@ module.exports = {
     testPathIgnorePatterns: [
         "/node_modules/",
     ],
+    transformIgnorePatterns: [
+        "/node_modules/(?!(archiver|compress-commons|crc32-stream|zip-stream|is-stream|get-port)/)",
+    ],
+    transform: {
+        "^.+\\.ts$": ["ts-jest"],
+        "^.+\\.js$": ["ts-jest", { tsconfig: { allowJs: true } }],
+    },
     coveragePathIgnorePatterns: [
         ...(base.coveragePathIgnorePatterns ?? []),
         "/tests/integ/",
