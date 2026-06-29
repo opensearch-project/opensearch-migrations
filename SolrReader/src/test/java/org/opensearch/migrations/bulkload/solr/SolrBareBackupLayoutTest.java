be5f5cdca744a830892e194bb20067aeefbb04dd
jzonthemtn <jeff.zemerick@mtnfog.com>
2026-06-29 10:03:22 -0400

    Applying spotless.
    Signed-off-by: jzonthemtn <jeff.zemerick@mtnfog.com>

--- SolrReader/src/test/java/org/opensearch/migrations/bulkload/solr/SolrBareBackupLayoutTest.java
+++ SolrReader/src/test/java/org/opensearch/migrations/bulkload/solr/SolrBareBackupLayoutTest.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-import org.opensearch.migrations.bulkload.solr.SolrBackupLayout.BareBackupLayout;
 import org.opensearch.migrations.bulkload.solr.SolrBackupLayout.SolrBackupMode;
 
 import org.junit.jupiter.api.Test;
